d65441b9d0275cec95b32c979f6e252c9a0f8552
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-03 18:24:11 +0200

    Added brigand

---
+++ include/boost/simd/detail/brigand/types/no_such_type.hpp
@@ -0,0 +1,14 @@
+/*==================================================================================================
+  Copyright (c) 2015 Edouard Alligand and Joel Falcou
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+=================================================================================================**/
+#pragma once
+
+namespace brigand
+{
+    // type to return when finding something fails
+    // I wanted to call it "not sure if type" but Joel didn't want to :(
+    struct no_such_type_ {};
+}
