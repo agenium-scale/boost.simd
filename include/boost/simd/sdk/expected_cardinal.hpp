0083ea4d6fd855db7af5d0d92f7d194233b3be34
Joel Falcou <joel.falcou@lri.fr>
2015-07-30 22:47:20 +0200

    Made expected_cardinal public

--- include/boost/simd/sdk/expected_cardinal.hpp
+++ include/boost/simd/sdk/expected_cardinal.hpp
@@ -13,8 +13,8 @@
 
 **/
 //==================================================================================================
-#ifndef BOOST_SIMD_DETAIL_EXPECTED_CARDINAL_HPP_INCLUDED
-#define BOOST_SIMD_DETAIL_EXPECTED_CARDINAL_HPP_INCLUDED
+#ifndef BOOST_SIMD_SDK_EXPECTED_CARDINAL_HPP_INCLUDED
+#define BOOST_SIMD_SDK_EXPECTED_CARDINAL_HPP_INCLUDED
 
 #include <boost/simd/arch/limits.hpp>
 #include <boost/simd/detail/brigand.hpp>
@@ -23,26 +23,23 @@ namespace boost { namespace simd
 {
   template<typename T> struct logical;
 
-  namespace detail
-  {
-    /*!
-      @ingroup group-detail
-      @brief Expected cardinal of register
+  /*!
+    @ingroup group-api
+    @brief Expected cardinal of register
 
-      Computes the cardinal a potential SIMD register containing elements of type @c Type on
-      extension @c Extension.
+    Computes the cardinal a potential SIMD register containing elements of type @c Type on
+    extension @c Extension.
 
-      @tparam Type      Element type of the expected register
-      @tparam Extension SIMD Extension tag
-    **/
-    template<typename Type, typename Extension>
-    struct expected_cardinal : brigand::int_<limits<Extension>::bytes/sizeof(Type)>
-    {};
+    @tparam Type      Element type of the expected register
+    @tparam Extension SIMD Extension tag
+  **/
+  template<typename Type, typename Extension>
+  struct expected_cardinal : brigand::int_<limits<Extension>::bytes/sizeof(Type)>
+  {};
 
-    template<typename Type, typename Extension>
-    struct expected_cardinal<logical<Type>> : expected_cardinal<Type,Extension>
-    {};
-  }
+  template<typename Type, typename Extension>
+  struct expected_cardinal<logical<Type>,Extension> : expected_cardinal<Type,Extension>
+  {};
 } }
 
 #endif
