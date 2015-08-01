fab953ad0f714523af6f8ab7dc86d9ad64179996
Joel Falcou <joel.falcou@lri.fr>
2015-08-01 17:44:28 +0200

    brigand update

--- include/boost/simd/detail/brigand.hpp
+++ include/boost/simd/detail/brigand.hpp
@@ -35,20 +35,24 @@ namespace std
 namespace brigand
 {
   template <typename... T>
-  struct pair_wrapper
+  struct pair_wrapper_
   {
     static_assert (sizeof...(T) == 2
                   , "as_pair requires a type list of exactly two types"
                   );
 
-        using type = no_such_type_;
+    // type need to be defined or the error becomes a hard, non-static assert error
+    using type = no_such_type_;
   };
 
   template <typename T, typename U>
-  struct pair_wrapper<T,U>
+  struct pair_wrapper_<T,U>
   {
       using type = std::pair<T,U>;
   };
+
+  template <typename... T>
+  using pair_wrapper = typename pair_wrapper_<T...>::type;
 
   template <typename L>
   using as_pair = apply<L, pair_wrapper>;
@@ -64,10 +68,7 @@ namespace std
 namespace brigand
 {
   template <typename... T>
-  struct tuple_wrapper
-  {
-      using type = typename std::tuple<T...>;
-  };
+  using tuple_wrapper = typename std::tuple<T...>;
 
   template <typename L>
   using as_tuple = apply<L, tuple_wrapper>;
@@ -86,10 +87,7 @@ namespace brigand
 {
 
     template <typename... T>
-    struct variant_wrapper
-    {
-        using type = typename boost::variant<T...>;
-    };
+    using variant_wrapper  = typename boost::variant<T...>;
 
     template <typename L>
     using as_variant = apply<L, variant_wrapper>;
