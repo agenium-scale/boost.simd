1ced43819640e9a21d9847419b48198d861b8672
Charly Chevalier <charly.chevalier@numscale.com>
2015-08-03 14:08:11 +0200

    Doc examples

---
+++ example/dot.cpp
@@ -0,0 +1,35 @@
+//==================================================================================================
+/*
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+
+#include <boost/simd/pack.hpp>
+#include <boost/simd/io.hpp>
+#include <boost/simd/functions/splat.hpp>
+#include <boost/simd/functions/plus.hpp>
+#include <boost/simd/functions/multiplies.hpp>
+#include <iostream>
+
+//! [scalar-dot]
+template<typename Value> Value dot(Value* first1, Value* last1, Value* first2)
+{
+  Value v{};
+
+  while(first1 != last1)
+  {
+    v += *first1 *  *first2;
+    first1++;
+    first2++;
+  }
+
+  return v;
+}
+//! [scalar-dot]
+
+int main()
+{
+}
---
+++ example/helloworld.cpp
@@ -0,0 +1,44 @@
+//==================================================================================================
+/*
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+
+//! [hello]
+#include <boost/simd/pack.hpp>
+#include <boost/simd/io.hpp>
+#include <boost/simd/functions/splat.hpp>
+#include <boost/simd/functions/plus.hpp>
+#include <boost/simd/functions/multiplies.hpp>
+#include <iostream>
+
+int main()
+{
+  using p_t = boost::simd::pack<float>;
+
+  //! [hello-def-ctor]
+  p_t res;
+  //! [hello-def-ctor]
+
+  //! [hello-splat-ctor]
+  p_t u{10};
+  //! [hello-splat-ctor]
+
+  //! [hello-enum-ctor]
+  p_t r{11,11,11,11};
+  //! [hello-enum-ctor]
+
+  //! [hello-ops]
+  res = (u + r) * 2.f;
+  //! [hello-ops]
+
+  //! [hello-io]
+  std::cout << res << std::endl;
+  //! [hello-io]
+
+  return 0;
+}
+//! [hello]
