a24bdc8b9132a2f864b78eb9de06f117e50fcb87
Joel Falcou <joel.falcou@lri.fr>
2015-10-19 21:56:39 +0200

    logical type

---
+++ include/boost/simd/logical.hpp
@@ -0,0 +1,111 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_SIMD_LOGICAL_HPP_INCLUDED
+#define BOOST_SIMD_LOGICAL_HPP_INCLUDED
+
+#include <boost/dispatch/meta/generation/as_integer.hpp>
+#include <boost/config.hpp>
+#include <iosfwd>
+
+namespace boost { namespace simd
+{
+  /*!
+    @ingroup group-api
+    Typed boolean value.
+
+    logical<T> is a typed boolean value which can be vectorized in a portable
+    way (contrary to bool). Its semantic is equivalent to bool on all aspect
+    but may provide a compiler dependent, optimized bool-to-type conversion.
+
+    @tparam T Type the boolean value is generated from.
+  **/
+  template<typename T> struct logical
+  {
+    using value_type  = T;
+    using bits        = dispatch::as_integer_t<T>;
+
+    /// Default constructor
+    BOOST_FORCEINLINE logical() = default;
+
+    /// Copy constructor
+    BOOST_FORCEINLINE logical(logical const&) = default;
+
+    /// Constructor from boolean value
+    BOOST_FORCEINLINE logical(bool v) : value_(v) {}
+
+    /// Constructor from non-boolean value
+    template<typename U> BOOST_FORCEINLINE explicit logical(U&& v) : value_(v != 0) {}
+
+    // Assignment operator
+    BOOST_FORCEINLINE logical& operator=(logical const& v) = default;
+
+    // Assignment operator from bool
+    BOOST_FORCEINLINE logical& operator=(bool v)
+    {
+      value_ = v;
+      return *this;
+    }
+
+    /// Bitwise complement operator
+    BOOST_FORCEINLINE logical operator ~() { return {!value_}; }
+
+    /// Logical not operator
+    BOOST_FORCEINLINE logical operator !() { return {!value_}; }
+
+    /// Convert a logical value to bool
+    BOOST_FORCEINLINE operator bool() const { return value_; }
+
+    /// Explicitly convert a logical value to bool
+    BOOST_FORCEINLINE bool value() const {return value_; }
+
+    private:
+    bool  value_;
+  };
+
+  /// Stream insertion operator
+  template<typename T>
+  BOOST_FORCEINLINE std::ostream& operator<<(std::ostream& os, logical<T> const& v )
+  {
+    return os << (v.value() ? "true" : "false");
+  }
+
+  /// Equality comparison operator
+  template<typename T> BOOST_FORCEINLINE
+  logical<T> operator ==(logical<T> const& a, logical<T> const& b)
+  {
+    return a.value() == b.value();
+  }
+
+  /// Inequality comparison operator
+  template<typename T> BOOST_FORCEINLINE
+  logical<T> operator !=(logical<T> const& a, logical<T> const& b)
+  {
+    return a.value() != b.value();
+  }
+
+  /// Equality comparison operator
+  template<typename T> BOOST_FORCEINLINE
+  logical<T> operator &&(logical<T> const& a, logical<T> const& b)
+  {
+    return a.value() && b.value();
+  }
+
+  /// Inequality comparison operator
+  template<typename T> BOOST_FORCEINLINE
+  logical<T> operator ||(logical<T> const& a, logical<T> const& b)
+  {
+    return a.value() || b.value();
+  }
+} }
+
+#endif
---
+++ test/api/logical.cpp
@@ -0,0 +1,89 @@
+//==================================================================================================
+/*
+  Copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/logical.hpp>
+#include <stf.hpp>
+
+using namespace boost::simd;
+
+STF_CASE_TPL( "Check logical<T> conversion from/to bool", STF_NUMERIC_TYPES )
+{
+  logical<T> bool_true(true);
+  STF_EQUAL( bool(bool_true)  , true );
+  STF_EQUAL( bool_true.value(), true );
+
+  logical<T> bool_false(false);
+  STF_EQUAL( bool(bool_false)     , false );
+  STF_EQUAL( bool_false.value()   , false );
+
+  logical<T> val_true( T(42) );
+  STF_EQUAL( bool(val_true)   , true );
+  STF_EQUAL( val_true.value() , true );
+
+  logical<T> val_false( T(0) );
+  STF_EQUAL( bool(val_false)  , false );
+  STF_EQUAL( val_false.value(), false );
+
+  if(val_false) STF_ERROR("logical<T> dont behave well in if()");
+  if((val_true  ? false  : true)) STF_ERROR("logical<T> dont behave well in ?:");
+}
+
+STF_CASE_TPL( "Check logical<T> complement operator", STF_NUMERIC_TYPES )
+{
+  logical<T> bool_true(true);
+  STF_EQUAL( (!bool_true).value(), false );
+  STF_EQUAL( (~bool_true).value(), false );
+
+  logical<T> bool_false(false);
+  STF_EQUAL( (!bool_false).value(), true  );
+  STF_EQUAL( (~bool_false).value(), true  );
+
+  logical<T> val_true( T(42) );
+  STF_EQUAL( (!val_true).value(), false );
+  STF_EQUAL( (~val_true).value(), false );
+
+  logical<T> val_false( T(0) );
+  STF_EQUAL( (!val_false).value(), true  );
+  STF_EQUAL( (~val_false).value(), true  );
+}
+
+STF_CASE_TPL( "Check logical<T> comparison operators", STF_NUMERIC_TYPES )
+{
+  logical<T> bool_true(true);
+  logical<T> bool_false(false);
+  logical<T> val_true( T(42) );
+  logical<T> val_false( T(0) );
+
+  STF_EQUAL( bool_true  , val_true  );
+  STF_EQUAL( bool_false , val_false );
+  STF_NOT_EQUAL( bool_true  , val_false );
+  STF_NOT_EQUAL( bool_false , val_true  );
+}
+
+STF_CASE_TPL( "Check logical<T> logical and operator", STF_NUMERIC_TYPES )
+{
+  logical<T> val_true( T(42) );
+  logical<T> val_false( T(0) );
+
+  STF_EQUAL( val_true  && val_true  , val_true  );
+  STF_EQUAL( val_true  && val_false , val_false );
+  STF_EQUAL( val_false && val_true  , val_false );
+  STF_EQUAL( val_false && val_false , val_false );
+}
+
+STF_CASE_TPL( "Check logical<T> logical or operator", STF_NUMERIC_TYPES )
+{
+  logical<T> val_true( T(42) );
+  logical<T> val_false( T(0) );
+
+  STF_EQUAL( val_true  || val_true  , val_true  );
+  STF_EQUAL( val_true  || val_false , val_true  );
+  STF_EQUAL( val_false || val_true  , val_true  );
+  STF_EQUAL( val_false || val_false , val_false );
+}
