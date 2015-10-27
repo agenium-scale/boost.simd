219e5cf0e77450bc0b87e7cfe51fb7859d56f9aa
jtlap <jtlapreste@gmail.com>
2015-10-27 14:29:51 +0100

    adding saturated dists function

---
+++ include/boost/simd/arch/common/scalar/function/dists.hpp
@@ -0,0 +1,68 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2009-2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_DISTS_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_DISTS_HPP_INCLUDED
+
+#include <boost/simd/function/scalar/dist.hpp>
+#include <boost/simd/constant/valmax.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/dispatch/function/overload.hpp>
+#include <boost/config.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( dists_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::arithmetic_<A0> >
+                          , bd::scalar_< bd::arithmetic_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 a0, A0 a1) const BOOST_NOEXCEPT
+    {
+      A0 tmp = dist(a0, a1);
+      return tmp < Zero<A0>() ? Valmax<A0>() : tmp;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( dists_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::unsigned_<A0> >
+                          , bd::scalar_< bd::unsigned_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 a0, A0 a1) const BOOST_NOEXCEPT
+    {
+      return dist(a0, a1);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( dists_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_<bd::floating_<A0> >
+                          , bd::scalar_<bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 a0, A0 a1) const BOOST_NOEXCEPT
+    {
+      return dist(a0, a1);
+    }
+  };
+} } }
+
+
+#endif
---
+++ include/boost/simd/function/dists.hpp
@@ -0,0 +1,70 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2009-2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2012-2015 NumScale SAS
+  @copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_DISTS_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_DISTS_HPP_INCLUDED
+
+#if defined(DOXYGEN_ONLY)
+namespace boost { namespace simd
+{
+  /*!
+
+    @ingroup group-arithmetic
+
+    Computes the (saturated) absolute value of the difference of its parameters.
+
+    @par semantic:
+    For any given value @c x,  @c y of type @c T:
+
+    @code
+    T r = dists(x, y);
+    @endcode
+
+    is similar to:
+
+    @code
+    T r = abss(x-y);
+    @endcode
+
+    @par Note
+
+    The result is never negative. For floating, it can of course be Nan.
+
+    @see  ulpdist
+
+    @alias  saturated_dist
+
+    @return      a value of the same type as the input.
+
+
+**/
+  template<typename T> auto dists(T const& x, T const& y) {}
+
+  namespace functional
+  {
+    /*!
+      @ingroup group-arithmetic
+
+      Function object tied to simd::dists
+
+      @see simd::dists
+    **/
+    const boost::dispatch::functor<tag::dists_> dists = {};
+  }
+} }
+#endif
+
+#include <boost/simd/function/definition/dists.hpp>
+#include <boost/simd/arch/common/scalar/function/dists.hpp>
+//#include <boost/simd/arch/common/function/simd/dists.hpp>
+
+#endif
