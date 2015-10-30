bca50ec329c890d6c41e62cc9d44f47f1c342b84
Charly Chevalier <charly.chevalier@numscale.com>
2015-10-30 15:41:57 +0100

    Implement Real<T,D,S>() constant generator
    Real<T,D,S> allow generic generation of IEEE
    constant choosing the correct representation
    from the target type.

---
+++ include/boost/simd/constant/real.hpp
@@ -0,0 +1,32 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2012-2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_CONSTANT_REAL_HPP_INCLUDED
+#define BOOST_SIMD_CONSTANT_REAL_HPP_INCLUDED
+
+#if defined(DOXYGEN_ONLY)
+namespace boost { namespace simd
+{
+  /*!
+    @ingroup group-constant
+    @brief Generate a constant from a static real number representation.
+
+    @tparam Type    Type of the generated constant
+    @tparam Double  Numerator of the generated constant
+    @tparam Single  Denumerator of the generated constant
+  **/
+  template<typename Type, std::uintmax_t Double, std::uintmax_t Single> auto Real();
+} }
+#endif
+
+#include <boost/simd/constant/definition/real.hpp>
+#include <boost/simd/arch/common/scalar/constant/constant_value.hpp>
+
+#endif
---
+++ test/constant/scalar/real.cpp
@@ -0,0 +1,21 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/constant/real.hpp>
+#include <simd_test.hpp>
+
+STF_CASE( "Check real constant behavior for IEEE types")
+{
+  using boost::simd::Real;
+
+  STF_EXPR_IS   ( (Real<double,0x402ABD70A3D70A3DULL,0x4155EB85UL>() ) , double  );
+  STF_EXPR_IS   ( (Real<float,0x402ABD70A3D70A3DULL,0x4155EB85UL>() ) , float  );
+  STF_IEEE_EQUAL( (Real<double,0x402ABD70A3D70A3DULL,0x4155EB85UL>() ) , 13.37   );
+  STF_IEEE_EQUAL( (Real<float,0x402ABD70A3D70A3DULL,0x4155EB85UL>() ) , 13.37f   );
+}
