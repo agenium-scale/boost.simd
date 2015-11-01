18e5290657c2e7dd663a5af5e1506ff8a27be757
jtlap <jtlapreste@gmail.com>
2015-11-01 15:28:33 +0100

    test skeleton for exponential functions

---
+++ test/function/scalar/cbrt.cpp
@@ -0,0 +1,26 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/cbrt.hpp>
+#include <stf.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+
+STF_CASE("cbrt TO DO")
+{
+  STF_FAIL("TO DO");
+
+}
+
