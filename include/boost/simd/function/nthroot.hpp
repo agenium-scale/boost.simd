faf94ad958fb7756855e1fda29d98a032ed3aac9
jtlap <jtlapreste@gmail.com>
2015-11-03 15:30:55 +0100

    nthroot function

--- include/boost/simd/function/nthroot.hpp
+++ include/boost/simd/function/nthroot.hpp
@@ -2,8 +2,8 @@
 /*!
   @file
 
-  @copyright 2009-2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
-  @copyright 2012-2015 NumScale SAS
+  @copyright 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  @copyright 2015 NumScale SAS
   @copyright 2015 J.T.Lapreste
 
   Distributed under the Boost Software License, Version 1.0.
@@ -17,17 +17,55 @@
 namespace boost { namespace simd
 {
   /*!
-  @ingroup group-trigonometry
 
-    Computes nthroot value of its parameter.
+    @ingroup group-exponential
 
-  **/
+    nth root function: \f$\sqrt[n]{x}\f$
+    \arg n must be of integer type
+    \arg if n is even and x negative the result is @ref Nan
+    \arg if x is null the result is @ref Zero
+    \arg if x is one  the result is @ref One
+
+    @par Semantic:
+
+    For every parameters of  floating type T and integral type N:
+
+    @code
+    T r = nthroot(x, n);
+    @endcode
+
+    is similar to:
+
+    @code
+    T r = n >= 0 ? pow(x, rec(tofloat(n))) : Nan;
+    @endcode
+
+    @par Note:
+    nthroot is more expansive than pow(x, rec(tofloat(n))) because
+    it takes care of some limits issues that @ref pow does not mind of.
+
+    See if it suits you better.
+
+    @see pow, rec, sqrt, cbrt
+
+
+    @return a value of the same type as the parameter
+
+**/
   template<typename T> auto nthroot(T const& x) {}
 
   namespace functional
   {
     /*!
-      @ingroup group-callable-trigonometry
+      @ingroup group-callable-exponential
+
+
+      nth root function: \f$\sqrt[a_1]{a_0}\f$
+      \arg a1 must be of integer type
+      \arg if a1 is even and x negative the result is nan
+      \arg if x is null the result is zero
+      \arg if x is one  the result is one
+
 
       Function object tied to simd::nthroot
 
@@ -38,8 +76,7 @@ namespace boost { namespace simd
 } }
 #endif
 
-#include <boost/simd/function/definition/nthroot.hpp>
-#include <boost/simd/arch/common/scalar/function/nthroot.hpp>
+#include <boost/simd/function/scalar/nthroot.hpp>
 #include <boost/simd/function/simd/nthroot.hpp>
 
 #endif
