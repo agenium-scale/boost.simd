05ea51b3c39f504b796ca704be22ac3f23d64724
jtlap <jtlapreste@gmail.com>
2015-11-03 14:18:30 +0100

    exprecnegc function

--- include/boost/simd/function/exprecnegc.hpp
+++ include/boost/simd/function/exprecnegc.hpp
@@ -16,17 +16,40 @@
 namespace boost { namespace simd
 {
   /*!
-  @ingroup group-trigonometry
 
-    Computes exprecnegc value of its parameter.
+    @ingroup group-exponential
 
-  **/
+    Computes the  function: \f$1-e^{-\frac1x}\f$
+
+    @par Semantic:
+
+    For every parameter of floating type T
+
+    @code
+    T r = exprecnegc(x);
+    @endcode
+
+    is equivalent to
+    @code
+    T r = oneminus(exp(-rec((x))));
+    @endcode
+
+
+    @see exp, exprecneg
+
+    @return a value of the same type as the (floating) input parameter
+
+**/
   template<typename T> auto exprecnegc(T const& x) {}
 
   namespace functional
   {
     /*!
-      @ingroup group-callable-trigonometry
+      @ingroup group-callable-exponential
+
+
+      Computes the  function: \f$1-e^{-\frac1x}\f$
+
 
       Function object tied to simd::exprecnegc
 
@@ -37,8 +60,7 @@ namespace boost { namespace simd
 } }
 #endif
 
-#include <boost/simd/function/definition/exprecnegc.hpp>
-#include <boost/simd/arch/common/generic/function/exprecnegc.hpp>
+#include <boost/simd/function/scalar/exprecnegc.hpp>
 #include <boost/simd/function/simd/exprecnegc.hpp>
 
 #endif
