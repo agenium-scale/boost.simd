eea55f24e1346fe516105c8a2fb9dc7dde0a6e30
jtlap <jtlapreste@gmail.com>
2015-11-04 15:19:56 +0100

    replacing misplaced files 'detail'

---
+++ include/boost/simd/arch/common/detail/tags.hpp
@@ -0,0 +1,12 @@
+#ifndef BOOST_SIMD_ARCH_COMMON_DETAIL_TAGS_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_DETAIL_TAGS_HPP_INCLUDED
+
+namespace boost { namespace simd { namespace tag
+{
+  typedef boost::mpl::false_ not_simd_type;
+  typedef boost::mpl::true_      simd_type;
+  struct accu_tag{};
+
+} } }
+
+#endif
