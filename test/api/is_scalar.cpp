ea6168b5a629f683df6f256894cf98f037048672
Joel Falcou <joel.falcou@lri.fr>
2015-11-04 22:29:14 +0100

    Implemented is_scalar

---
+++ test/api/is_scalar.cpp
@@ -0,0 +1,26 @@
+//==================================================================================================
+/*
+  Copyright 2009 - 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/sdk/is_scalar.hpp>
+#include <boost/dispatch/adapted/std/array.hpp>
+#include <array>
+#include <simd_test.hpp>
+
+using namespace boost::simd;
+
+STF_CASE_TPL( "Check is_scalar for scalar types", STF_ALL_TYPES )
+{
+  STF_EXPECT( is_scalar<T>::value );
+}
+
+STF_CASE( "Check is_scalar for non-scalar types" )
+{
+  STF_EXPECT_NOT( (is_scalar<std::array<int,9>>::value) );
+}
+
