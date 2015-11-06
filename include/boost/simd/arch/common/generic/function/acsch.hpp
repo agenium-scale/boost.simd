e4d0571328d1560edf1ec2b606781264295e81b7
jtlap <jtlapreste@gmail.com>
2015-11-06 18:03:19 +0100

    acsch

---
+++ include/boost/simd/arch/common/generic/function/acsch.hpp
@@ -0,0 +1,39 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_ACSCH_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_ACSCH_HPP_INCLUDED
+
+#include <boost/simd/function/asinh.hpp>
+#include <boost/simd/function/rec.hpp>
+#include <boost/dispatch/function/overload.hpp>
+#include <boost/config.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( acsch_
+                          , (typename A0)
+                             , bd::cpu_
+                            , bd::generic_< bd::unspecified_<A0> >
+                            )
+  {
+    using result_t = A0;
+    A0 operator() ( A0 const& a0) const
+    {
+      return bs::asinh(bs::rec(a0));
+    }
+  };
+} } }
+
+
+#endif
---
+++ test/function/scalar/acsch.cpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/acsch.hpp>
+#include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+
+STF_CASE_TPL (" acsch",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::acsch;
+  using r_t = decltype(acsch(T()));
+
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(acsch(bs::Inf<T>()), bs::Zero<r_t>(), 0);
+  STF_ULP_EQUAL(acsch(bs::Minf<T>()), bs::Zero<r_t>(), 0);
+  STF_ULP_EQUAL(acsch(bs::Nan<T>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(acsch(-bs::Zero<T>()), bs::Minf<r_t>(), 0);
+  STF_ULP_EQUAL(acsch(bs::Zero<T>()), bs::Inf<r_t>(), 0);
+#endif
+  STF_ULP_EQUAL(acsch(bs::One<T>()), T(0.88137358701954302523260932498), 0.5);
+}
+
+
