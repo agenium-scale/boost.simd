52490de0080434e2e972f1725dac50fbdc26f6f2
jtlap <jtlapreste@gmail.com>
2015-11-06 17:37:57 +0100

    acosh

---
+++ include/boost/simd/arch/common/scalar/function/acosh.hpp
@@ -0,0 +1,47 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_ACOSH_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_ACOSH_HPP_INCLUDED
+
+#include <boost/simd/constant/log_2.hpp>
+#include <boost/simd/constant/oneotwoeps.hpp>
+#include <boost/simd/function/scalar/log.hpp>
+#include <boost/simd/function/scalar/log1p.hpp>
+#include <boost/simd/function/scalar/minusone.hpp>
+#include <boost/simd/function/scalar/sqr.hpp>
+#include <boost/simd/function/scalar/sqrt.hpp>
+#include <boost/dispatch/function/overload.hpp>
+#include <boost/config.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( acosh_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() (A0 a0) const BOOST_NOEXCEPT
+    {
+      A0 t = minusone(a0);
+      if(BOOST_LIKELY(t <= Oneotwoeps<A0>()))
+        return log1p(t+bs::sqrt(t+t+sqr(t)));
+      else
+        return log(t)+Log_2<A0>();
+    }
+  };
+} } }
+
+
+#endif
---
+++ test/function/scalar/acosh.cpp
@@ -0,0 +1,41 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/acosh.hpp>
+#include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+
+STF_CASE_TPL (" acosh",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::acosh;
+  using r_t = decltype(acosh(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(acosh(bs::Inf<T>()), bs::Inf<r_t>(), 0);
+  STF_ULP_EQUAL(acosh(bs::Minf<T>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(acosh(bs::Nan<T>()), bs::Nan<r_t>(), 0);
+#endif
+  STF_ULP_EQUAL(acosh(bs::Zero<T>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(acosh(bs::One<T>()), bs::Zero<r_t>(), 0.5);
+  STF_ULP_EQUAL(acosh(bs::Two<T>()), T(1.31695789692481670862504634731), 0.5);
+}
+
