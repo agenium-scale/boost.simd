7f858e28f74e25332812e4865f2b89f89d98b157
jtlap <jtlapreste@gmail.com>
2015-11-08 10:37:19 +0100

    base kernels for inverse trigonometry

--- include/boost/simd/arch/common/detail/tags.hpp
+++ include/boost/simd/arch/common/detail/tags.hpp
@@ -34,6 +34,7 @@ namespace boost { namespace simd { namespace tag
   struct degree_tag {};
   struct pi_tag     {};
 
+<<<<<<< HEAD
   enum reduction
   {
     r_0_pio4, r_0_pio2, r_0_20pi, r_0_mpi, r_0_dmpi, r_0_inf
@@ -155,6 +156,8 @@ namespace boost { namespace simd { namespace tag
   const clipped_medium_tag clipped_medium_ = {};
 
 
+=======
+>>>>>>> base kernels for inverse trigonometry
 } } }
 
 
