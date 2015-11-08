37c76e23422975ee1e853923cdf3b9ca2d546199
jtlap <jtlapreste@gmail.com>
2015-11-08 10:30:21 +0100

    atand

---
+++ test/function/scalar/atand.cpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/atand.hpp>
+#include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+
+STF_CASE_TPL (" atandreal",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::atand;
+
+  using r_t = decltype(atand(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(atand(bs::Inf<T>()), 90, 0.5);
+  STF_ULP_EQUAL(atand(bs::Minf<T>()), -90, 0.5);
+  STF_ULP_EQUAL(atand(bs::Nan<T>()), bs::Nan<r_t>(), 0.5);
+#endif
+  STF_ULP_EQUAL(atand(bs::Half<T>()), T(2.656505117707799e+01), 0.5);
+  STF_ULP_EQUAL(atand(bs::Mhalf<T>()), T(-2.656505117707799e+01), 0.5);
+  STF_ULP_EQUAL(atand(bs::Mone<T>()), T(-45), 0.5);
+  STF_ULP_EQUAL(atand(bs::One<T>()), T(45), 0.5);
+  STF_ULP_EQUAL(atand(bs::Zero<T>()), bs::Zero<r_t>(), 0.5);
+}
