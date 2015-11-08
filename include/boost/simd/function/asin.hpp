c5c932fd648077a43c3edbd0445584110b19e2ff
jtlap <jtlapreste@gmail.com>
2015-11-08 10:29:01 +0100

    asin

---
+++ include/boost/simd/function/asin.hpp
@@ -0,0 +1,64 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_ASIN_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_ASIN_HPP_INCLUDED
+
+#if defined(DOXYGEN_ONLY)
+namespace boost { namespace simd
+{
+  /*!
+
+    @ingroup group-trigonometric
+
+    inverse sine.
+
+    @par Semantic:
+
+    For every parameter of floating type T
+
+    @code
+    T r = asin(x);
+    @endcode
+
+    Returns the arc @c r in the interval
+    \f$[-\pi/2, \pi/2]\f$ such that <tt>sin(r) == x</tt>.
+    If @c x is outside \f$[-1, 1]\f$ the result is Nan.
+
+    @see asind, asinpi
+
+    @return a value of the same type as the parameter
+
+**/
+  template<typename T> auto asin(T const& x) {}
+
+  namespace functional
+  {
+    /*!
+      @ingroup group-callable-trigonometric
+
+
+      inverse sine.
+
+
+      Function object tied to simd::asin
+
+      @see simd::asin
+    **/
+    const boost::dispatch::functor<tag::asin_> asin = {};
+  }
+} }
+#endif
+
+#include <boost/simd/function/scalar/asin.hpp>
+#include <boost/simd/function/simd/asin.hpp>
+
+#endif
---
+++ test/function/scalar/asin.cpp
@@ -0,0 +1,45 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/asin.hpp>
+#include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+#include <boost/simd/constant/pio_6.hpp>
+#include <boost/simd/constant/pio_2.hpp>
+
+STF_CASE_TPL (" asin",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::asin;
+
+  using r_t = decltype(asin(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(asin(bs::Inf<T>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(asin(bs::Minf<T>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(asin(bs::Nan<T>()), bs::Nan<r_t>(), 0);
+#endif
+  STF_ULP_EQUAL(asin(bs::Half<T>()), bs::Pio_6<r_t>(), 0.5);
+  STF_ULP_EQUAL(asin(bs::Mhalf<T>()), -bs::Pio_6<r_t>(), 0.5);
+  STF_ULP_EQUAL(asin(bs::Mone<T>()), -bs::Pio_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(asin(bs::One<T>()), bs::Pio_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(asin(bs::Zero<T>()), bs::Zero<r_t>(), 0.5);
+}
