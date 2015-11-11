285aefd86b46cb151b0319a906cee674a90ea656
jtlap <jtlapreste@gmail.com>
2015-11-11 07:55:15 +0100

    cospi

---
+++ include/boost/simd/arch/common/generic/function/cospi.hpp
@@ -0,0 +1,81 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_COSPI_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_COSPI_HPP_INCLUDED
+
+#include <boost/simd/arch/common/detail/generic/trigo.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/function/is_odd.hpp>
+#include <boost/simd/sdk/is_not_scalar.hpp>
+#include <boost/dispatch/function/overload.hpp>
+#include <boost/dispatch/meta/as_floating.hpp>
+#include <boost/config.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( cospi_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::generic_< bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0) const BOOST_NOEXCEPT
+    {
+      return detail::trig_base<A0,pi_tag,is_not_scalar_t<A0>,big_tag>::cosa(a0);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( cospi_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::generic_< bd::floating_<A0> >
+                          , bs::fast_tag
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0,  fast_tag const &) const BOOST_NOEXCEPT
+    {
+      return detail::trig_base<A0,pi_tag,is_not_scalar_t<A0>,clipped_pio4_tag>::cosa(a0);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( cospi_
+                          , (typename A0, typename A1)
+                          , bd::cpu_
+                          , bd::generic_< bd::floating_<A0> >
+                          , bd::scalar_ < bd::unspecified_<A1> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0, A1 const&) const BOOST_NOEXCEPT
+    {
+      return detail::trig_base<A0,pi_tag,is_not_scalar_t<A0>,A1>::cosa(a0);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( cospi_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::generic_< bd::arithmetic_<A0> >
+                          )
+  {
+    using result_t = bd::as_floating_t<A0>;
+    BOOST_FORCEINLINE result_t operator() ( A0 const& a0) const BOOST_NOEXCEPT
+    {
+      return bs::if_else(bs::is_odd(a0),Mone<result_t>(),One<result_t>());
+    }
+  };
+} } }
+
+
+#endif
