4cbc32ad1cc0ec11a6b50626800438ebc55341a7
jtlap <jtlapreste@gmail.com>
2015-11-11 08:37:12 +0100

    missng dispatch for all, perhaps any and none to fulfill

--- include/boost/simd/arch/common/scalar/function/all.hpp
+++ include/boost/simd/arch/common/scalar/function/all.hpp
@@ -23,6 +23,18 @@ namespace boost { namespace simd { namespace ext
   BOOST_DISPATCH_OVERLOAD ( all_
                           , (typename A0)
                           , bd::cpu_
+                          , bd::scalar_<logical_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 a0) const BOOST_NOEXCEPT
+    {
+      return a0;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( all_
+                          , (typename A0)
+                          , bd::cpu_
                           , bd::scalar_<bd::arithmetic_<A0> >
                           )
   {
