a2a6c30b436cb3b033089d0eb40d72ff564de084
jtlap <jtlapreste@gmail.com>
2015-11-11 08:24:35 +0100

    sinpi

---
+++ include/boost/simd/function/sinpi.hpp
@@ -0,0 +1,70 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SINPI_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_SINPI_HPP_INCLUDED
+
+#if defined(DOXYGEN_ONLY)
+namespace boost { namespace simd
+{
+  /*!
+
+    @ingroup group-trigonometric
+
+    sine of angle in \f$\pi\f$ multiples.
+
+    @par Semantic:
+
+    For every parameter of floating type T
+
+    @code
+    T r = sinpi(x);
+    @endcode
+
+    is similar to:
+
+    @code
+    T r = sin(Pi<T>()*x);
+    @endcode
+
+    @see sine, fast_sin, sincospi, sin, sind
+
+    @return a value of the same type as the parameter
+
+**/
+  template<typename T> auto sinpi(T const& x) {}
+
+  namespace functional
+  {
+    /*!
+      @ingroup group-callable-trigonometric
+
+
+      sine of angle in \f$\pi\f$ multiples.
+
+
+
+      sine of angle in \f$\pi\f$ multiples.
+
+
+      Function object tied to simd::sinpi
+
+      @see simd::sinpi
+    **/
+    const boost::dispatch::functor<tag::sinpi_> sinpi = {};
+  }
+} }
+#endif
+
+#include <boost/simd/function/scalar/sinpi.hpp>
+#include <boost/simd/function/simd/sinpi.hpp>
+
+#endif
---
+++ test/function/scalar/sinpi.cpp
@@ -0,0 +1,49 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/sinpi.hpp>
+#include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+#include <boost/simd/constant/quarter.hpp>
+#include <boost/simd/constant/sqrt_2.hpp>
+#include <boost/simd/constant/sqrt_2o_2.hpp>
+#include <boost/simd/constant/half.hpp>
+#include <boost/simd/constant/mhalf.hpp>
+
+STF_CASE_TPL (" sinpi",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::sinpi;
+
+  using r_t = decltype(sinpi(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(sinpi(bs::Inf<T>()), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::Minf<T>()), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::Nan<T>()), bs::Nan<r_t>(), 0.5);
+#endif
+  STF_ULP_EQUAL(sinpi(-bs::Quarter<T>()), -bs::Sqrt_2o_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::Half<T>()), bs::One<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::Mhalf<T>()), -bs::One<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::One<T>()), bs::Zero<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::Quarter<T>()), bs::Sqrt_2o_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(sinpi(bs::Zero<T>()), bs::Zero<r_t>(), 0.5);
+}
