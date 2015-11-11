ad172944d6f77175825e753dc4b39f352869847b
jtlap <jtlapreste@gmail.com>
2015-11-11 07:55:58 +0100

    csc

---
+++ include/boost/simd/arch/common/generic/function/csc.hpp
@@ -0,0 +1,65 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_CSC_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_CSC_HPP_INCLUDED
+
+#include <boost/simd/options.hpp>
+#include <boost/simd/function/rec.hpp>
+#include <boost/simd/function/sin.hpp>
+#include <boost/dispatch/function/overload.hpp>
+#include <boost/config.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( csc_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::generic_< bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0) const BOOST_NOEXCEPT
+    {
+      return csc(a0, big_);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( csc_
+                          , (typename A0, typename A1)
+                          , bd::cpu_
+                          , bd::generic_< bd::floating_<A0> >
+                          , bd::scalar_ < bd::unspecified_<A1> >
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0, A1 const&) const BOOST_NOEXCEPT
+    {
+      return rec(sin(a0, A1()));
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( csc_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::generic_< bd::floating_<A0> >
+                          ,  bs::fast_tag
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0, fast_tag const&) const BOOST_NOEXCEPT
+    {
+      return rec(sin(a0, fast_));
+    }
+  };
+} } }
+
+
+#endif
---
+++ test/function/scalar/csc.cpp
@@ -0,0 +1,98 @@
+//==================================================================================================
+/*!
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/csc.hpp>
+#include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
+#include <boost/simd/constant/sqrt_2.hpp>
+#include <boost/simd/constant/pio_2.hpp>
+#include <boost/simd/constant/pio_4.hpp>
+
+
+STF_CASE_TPL (" csc",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::csc;
+
+  using r_t = decltype(csc(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(csc(-bs::Zero<T>()), bs::Minf<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Inf<T>()), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Minf<T>()), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Nan<T>()), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Zero<T>()), bs::Inf<r_t>(), 0.5);
+#endif
+  STF_ULP_EQUAL(csc(-bs::Pio_2<T>()), bs::Mone<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(-bs::Pio_4<T>()), -bs::Sqrt_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Pio_2<T>()), bs::One<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Pio_4<T>()), bs::Sqrt_2<r_t>(), 0.5);
+}
+
+STF_CASE_TPL (" csc fast_",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::csc;
+
+  using r_t = decltype(csc(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(csc(-bs::Zero<T>(), bs::fast_), bs::Minf<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Inf<T>(), bs::fast_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Minf<T>(), bs::fast_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Nan<T>(), bs::fast_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Zero<T>(), bs::fast_), bs::Inf<r_t>(), 0.5);
+#endif
+  STF_ULP_EQUAL(csc(-bs::Pio_2<T>(), bs::fast_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(-bs::Pio_4<T>(), bs::fast_), -bs::Sqrt_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Pio_2<T>(), bs::fast_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Pio_4<T>(), bs::fast_), bs::Sqrt_2<r_t>(), 0.5);
+}
+
+STF_CASE_TPL (" csc clipped_small_",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::csc;
+
+  using r_t = decltype(csc(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(csc(-bs::Zero<T>(), bs::clipped_small_), bs::Minf<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Inf<T>(), bs::clipped_small_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Minf<T>(), bs::clipped_small_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Nan<T>(), bs::clipped_small_), bs::Nan<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Zero<T>(), bs::clipped_small_), bs::Inf<r_t>(), 0.5);
+#endif
+  STF_ULP_EQUAL(csc(-bs::Pio_2<T>(), bs::clipped_small_), bs::Mone<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(-bs::Pio_4<T>(), bs::clipped_small_), -bs::Sqrt_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Pio_2<T>(), bs::clipped_small_), bs::One<r_t>(), 0.5);
+  STF_ULP_EQUAL(csc(bs::Pio_4<T>(), bs::clipped_small_), bs::Sqrt_2<r_t>(), 0.5);
+}
