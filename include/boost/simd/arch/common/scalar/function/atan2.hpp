fa70388efdb6852822a6ec41ffa8b8aad1cbe02a
jtlap <jtlapreste@gmail.com>
2015-11-13 08:18:18 +0100

    test conformity and add std_ and fast_ for atan2

--- include/boost/simd/arch/common/scalar/function/atan2.hpp
+++ include/boost/simd/arch/common/scalar/function/atan2.hpp
@@ -12,6 +12,7 @@
 #ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_ATAN2_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_ATAN2_HPP_INCLUDED
 
+#include <boost/simd/options.hpp>
 #include <boost/simd/constant/nan.hpp>
 #include <boost/simd/constant/one.hpp>
 #include <boost/simd/constant/pi.hpp>
@@ -19,13 +20,13 @@
 #include <boost/simd/function/scalar/copysign.hpp>
 #include <boost/simd/function/scalar/if_else.hpp>
 #include <boost/simd/function/scalar/if_else_zero.hpp>
-#include <boost/simd/function/scalar/is_gtz.hpp>
+#include <boost/simd/function/scalar/is_positive.hpp>
 #include <boost/simd/function/scalar/is_inf.hpp>
-#include <boost/simd/function/scalar/is_ltz.hpp>
+#include <boost/simd/function/scalar/is_negative.hpp>
 #include <boost/simd/function/scalar/is_nan.hpp>
-#include <boost/simd/function/scalar/signnz.hpp>
 #include <boost/dispatch/function/overload.hpp>
 #include <boost/config.hpp>
+#include <cmath>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -51,11 +52,41 @@ namespace boost { namespace simd { namespace ext
         a1 = copysign(One<A0>(), a1);
       }
       #endif
-      A0 z = detail::invtrig_base<A0,tag::radian_tag, tag::not_simd_type>::kernel_atan(a0/a1);
-      z = if_else(is_gtz(a1), z, Pi<A0>()-z)*signnz(a0);
-      return if_else(is_eqz(a0), if_else_zero(is_ltz(a1), Pi<A0>()), z);
+      A0 z = detail::invtrig_base<A0,radian_tag, tag::not_simd_type>::kernel_atan(a0/a1);
+      z = copysign(if_else(is_positive(a1), z, Pi<A0>()-z), a0);
+      return if_else(is_eqz(a0), if_else_zero(is_negative(a1), copysign(Pi<A0>(), a0)), z);
     }
   };
+
+  BOOST_DISPATCH_OVERLOAD ( atan2_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bs::fast_tag
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 a0, A0 a1, fast_tag const&) const BOOST_NOEXCEPT
+    {
+      A0 z = detail::invtrig_base<A0,radian_tag, tag::not_simd_type>::kernel_atan(a0/a1);
+      return copysign(if_else(is_positive(a1), z, Pi<A0>()-z), a0);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( atan2_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bs::std_tag
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 y, A0 x, std_tag const&) const BOOST_NOEXCEPT
+    {
+      return std::atan2(y, x);
+    }
+  };
+
 } } }
 
 
