c901e5408e64801e4e07626e8268bdd48482c7e9
jtlap <jtlapreste@gmail.com>
2015-11-13 15:18:27 +0100

    adding is_normal

---
+++ include/boost/simd/function/is_normal.hpp
@@ -0,0 +1,68 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_IS_NORMAL_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_IS_NORMAL_HPP_INCLUDED
+
+#if defined(DOXYGEN_ONLY)
+namespace boost { namespace simd
+{
+  /*!
+
+    @ingroup group-predicates
+
+    Returns @ref True or @ref False according x is normal or not.
+
+    @par Semantic:
+
+    For x of type T
+
+    @code
+    as_logical_t<T> r = is_normal(x);
+    @endcode
+
+    is similar to:
+
+    @code
+    as_logical_t<T> r = !(is_denormal(x) || is_invalid(x));
+    @endcode
+
+    @see is_invalid,  is_denormal
+
+
+    @return a logical value
+
+**/
+  template<typename T> auto is_normal(T const& x) {}
+
+  namespace functional
+  {
+    /*!
+      @ingroup group-callable-predicates
+
+      Returns @ref True or @ref False according x is normal or not.
+
+
+      Function object tied to simd::is_normal
+
+      @see simd::is_normal
+    **/
+    const boost::dispatch::functor<tag::is_normal_> is_normal = {};
+  }
+} }
+#endif
+
+#include <boost/simd/function/definition/is_normal.hpp>
+#include <boost/simd/arch/common/scalar/function/is_normal.hpp>
+#include <boost/simd/arch/common/generic/function/is_normal.hpp>
+#include <boost/simd/function/simd/is_normal.hpp>
+
+#endif
