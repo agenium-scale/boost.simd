31532fe9b5c6f4be722a24a9153694ee1938e5e0
jtlap <jtlapreste@gmail.com>
2015-11-13 10:06:35 +0100

    uneeded includes in scalar file for log

--- include/boost/simd/arch/common/scalar/function/log.hpp
+++ include/boost/simd/arch/common/scalar/function/log.hpp
@@ -12,13 +12,7 @@
 #ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_LOG_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_LOG_HPP_INCLUDED
 
-#include <boost/simd/arch/common/detail/scalar/logarithm.hpp>
-#include <boost/simd/detail/assert_utils.hpp>
-#include <boost/simd/function/is_not_nan.hpp>
-#include <boost/simd/function/is_positive.hpp>
-#include <boost/simd/function/logical_and.hpp>
 #include <boost/simd/options.hpp>
-#include <boost/simd/sdk/is_not_scalar.hpp>
 #include <boost/dispatch/function/overload.hpp>
 #include <boost/assert.hpp>
 #include <boost/config.hpp>
