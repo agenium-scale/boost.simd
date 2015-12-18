0cadd386e87429470e14b093cec3a8331f2ec9c0
jtlap <jtlapreste@gmail.com>
2015-12-18 11:49:40 +0100

    problems after merge

--- include/boost/simd/arch/common/generic/function/asec.hpp
+++ include/boost/simd/arch/common/generic/function/asec.hpp
@@ -9,28 +9,27 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_ASECD_HPP_INCLUDED
-#define BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_ASECD_HPP_INCLUDED
+#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_ASEC_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_FUNCTION_GENERIC_ASEC_HPP_INCLUDED
 
 #include <boost/config.hpp>
-#include <boost/simd/function/simd/asec.hpp>
-#include <boost/simd/function/simd/indeg.hpp>
+#include <boost/simd/function/acos.hpp>
+#include <boost/simd/function/rec.hpp>
 #include <boost/dispatch/function/overload.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
   namespace bd = boost::dispatch;
   namespace bs = boost::simd;
-  BOOST_DISPATCH_OVERLOAD ( asecd_
+  BOOST_DISPATCH_OVERLOAD ( asec_
                           , (typename A0)
-                             , bd::cpu_
-                            , bd::generic_< bd::floating_<A0> >
-                            )
+                          , bd::cpu_
+                          , bd::generic_< bd::unspecified_<A0> >
+                          )
   {
-    using result_t = A0;
-    A0 operator() ( A0 const& a0) const
+    BOOST_FORCEINLINE A0 operator() ( A0 const& a0) const BOOST_NOEXCEPT
     {
-      return bs::indeg(bs::asec(a0));
+      return bs::acos(bs::rec(a0));
     }
   };
 } } }
