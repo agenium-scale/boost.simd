0cf96ba203392d7ebfa58a1fdd0df9c3ed3bf10b
Charly Chevalier <charly.chevalier@numscale.com>
2015-12-23 17:21:53 +0100

    Remove obsolete boost::dispatch::cardinal_of

--- include/boost/simd/arch/common/generic/function/rol.hpp
+++ include/boost/simd/arch/common/generic/function/rol.hpp
@@ -28,16 +28,18 @@
 namespace boost { namespace simd { namespace ext
 {
   namespace bd = boost::dispatch;
- BOOST_DISPATCH_OVERLOAD ( rol_
+  namespace bs = boost::simd;
+
+  BOOST_DISPATCH_OVERLOAD ( rol_
                           , (typename A0, typename A1)
-                         , bd::cpu_
-                         , bd::generic_< bd::integer_<A0> >
-                         , bd::generic_< bd::integer_<A1> >
- )
+                          , bd::cpu_
+                          , bd::generic_< bd::integer_<A0> >
+                          , bd::generic_< bd::integer_<A1> >
+                          )
   {
     BOOST_FORCEINLINE A0 operator() ( A0 const& a0, A1 const& a1
-                                    , typename std::enable_if<bd::cardinal_of<A1>::value
-                                     == bd::cardinal_of<A0>::value>::type* = 0
+                                    , typename std::enable_if<bs::cardinal_of<A1>::value
+                                     == bs::cardinal_of<A0>::value>::type* = 0
       ) const
     {
       using s_t = bd::scalar_of_t<A0>;
@@ -56,8 +58,8 @@ namespace boost { namespace simd { namespace ext
   )
   {
     BOOST_FORCEINLINE A0 operator() ( A0 const& a0, A1 const& a1
-                                    , typename std::enable_if<bd::cardinal_of<A1>::value
-                                     == bd::cardinal_of<A0>::value>::type* = 0
+                                    , typename std::enable_if<bs::cardinal_of<A1>::value
+                                     == bs::cardinal_of<A0>::value>::type* = 0
                                     ) const
     {
       using i_t = bd::as_integer_t<A0, unsigned>;
