b155dbb7336bb8518c271d9df79ec08f8e90d57b
Charly Chevalier <charly.chevalier@numscale.com>
2015-12-31 18:43:39 +0100

    Sample hub header

---
+++ include/boost/simd/operator.hpp
@@ -0,0 +1,51 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_SIMD_OPERATOR_HPP_INCLUDED
+#define BOOST_SIMD_OPERATOR_HPP_INCLUDED
+
+namespace boost { namespace simd
+{
+  /*!
+    @ingroup group-functions
+    @defgroup group-operator Native C++ operators
+
+    Those functions provides scalar and SIMD version of the native C and C++ operators.
+  **/
+} }
+
+#include <boost/simd/function/bitwise_and.hpp>
+#include <boost/simd/function/bitwise_not.hpp>
+#include <boost/simd/function/bitwise_or.hpp>
+#include <boost/simd/function/bitwise_xor.hpp>
+#include <boost/simd/function/comma.hpp>
+#include <boost/simd/function/complement.hpp>
+#include <boost/simd/function/divides.hpp>
+#include <boost/simd/function/if_else.hpp>
+#include <boost/simd/function/is_equal.hpp>
+#include <boost/simd/function/is_greater.hpp>
+#include <boost/simd/function/is_greater_equal.hpp>
+#include <boost/simd/function/is_less.hpp>
+#include <boost/simd/function/is_less_equal.hpp>
+#include <boost/simd/function/is_not_equal.hpp>
+#include <boost/simd/function/logical_and.hpp>
+#include <boost/simd/function/logical_not.hpp>
+#include <boost/simd/function/logical_or.hpp>
+#include <boost/simd/function/minus.hpp>
+#include <boost/simd/function/modulo.hpp>
+#include <boost/simd/function/multiplies.hpp>
+#include <boost/simd/function/plus.hpp>
+#include <boost/simd/function/shift_left.hpp>
+#include <boost/simd/function/shift_right.hpp>
+#include <boost/simd/function/unary_minus.hpp>
+#include <boost/simd/function/unary_plus.hpp>
+
+#endif
