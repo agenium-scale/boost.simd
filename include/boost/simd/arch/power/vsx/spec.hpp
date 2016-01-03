a7807e4771bafdde8e751f4fd2cf6a15a360fcb8
jtlap <jtlapreste@gmail.com>
2016-01-03 10:35:47 +0100

    correcting remaining wrong headers in hpp files

--- include/boost/simd/arch/power/vsx/spec.hpp
+++ include/boost/simd/arch/power/vsx/spec.hpp
@@ -4,7 +4,6 @@
 
   Defines the PPC VSX specifications
 
-  @copyright 2015 LRI UMR 8623 CNRS/Univ Paris Sud XI
   @copyright 2015 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
