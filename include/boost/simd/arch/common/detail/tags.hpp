26819d5064549521cf5b6294013ba99b13225302
Charly Chevalier <charly.chevalier@numscale.com>
2016-01-03 20:25:00 +0100

    Licence-less file

--- include/boost/simd/arch/common/detail/tags.hpp
+++ include/boost/simd/arch/common/detail/tags.hpp
@@ -1,3 +1,16 @@
+//==================================================================================================
+/*!
+  @file
+
+  Useful tags forward declarations and aliases
+
+  @copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
 #ifndef BOOST_SIMD_ARCH_COMMON_DETAIL_TAGS_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_DETAIL_TAGS_HPP_INCLUDED
 
@@ -9,6 +22,7 @@ namespace boost { namespace simd { namespace tag
   struct log_;
   struct log2_;
   struct log10_;
+
   using  not_simd_type = std::integral_constant<bool, false>;
   using  simd_type = std::integral_constant<bool, true>;
 } } }
