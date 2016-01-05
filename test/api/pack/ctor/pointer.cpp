8be7b316d5e3cba66dcf984d956d7fa72f14dc07
Charly Chevalier <charly.chevalier@numscale.com>
2016-01-05 14:25:01 +0100

    Add test for pack and related operations

---
+++ test/api/pack/ctor/pointer.cpp
@@ -0,0 +1,38 @@
+//==================================================================================================
+/*
+  Copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+
+#include <boost/simd/pack.hpp>
+#include <boost/align/aligned_alloc.hpp>
+#include <numeric>
+#include <simd_test.hpp>
+
+template <typename T, std::size_t N, typename Env>
+void test(Env& $)
+{
+  namespace ba = boost::alignment;
+  using pack_t = boost::simd::pack<T, N>;
+  std::size_t alg = sizeof(typename pack_t::storage_type);
+
+  T* ptr = static_cast<T*>(ba::aligned_alloc(alg, (sizeof(T)) * N));
+  std::iota(ptr, ptr + N, T{1});
+
+  pack_t p(ptr);
+  STF_EXPECT( std::equal(p.begin(),p.end(),ptr) );
+
+  ba::aligned_free(ptr);
+}
+
+STF_CASE_TPL("Check that pack constructs from aligned pointer" , STF_NUMERIC_TYPES)
+{
+  test<T,  2>($);
+  test<T,  4>($);
+  test<T,  8>($);
+  test<T, 16>($);
+  test<T, 32>($);
+}
