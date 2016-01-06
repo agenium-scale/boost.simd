ccf1b86f045e85019a57c416e222c1f0dd1d5b59
jtlap <jtlapreste@gmail.com>
2016-01-06 13:54:20 +0100

    adding std_tag call to more functions pow, ld/frexp

--- include/boost/simd/arch/common/scalar/function/pow.hpp
+++ include/boost/simd/arch/common/scalar/function/pow.hpp
@@ -12,13 +12,18 @@
 #ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_POW_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_POW_HPP_INCLUDED
 
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/one.hpp>
 #include <boost/simd/constant/nan.hpp>
 #include <boost/simd/function/scalar/is_flint.hpp>
+#include <boost/simd/function/scalar/is_inf.hpp>
 #include <boost/simd/function/scalar/is_ltz.hpp>
+#include <boost/simd/function/scalar/is_negative.hpp>
 #include <boost/simd/function/scalar/is_odd.hpp>
 #include <boost/simd/function/scalar/pow_abs.hpp>
 #include <boost/dispatch/function/overload.hpp>
 #include <boost/config.hpp>
+#include <cmath>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -34,12 +39,30 @@ namespace boost { namespace simd { namespace ext
     BOOST_FORCEINLINE A0 operator() ( A0 a0, A0 a1) const BOOST_NOEXCEPT
     {
       bool ltza0 = is_ltz(a0);
-      if( (ltza0 && !is_flint(a1)) ) return Nan<A0>();
+      auto isinfa1 = is_inf(a1);
+      if( a0 == Mone<A0>() && isinfa1 ) return One<A0>();
+      if( ltza0 && !is_flint(a1) && !is_inf(a1) ) return Nan<A0>();
       A0 z = pow_abs(a0, a1);
-      return  (ltza0 && is_odd(a1)) ? -z : z;
+      if (isinfa1) return z;
+      return  (is_negative(a0) && is_odd(a1)) ? -z : z;
+    }
+  };
+
+   BOOST_DISPATCH_OVERLOAD ( pow_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bs::std_tag
+                          )
+  {
+    BOOST_FORCEINLINE A0 operator() ( A0 a0, A0 a1, std_tag const&) const BOOST_NOEXCEPT
+    {
+      return std::pow(a0, a1);
     }
   };
 } } }
 
 
 #endif
+
