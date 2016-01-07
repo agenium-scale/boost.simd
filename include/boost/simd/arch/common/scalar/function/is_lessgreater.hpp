c2f5f7e5d291bfa3848d7945d10e03eead9cbbb3
jtlap <jtlapreste@gmail.com>
2016-01-07 15:57:02 +0100

    is_lessgreater (yes it is part of libstdc++ )

---
+++ include/boost/simd/arch/common/scalar/function/is_lessgreater.hpp
@@ -0,0 +1,81 @@
+//==================================================================================================
+/*!
+  @file
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_IS_LESSGREATER_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_SCALAR_FUNCTION_IS_LESSGREATER_HPP_INCLUDED
+
+#include <boost/config.hpp>
+#include <boost/simd/function/scalar/is_ord.hpp>
+#include <boost/simd/options.hpp>
+#include <boost/simd/logical.hpp>
+#include <boost/dispatch/function/overload.hpp>
+#include <cmath>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( is_lessgreater_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::bool_<A0> >
+                          , bd::scalar_< bd::bool_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE bool operator() ( A0 a0 , A0 a1 ) const BOOST_NOEXCEPT
+    {
+      return  a0!=a1;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( is_lessgreater_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::arithmetic_<A0> >
+                          , bd::scalar_< bd::arithmetic_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE logical<A0> operator() ( A0 a0 , A0 a1 ) const BOOST_NOEXCEPT
+    {
+      return a0!=a1;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( is_lessgreater_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bd::scalar_< bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE logical<A0> operator() ( A0 a0, A0 a1) const BOOST_NOEXCEPT
+    {
+      return is_ord(a0,a1)&&(a0!=a1);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( is_lessgreater_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bs::std_tag
+                          )
+  {
+    BOOST_FORCEINLINE logical<A0> operator() ( A0 a0, A0 a1
+                                             , std_tag const &) const BOOST_NOEXCEPT
+    {
+      return std::islessgreater(a0, a1);
+    }
+  };
+} } }
+
+
+#endif
