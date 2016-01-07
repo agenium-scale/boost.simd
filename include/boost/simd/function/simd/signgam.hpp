ac9437bf08cd43e8548050affb1b3185644fe519
jtlap <jtlapreste@gmail.com>
2016-01-07 14:35:09 +0100

    signgam computes the sign of the gamma function without computing gamma

---
+++ include/boost/simd/function/simd/signgam.hpp
@@ -0,0 +1,21 @@
+//==================================================================================================
+/*!
+  @file
+
+  Copyright 2015 NumScale SAS
+  Copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SIMD_SIGNGAM_INCLUDED
+#define BOOST_SIMD_FUNCTION_SIMD_SIGNGAM_INCLUDED
+
+#include <boost/simd/function/scalar/signgam.hpp>
+//#include <boost/simd/arch/common/function/simd/signgam.hpp>
+
+#endif
+
+
+
