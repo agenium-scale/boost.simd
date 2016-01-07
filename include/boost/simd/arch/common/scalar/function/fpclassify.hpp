02630043df1e2fefabbba0a91ba82845f5b3b0b0
jtlap <jtlapreste@gmail.com>
2016-01-07 16:32:30 +0100

    adding fpclassify

---
+++ include/boost/simd/arch/common/scalar/function/fpclassify.hpp
@@ -0,0 +1,51 @@
+//==================================================================================================
+/*!
+  @file
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_FPCLASSIFY_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_FUNCTION_SCALAR_FPCLASSIFY_HPP_INCLUDED
+
+#include <boost/dispatch/function/overload.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
+#include <boost/simd/options.hpp>
+#include <boost/config.hpp>
+#include <cmath>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+  BOOST_DISPATCH_OVERLOAD ( fpclassify_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE bd::as_integer_t<A0> operator() (A0 a0) const BOOST_NOEXCEPT
+    {
+      return std::fpclassify(a0);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( fpclassify_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bd::scalar_< bd::floating_<A0> >
+                          , bs::std_tag
+                          )
+  {
+    BOOST_FORCEINLINE int operator() (A0 a0, std_tag const&) const BOOST_NOEXCEPT
+    {
+      return std::fpclassify(a0);
+    }
+  };
+} } }
+
+
+#endif
