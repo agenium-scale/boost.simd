a09b28182a51d35c13ac2116a4284a63623c0067
jtlap <jtlapreste@gmail.com>
2016-01-15 16:59:12 +0100

    adding variadic horn

---
+++ include/boost/simd/function/simd/horn.hpp
@@ -0,0 +1,17 @@
+//==================================================================================================
+/*!
+  @file
+
+  Copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SIMD_HORN_INCLUDED
+#define BOOST_SIMD_FUNCTION_SIMD_HORN_INCLUDED
+
+#include <boost/simd/function/scalar/horn.hpp>
+
+#endif
+
