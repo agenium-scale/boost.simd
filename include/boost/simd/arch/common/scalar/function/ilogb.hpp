9791cf1cb6bc7af1d88f8417e66bd843d40d1fb0
Charly Chevalier <charly.chevalier@numscale.com>
2016-03-15 22:35:20 +0100

    Fix MSVC conversion warning in ilogb

--- include/boost/simd/arch/common/scalar/function/ilogb.hpp
+++ include/boost/simd/arch/common/scalar/function/ilogb.hpp
@@ -34,7 +34,7 @@ namespace boost { namespace simd { namespace ext
   {
     BOOST_FORCEINLINE A0 operator() ( A0 a0 ) const BOOST_NOEXCEPT
     {
-      return bs::ilogb(static_cast<bd::as_floating_t<A0>>(a0));
+      return static_cast<A0>(bs::ilogb(static_cast<bd::as_floating_t<A0>>(a0)));
     }
   };
 
