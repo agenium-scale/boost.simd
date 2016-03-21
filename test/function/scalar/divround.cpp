98f286e49be5d96151b7fc095e7667e2e5fcc8c9
jtlap <jtlapreste@gmail.com>
2016-03-21 16:29:09 +0100

    adding lacking tests for div and idiv options

--- test/function/scalar/divround.cpp
+++ test/function/scalar/divround.cpp
@@ -7,7 +7,7 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#include <boost/simd/function/divround.hpp>
+#include <boost/simd/function/div.hpp>
 #include <simd_test.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 #include <boost/simd/constant/inf.hpp>
@@ -18,89 +18,80 @@
 #include <boost/simd/constant/zero.hpp>
 #include <boost/simd/constant/two.hpp>
 
-
-STF_CASE_TPL (" divroundreal",  STF_IEEE_TYPES)
+STF_CASE_TPL (" divreal",  STF_IEEE_TYPES)
 {
   namespace bs = boost::simd;
-  using bs::divround;
-  using r_t = decltype(divround(T(), T()));
+  using bs::div;
+  using r_t = decltype(div(T(), T()));
 
-  // return type conformity test
-  STF_TYPE_IS(r_t, T);
-
-  // specific values tests
-#ifndef STF_NO_INVALIDS
-  STF_IEEE_EQUAL(divround(bs::Inf<T>(), bs::Inf<T>()), bs::Nan<r_t>());
-  STF_IEEE_EQUAL(divround(bs::Minf<T>(), bs::Minf<T>()), bs::Nan<r_t>());
-  STF_IEEE_EQUAL(divround(bs::Nan<T>(), bs::Nan<T>()), bs::Nan<r_t>());
+  STF_IEEE_EQUAL(div(bs::Inf<T>(), bs::Inf<T>(), bs::round), bs::Nan<r_t>());
+  STF_IEEE_EQUAL(div(bs::Minf<T>(), bs::Minf<T>(), bs::round), bs::Nan<r_t>());
+  STF_IEEE_EQUAL(div(bs::Nan<T>(), bs::Nan<T>(), bs::round), bs::Nan<r_t>());
 #endif
-  STF_EQUAL(divround(T(4),T(0)), bs::Inf<r_t>());
-  STF_EQUAL(divround(T(4),T(3)), bs::One<r_t>());
-  STF_EQUAL(divround(bs::Mone<T>(), bs::Mone<T>()), bs::One<r_t>());
-  STF_EQUAL(divround(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
-  STF_EQUAL(divround(bs::Mone<T>(),bs::Zero<T>()), bs::Minf<r_t>());
-  STF_EQUAL(divround(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
-  STF_EQUAL(divround(bs::One<T>(),bs::Zero<T>()), bs::Inf<r_t>());
-  STF_IEEE_EQUAL(divround(bs::Zero<T>(),bs::Zero<T>()), bs::Nan<r_t>());
+  STF_EQUAL(div(T(4),T(0), bs::round), bs::Inf<r_t>());
+  STF_EQUAL(div(T(4),T(3), bs::round), bs::One<r_t>());
+  STF_EQUAL(div(bs::Mone<T>(), bs::Mone<T>(), bs::round), bs::One<r_t>());
+  STF_EQUAL(div(bs::One<T>(), bs::One<T>(), bs::round), bs::One<r_t>());
+  STF_EQUAL(div(bs::Mone<T>(),bs::Zero<T>(), bs::round), bs::Minf<r_t>());
+  STF_EQUAL(div(bs::One<T>(), bs::One<T>(), bs::round), bs::One<r_t>());
+  STF_EQUAL(div(bs::One<T>(),bs::Zero<T>(), bs::round), bs::Inf<r_t>());
+  STF_IEEE_EQUAL(div(bs::Zero<T>(),bs::Zero<T>(), bs::round), bs::Nan<r_t>());
 } // end of test for floating_
 
-STF_CASE_TPL (" divroundunsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
+STF_CASE_TPL (" divunsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
 {
   namespace bs = boost::simd;
-  using bs::divround;
-  using r_t = decltype(divround(T(), T()));
+  using bs::div;
+  using r_t = decltype(div(T(), T()));
 
-  STF_TYPE_IS(r_t, T);
-
-  // specific values tests
-  STF_EQUAL(divround(T(4),T(0)), bs::Valmax<r_t>());
-  STF_EQUAL(divround(T(4),T(3)), T(1));
-  STF_EQUAL(divround(T(6),T(4)), T(2));
-  STF_EQUAL(divround(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
-  STF_EQUAL(divround(bs::Valmax<T>(),  bs::Two<T>()), bs::Valmax<r_t>()/bs::Two<T>()+bs::One<r_t>());
+  STF_EQUAL(div(T(4),T(0), bs::round), bs::Valmax<r_t>());
+  STF_EQUAL(div(T(4),T(3), bs::round), T(1));
+  STF_EQUAL(div(T(6),T(4), bs::round), T(2));
+  STF_EQUAL(div(bs::One<T>(), bs::One<T>(), bs::round), bs::One<r_t>());
+  STF_EQUAL(div(bs::Valmax<T>(),  bs::Two<T>(), bs::round), bs::Valmax<r_t>()/bs::Two<T>()+bs::One<r_t>());
 } // end of test for unsigned_int_
 
-STF_CASE_TPL (" divroundsigned_int",  STF_SIGNED_INTEGRAL_TYPES)
+STF_CASE_TPL (" divsigned_int",  STF_SIGNED_INTEGRAL_TYPES)
 {
   namespace bs = boost::simd;
 
-  using bs::divround;
-  using r_t = decltype(divround(T(), T()));
+  using bs::div;
+  using r_t = decltype(div(T(), T()));
   STF_TYPE_IS(r_t,T);
 
   // specific values tests
-  STF_EQUAL(divround(T(-4),T(0)), bs::Valmin<r_t>());
-  STF_EQUAL(divround(T(4),T(0)), bs::Valmax<r_t>());
-  STF_EQUAL(divround(T(4),T(3)), T(1));
-  STF_EQUAL(divround(T(-4),T(-3)), T(1));
-  STF_EQUAL(divround(T(4),T(-3)), T(-1));
-  STF_EQUAL(divround(T(-4),T(3)), T(-1));
-  STF_EQUAL(divround(T(5),T(3)), T(2));
-  STF_EQUAL(divround(T(-5),T(-3)), T(2));
-  STF_EQUAL(divround(T(5),T(-3)), T(-2));
-  STF_EQUAL(divround(T(-5),T(3)), T(-2));
+  STF_EQUAL(div(T(-4),T(0), bs::round), bs::Valmin<r_t>());
+  STF_EQUAL(div(T(4),T(0), bs::round), bs::Valmax<r_t>());
+  STF_EQUAL(div(T(4),T(3), bs::round), T(1));
+  STF_EQUAL(div(T(-4),T(-3), bs::round), T(1));
+  STF_EQUAL(div(T(4),T(-3), bs::round), T(-1));
+  STF_EQUAL(div(T(-4),T(3), bs::round), T(-1));
+  STF_EQUAL(div(T(5),T(3), bs::round), T(2));
+  STF_EQUAL(div(T(-5),T(-3), bs::round), T(2));
+  STF_EQUAL(div(T(5),T(-3), bs::round), T(-2));
+  STF_EQUAL(div(T(-5),T(3), bs::round), T(-2));
 
-  STF_EQUAL(divround(T(5),T(4)), T(1));
-  STF_EQUAL(divround(T(-5),T(-4)), T(1));
-  STF_EQUAL(divround(T(5),T(-4)), T(-1));
-  STF_EQUAL(divround(T(-5),T(4)), T(-1));
-  STF_EQUAL(divround(T(6),T(4)), T(2));
-  STF_EQUAL(divround(T(-6),T(-4)), T(2));
-  STF_EQUAL(divround(T(6),T(-4)), T(-2));
-  STF_EQUAL(divround(T(-6),T(4)), T(-2));
-  STF_EQUAL(divround(T(8),T(4)), T(2));
-  STF_EQUAL(divround(T(-8),T(-4)), T(2));
-  STF_EQUAL(divround(T(8),T(-4)), T(-2));
-  STF_EQUAL(divround(T(-8),T(4)), T(-2));
-  STF_EQUAL(divround(T(9),T(4)), T(2));
-  STF_EQUAL(divround(T(-9),T(-4)), T(2));
-  STF_EQUAL(divround(T(9),T(-4)), T(-2));
-  STF_EQUAL(divround(T(-9),T(4)), T(-2));
-  STF_EQUAL(divround(T(10),T(4)), T(3));
-  STF_EQUAL(divround(T(-10),T(-4)), T(3));
-  STF_EQUAL(divround(T(10),T(-4)), T(-3));
-  STF_EQUAL(divround(T(-10),T(4)), T(-3));
+  STF_EQUAL(div(T(5),T(4), bs::round), T(1));
+  STF_EQUAL(div(T(-5),T(-4), bs::round), T(1));
+  STF_EQUAL(div(T(5),T(-4), bs::round), T(-1));
+  STF_EQUAL(div(T(-5),T(4), bs::round), T(-1));
+  STF_EQUAL(div(T(6),T(4), bs::round), T(2));
+  STF_EQUAL(div(T(-6),T(-4), bs::round), T(2));
+  STF_EQUAL(div(T(6),T(-4), bs::round), T(-2));
+  STF_EQUAL(div(T(-6),T(4), bs::round), T(-2));
+  STF_EQUAL(div(T(8),T(4), bs::round), T(2));
+  STF_EQUAL(div(T(-8),T(-4), bs::round), T(2));
+  STF_EQUAL(div(T(8),T(-4), bs::round), T(-2));
+  STF_EQUAL(div(T(-8),T(4), bs::round), T(-2));
+  STF_EQUAL(div(T(9),T(4), bs::round), T(2));
+  STF_EQUAL(div(T(-9),T(-4), bs::round), T(2));
+  STF_EQUAL(div(T(9),T(-4), bs::round), T(-2));
+  STF_EQUAL(div(T(-9),T(4), bs::round), T(-2));
+  STF_EQUAL(div(T(10),T(4), bs::round), T(3));
+  STF_EQUAL(div(T(-10),T(-4), bs::round), T(3));
+  STF_EQUAL(div(T(10),T(-4), bs::round), T(-3));
+  STF_EQUAL(div(T(-10),T(4), bs::round), T(-3));
 
-  STF_EQUAL(divround(bs::Mone<T>(), bs::Mone<T>()), bs::One<r_t>());
-  STF_EQUAL(divround(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
+  STF_EQUAL(div(bs::Mone<T>(), bs::Mone<T>(), bs::round), bs::One<r_t>());
+  STF_EQUAL(div(bs::One<T>(), bs::One<T>(), bs::round), bs::One<r_t>());
 } // end of test for signed_int_
--- test/function/scalar/idivceil.cpp
+++ test/function/scalar/idivceil.cpp
@@ -7,7 +7,7 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#include <boost/simd/function/idivceil.hpp>
+#include <boost/simd/function/idiv.hpp>
 #include <simd_test.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 #include <boost/simd/constant/inf.hpp>
@@ -20,72 +20,61 @@
 #include <boost/simd/constant/valmin.hpp>
 #include <boost/simd/constant/mzero.hpp>
 
-STF_CASE_TPL (" idivceil real",  STF_IEEE_TYPES)
+
+STF_CASE_TPL (" idiv real",  STF_IEEE_TYPES)
 {
   namespace bs = boost::simd;
   namespace bd = boost::dispatch;
 
-  using bs::idivceil;
-  using r_t = decltype(idivceil(T(), T()));
+  using bs::idiv;
+  using r_t = decltype(idiv(T(), T()));
 
-  // return type conformity test
-  STF_TYPE_IS(r_t, bd::as_integer_t<T>);
-
-  // specific values tests
-#ifndef STF_NO_INVALIDS
-  STF_EQUAL(idivceil(bs::Inf<T>(), bs::Inf<T>()), bs::Zero<r_t>());
-  STF_EQUAL(idivceil(bs::Minf<T>(), bs::Minf<T>()), bs::Zero<r_t>());
-  STF_IEEE_EQUAL(idivceil(bs::Nan<T>(), bs::Nan<T>()), bs::Zero<r_t>());
+  STF_EQUAL(idiv(bs::Inf<T>(), bs::Inf<T>(), bs::ceil), bs::Zero<r_t>());
+  STF_EQUAL(idiv(bs::Minf<T>(), bs::Minf<T>(), bs::ceil), bs::Zero<r_t>());
+  STF_IEEE_EQUAL(idiv(bs::Nan<T>(), bs::Nan<T>(), bs::ceil), bs::Zero<r_t>());
 #endif
- STF_EQUAL(idivceil(bs::Mone<T>(), bs::Mone<T>()), bs::One<r_t>());
-  STF_EQUAL(idivceil(bs::Mone<T>(),bs::Zero<T>()), bs::Minf<r_t>());
-  STF_EQUAL(idivceil(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
-  STF_EQUAL(idivceil(bs::One<T>(),bs::Zero<T>()), bs::Valmax<r_t>());
-  STF_EQUAL(idivceil(bs::One<T>(),bs::Mzero<T>()), bs::Valmin<r_t>());
-  STF_EQUAL(idivceil(bs::Zero<T>(),bs::Zero<T>()), bs::Zero<r_t>());
-  STF_EQUAL(idivceil(T(4),T(3)), r_t(2));
-  STF_EQUAL(idivceil(T(-4),T(-3)), r_t(2));
-  STF_EQUAL(idivceil(T(-4),T(3)), r_t(-1));
-  STF_EQUAL(idivceil(T(4),T(-3)), r_t(-1));
+ STF_EQUAL(idiv(bs::Mone<T>(), bs::Mone<T>(), bs::ceil), bs::One<r_t>());
+  STF_EQUAL(idiv(bs::Mone<T>(),bs::Zero<T>(), bs::ceil), bs::Minf<r_t>());
+  STF_EQUAL(idiv(bs::One<T>(), bs::One<T>(), bs::ceil), bs::One<r_t>());
+  STF_EQUAL(idiv(bs::One<T>(),bs::Zero<T>(), bs::ceil), bs::Valmax<r_t>());
+  STF_EQUAL(idiv(bs::One<T>(),bs::Mzero<T>(), bs::ceil), bs::Valmin<r_t>());
+  STF_EQUAL(idiv(bs::Zero<T>(),bs::Zero<T>(), bs::ceil), bs::Zero<r_t>());
+  STF_EQUAL(idiv(T(4),T(3)  , bs::ceil), r_t(2) );
+  STF_EQUAL(idiv(T(-4),T(-3), bs::ceil), r_t(2) );
+  STF_EQUAL(idiv(T(-4),T(3) , bs::ceil), r_t(-1));
+  STF_EQUAL(idiv(T(4),T(-3) , bs::ceil), r_t(-1));
 } // end of test for floating_
 
-STF_CASE_TPL (" idivceil unsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
+STF_CASE_TPL (" idiv unsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
 {
   namespace bs = boost::simd;
   namespace bd = boost::dispatch;
 
-  using bs::idivceil;
-  using r_t = decltype(idivceil(T(), T()));
+  using bs::idiv;
+  using r_t = decltype(idiv(T(), T()));
 
-  // return type conformity test
-  STF_TYPE_IS(r_t, (bd::as_integer_t<T, unsigned>));
-
-  // specific values tests
-  STF_EQUAL(idivceil(T(4),T(3)), T(2));
-  STF_EQUAL(idivceil(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
-  STF_EQUAL(idivceil(bs::One<T>(), bs::Zero<T>()), bs::Valmax<r_t>());
+  STF_EQUAL(idiv(T(4),T(3), bs::ceil), T(2));
+  STF_EQUAL(idiv(bs::One<T>(), bs::One<T>(), bs::ceil), bs::One<r_t>());
+  STF_EQUAL(idiv(bs::One<T>(), bs::Zero<T>(), bs::ceil), bs::Valmax<r_t>());
 } // end of test for unsigned_int_
 
-STF_CASE_TPL (" idivceil signed_int",  STF_SIGNED_INTEGRAL_TYPES)
+STF_CASE_TPL (" idiv signed_int",  STF_SIGNED_INTEGRAL_TYPES)
 {
   namespace bs = boost::simd;
   namespace bd = boost::dispatch;
-  using bs::idivceil;
+  using bs::idiv;
 
-  using r_t = decltype(idivceil(T(), T()));
+  using r_t = decltype(idiv(T(), T()));
 
-  // return type conformity test
-  STF_TYPE_IS(r_t, bd::as_integer_t<T>);
+  STF_EQUAL(idiv(T(4),T(3), bs::ceil), r_t(2));
+  STF_EQUAL(idiv(T(-4),T(-3), bs::ceil), r_t(2));
+  STF_EQUAL(idiv(T(-4),T(3), bs::ceil), r_t(-1));
+  STF_EQUAL(idiv(T(4),T(-3), bs::ceil), r_t(-1));
+  STF_EQUAL(idiv(bs::Mone<T>(), bs::Mone<T>(), bs::ceil), bs::One<r_t>());
+  STF_EQUAL(idiv(bs::One<T>(), bs::One<T>(), bs::ceil), bs::One<r_t>());
+  STF_EQUAL(idiv(bs::One<T>(), bs::Zero<T>(), bs::ceil), bs::Valmax<r_t>());
+  STF_EQUAL(idiv(bs::Mone<T>(), bs::Zero<T>(), bs::ceil), bs::Valmin<r_t>());
 
-  // specific values tests
-  STF_EQUAL(idivceil(T(4),T(3)), r_t(2));
-  STF_EQUAL(idivceil(T(-4),T(-3)), r_t(2));
-  STF_EQUAL(idivceil(T(-4),T(3)), r_t(-1));
-  STF_EQUAL(idivceil(T(4),T(-3)), r_t(-1));
-  STF_EQUAL(idivceil(bs::Mone<T>(), bs::Mone<T>()), bs::One<r_t>());
-  STF_EQUAL(idivceil(bs::One<T>(), bs::One<T>()), bs::One<r_t>());
-  STF_EQUAL(idivceil(bs::One<T>(), bs::Zero<T>()), bs::Valmax<r_t>());
-  STF_EQUAL(idivceil(bs::Mone<T>(), bs::Zero<T>()), bs::Valmin<r_t>());
 } // end of test for signed_int_
 
 
