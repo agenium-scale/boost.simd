9ebff545407e9c4dd67b9d3cc39a2a502d30fc02
Joel Falcou <joel.falcou@lri.fr>
2016-04-05 08:14:49 +0200

    Complete extract for logical+emulation

--- test/function/simd/extract/dynamic.arithmetic.cpp
+++ test/function/simd/extract/dynamic.arithmetic.cpp
@@ -9,9 +9,8 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#include <boost/simd/pack.hpp>
 #include <boost/simd/function/extract.hpp>
-#include <boost/simd/constant/constant.hpp>
+#include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
 #include <array>
 
@@ -26,45 +25,15 @@ void test(Env& $)
   std::array<T,N> ref;
   for(std::size_t i = 0; i < N; ++i) ref[i] = T(i*2);
 
-  bs::pack<T, N> p(&ref[0], &ref[0]+N);
-  for(std::size_t i = 0; i < N; ++i) STF_EQUAL(bs::extract(p, i), ref[i]);
-}
-
-template <typename T, std::size_t N, typename Env, typename... Idx>
-void unroll ( bs::pack<T,N> const& p, std::array<T,N> const& ref
-            , brigand::list<Idx...> const&, Env& $
-            )
-{
-  BOOST_SIMD_LOCAL_UNROLL( (bs::extract<Idx::value>(p), ref[Idx::value]) );
-}
-
-template <typename T, std::size_t N, typename Env>
-void test_st(Env& $)
-{
-  namespace bs = boost::simd;
-  STF_TYPE_IS(T, decltype(bs::extract<0>(bs::pack<T, N>{})) );
-
-  std::array<T,N> ref;
-  for(std::size_t i = 0; i < N; ++i) ref[i] = T(i*2);
-
   bs::pack<T,N> p(&ref[0], &ref[0]+N);
-  unroll(p,ref,brigand::range<std::size_t, 0, N>(),$);
+  for(std::size_t i = 0; i < N; ++i) STF_EQUAL(bs::extract(p, i), ref[i]);
 }
 
 STF_CASE_TPL("Check dynamic extract on pack" , STF_NUMERIC_TYPES)
 {
   static const std::size_t N = boost::simd::pack<T>::static_size;
 
-  test<T, N>($);
   test<T, N/2>($);
+  test<T, N>($);
   test<T, N*2>($);
-}
-
-STF_CASE_TPL("Check static extract on pack" , STF_NUMERIC_TYPES)
-{
-  static const std::size_t N = boost::simd::pack<T>::static_size;
-
-  test_st<T, N>($);
-  test_st<T, N/2>($);
-  test_st<T, N*2>($);
 }
---
+++ test/function/simd/extract/static.logical.cpp
@@ -0,0 +1,56 @@
+//==================================================================================================
+/*!
+  @file
+
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/function/extract.hpp>
+#include <boost/simd/detail/unroll.hpp>
+#include <boost/simd/pack.hpp>
+#include <boost/simd/logical.hpp>
+#include <simd_test.hpp>
+#include <array>
+
+namespace bs = boost::simd;
+namespace bd = boost::dispatch;
+
+template <typename T, std::size_t N, typename Env, typename Idx>
+void unroll_step ( bs::pack<T,N> const& p, std::array<T,N> const& ref, Idx const&, Env& $)
+{
+  STF_EQUAL(bs::extract<Idx::value>(p), ref[Idx::value]);
+}
+
+template <typename T, std::size_t N, typename Env, typename... Idx>
+void unroll ( bs::pack<T,N> const& p, std::array<T,N> const& ref
+            , brigand::list<Idx...> const&, Env& $
+            )
+{
+  BOOST_SIMD_LOCAL_UNROLL( unroll_step(p,ref,Idx{},$) );
+}
+
+template <typename T, std::size_t N, typename Env>
+void test_st(Env& $)
+{
+  namespace bs = boost::simd;
+  STF_TYPE_IS(T, decltype(bs::extract<0>(bs::pack<T, N>{})) );
+
+  std::array<T,N> ref;
+  for(std::size_t i = 0; i < N; ++i) ref[i] = T(i*2);
+
+  bs::pack<T,N> p(&ref[0], &ref[0]+N);
+  unroll(p,ref,brigand::range<std::size_t, 0, N>(),$);
+}
+
+STF_CASE_TPL("Check static extract on pack" , STF_NUMERIC_TYPES)
+{
+  static const std::size_t N = boost::simd::pack<T>::static_size;
+
+  test_st<bs::logical<T>, N/2>($);
+  test_st<bs::logical<T>, N>($);
+  test_st<bs::logical<T>, N*2>($);
+}
