4025e399ecb4329940c0b2d0bdd3841736db24ca
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-11 22:16:21 +0200

    Fix for bitwise_and and emulation

--- include/boost/simd/arch/common/simd/function/bitwise_and.hpp
+++ include/boost/simd/arch/common/simd/function/bitwise_and.hpp
@@ -13,9 +13,9 @@
 #define BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_BITWISE_AND_HPP_INCLUDED
 
 #include <boost/simd/sdk/hierarchy/simd.hpp>
+#include <boost/simd/detail/brigand.hpp>
 #include <boost/simd/function/simd/bitwise_cast.hpp>
-#include <boost/dispatch/meta/as_integer.hpp>
-#include <boost/utility/enable_if.hpp>
+#include <boost/dispatch/function/overload.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -23,38 +23,32 @@ namespace boost { namespace simd { namespace ext
   namespace bd = boost::dispatch;
 
   BOOST_DISPATCH_OVERLOAD(bitwise_and_
-                         , (typename A0,typename A1,typename X)
-                         , bd::cpu_
-                         , bs::pack_<bd::arithmetic_<A0>,X>
-                         , bs::pack_<bd::arithmetic_<A1>,X>
+                         , (typename A0,typename A1)
+                         , bs::simd_
+                         , bs::pack_<bd::arithmetic_<A0>,bs::simd_native_>
+                         , bs::pack_<bd::arithmetic_<A1>,bs::simd_native_>
                          )
 
   {
-    BOOST_FORCEINLINE A0 operator()( const A0& a0, const A1& a1
-                                   , typename std::enable_if<
-                                     sizeof(A1) == sizeof(A0)
-                                     >::type* = 0
-                                   ) const BOOST_NOEXCEPT
-      {
-        return bitwise_and(a0, simd::bitwise_cast<A0>(a1));
-      }
+    BOOST_FORCEINLINE A0 operator()( const A0& a0, const A1& a1 ) const BOOST_NOEXCEPT
+    {
+      return bitwise_and(a0, simd::bitwise_cast<A0>(a1));
+    }
    };
 
-  BOOST_DISPATCH_OVERLOAD(bitwise_and_
-                         , (typename A0,typename X)
-                         , bd::cpu_
-                         , bs::pack_<bd::floating_<A0>,X>
-                         , bs::pack_<bd::arithmetic_<A0>,X>
-                         )
-
+  BOOST_DISPATCH_OVERLOAD_IF(bitwise_and_
+                            , (typename A0,typename A1)
+                            , (brigand::not_<std::is_same<A0,A1>>)
+                            , bs::simd_
+                            , bs::pack_<bd::arithmetic_<A0>,bs::simd_emulation_>
+                            , bs::pack_<bd::arithmetic_<A1>,bs::simd_emulation_>
+                            )
   {
-    BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1
-                                   ) const BOOST_NOEXCEPT
+    BOOST_FORCEINLINE A0 operator()( const A0& a0, const A1& a1 ) const BOOST_NOEXCEPT
     {
-      using iA0 = bd::as_integer_t<A0>;
-      return bitwise_cast<A0>(bitwise_and(bitwise_cast<iA0>(a0), a1));
+      return bitwise_and(a0, simd::bitwise_cast<A0>(a1));
     }
-  };
-} } }
+   };
+ } } }
 
 #endif
