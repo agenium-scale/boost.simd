eb464bbfde18407bc6f38ff0d910af7682bbb8ec
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-14 09:12:48 +0200

    Amend test for pack<T,1>

--- test/api/pack/proxy/cast.cpp
+++ test/api/pack/proxy/cast.cpp
@@ -1,25 +1,31 @@
 //==================================================================================================
 /*
-  Copyright 2015 NumScale SAS
+  Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-
+#include <boost/simd/logical.hpp>
 #include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
+#include <algorithm>
+#include <numeric>
+
+namespace bs = boost::simd;
 
 template <typename T, std::size_t N, typename Env>
 void test(Env& $)
 {
-  using pack_t = boost::simd::pack<T, N>;
-  pack_t p;
+  std::array<T, N> ref;
+  std::iota(ref.begin(), ref.end(), T(1));
+  bs::pack<T, N> p0(ref.begin(), ref.end()), res;
 
-  STF_EXPECT(p.empty() == false);
+  std::transform(p0.begin(),p0.end(),res.begin(), [](T e) { return e; } );
+  STF_ALL_EQUAL(res, ref);
 }
 
-STF_CASE_TPL("Check for default constructed pack behaviours" , STF_NUMERIC_TYPES)
+STF_CASE_TPL( "Check proxy access" , STF_NUMERIC_TYPES)
 {
   test<T,  1>($);
   test<T,  2>($);
---
+++ test/api/pack/proxy/not.cpp
@@ -0,0 +1,38 @@
+//==================================================================================================
+/*
+  Copyright 2016 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/pack.hpp>
+#include <algorithm>
+#include <numeric>
+#include <simd_test.hpp>
+
+namespace bs = boost::simd;
+
+template <typename T, std::size_t N, typename Env>
+void test_not(Env& $)
+{
+  std::array<T, N> a;
+  std::iota(a.begin(), a.end(), T(1));
+  bs::pack<T, N> p0(a.begin(), a.end()), ref(a.begin(), a.end());
+
+  bool supports_logical_not = true;
+  for (auto it0 = p0.begin(); it0 != p0.end(); ++it0)
+    supports_logical_not = supports_logical_not && !(!(*it0));
+
+  STF_EXPECT( supports_logical_not );
+}
+
+STF_CASE_TPL( "Check proxy bitwise negation" , STF_NUMERIC_TYPES)
+{
+  test_not<T,  1>($);
+  test_not<T,  2>($);
+  test_not<T,  4>($);
+  test_not<T,  8>($);
+  test_not<T, 16>($);
+  test_not<T, 32>($);
+}
