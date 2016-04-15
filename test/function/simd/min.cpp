ec9c1ed711c97daa46f72849f1f6a5b3244e4725
jtlap <jtlapreste@gmail.com>
2016-04-15 21:46:27 +0200

    min/max tests

--- test/function/simd/min.cpp
+++ test/function/simd/min.cpp
@@ -9,6 +9,7 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
+#define BOOST_SIMD_ENABLE_DIAG
 #include <boost/simd/pack.hpp>
 #include <boost/simd/function/min.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
@@ -20,9 +21,6 @@ void test(Env& $)
   namespace bs = boost::simd;
   using p_t = bs::pack<T, N>;
 
-  namespace bs = boost::simd;
-  namespace bd = boost::dispatch;
-
   T a1[N], a2[N], b[N];
   for(std::size_t i = 0; i < N; ++i)
   {
@@ -30,9 +28,9 @@ void test(Env& $)
      a2[i] = (i%2) ? T(i+N) : T(-(i+N));
      b[i] = bs::min(a1[i], a2[i]);
    }
-  p_t aa1(&a1[0], &a1[N]);
-  p_t aa2(&a2[0], &a2[N]);
-  p_t bb(&b[0], &b[N]);
+  p_t aa1(&a1[0], &a1[0]+N);
+  p_t aa2(&a2[0], &a2[0]+N);
+  p_t bb(&b[0], &b[0]+N);
   STF_IEEE_EQUAL(bs::min(aa1, aa2), bb);
 }
 
@@ -42,6 +40,7 @@ STF_CASE_TPL("Check min on pack" , STF_NUMERIC_TYPES)
   using p_t = bs::pack<T>;
   static const std::size_t N = bs::cardinal_of<p_t>::value;
   test<T, N>($);
-//  test<T, N/2>($);
-//  test<T, Nx2>($);
+  test<T, N/2>($);
+  test<T, N*2>($);
 }
+
