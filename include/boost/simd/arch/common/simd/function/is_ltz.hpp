51ffbe0f01499b35f669059ef59c1504104f60af
jtlap <jtlapreste@gmail.com>
2016-04-17 12:47:42 +0200

    missing BOOST_NOEXCEPT to is_ltz.hpp

--- include/boost/simd/arch/common/simd/function/is_ltz.hpp
+++ include/boost/simd/arch/common/simd/function/is_ltz.hpp
@@ -41,7 +41,7 @@ namespace boost { namespace simd { namespace ext
                           )
    {
      using result = bs::as_logical_t<A0>;
-     BOOST_FORCEINLINE result operator()(const A0&)const
+     BOOST_FORCEINLINE result operator()(const A0&) const BOOST_NOEXCEPT
      {
        return bs::False<result>();
      }
