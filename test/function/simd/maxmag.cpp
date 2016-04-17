6a23dc7d2f8cd76af30909cb554c00254b9bbd97
jtlap <jtlapreste@gmail.com>
2016-04-17 13:54:53 +0200

    min... max... and meanof tests

--- test/function/simd/maxmag.cpp
+++ test/function/simd/maxmag.cpp
@@ -9,6 +9,7 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
+#define BOOST_SIMD_ENABLE_DIAG
 #include <boost/simd/pack.hpp>
 #include <boost/simd/function/maxmag.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
@@ -19,9 +20,6 @@ void test(Env& $)
 {
   namespace bs = boost::simd;
   using p_t = bs::pack<T, N>;
-
-  namespace bs = boost::simd;
-  namespace bd = boost::dispatch;
 
   T a1[N], a2[N], b[N];
   for(std::size_t i = 0; i < N; ++i)
@@ -42,6 +40,6 @@ STF_CASE_TPL("Check maxmag on pack" , STF_NUMERIC_TYPES)
   using p_t = bs::pack<T>;
   static const std::size_t N = bs::cardinal_of<p_t>::value;
   test<T, N>($);
-//  test<T, N/2>($);
-//  test<T, Nx2>($);
+  test<T, N/2>($);
+  test<T, N*2>($);
 }
