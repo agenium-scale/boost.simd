2a5c9c5b6e12d4f875aa8f5d65c80b28f12ce203
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-19 18:48:46 +0200

    Refurbishing of common adds

--- include/boost/simd/arch/common/simd/function/adds.hpp
+++ include/boost/simd/arch/common/simd/function/adds.hpp
@@ -1,9 +1,7 @@
 //==================================================================================================
-/*!
-  @file
-
-  @copyright 2016 NumScale SAS
-  @copyright 2016 J.T. Lapreste
+/**
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T. Lapreste
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
@@ -11,9 +9,9 @@
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_ADDS_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_ADDS_HPP_INCLUDED
-#include <boost/simd/detail/overload.hpp>
 
-#include <boost/simd/meta/hierarchy/simd.hpp>
+#include <boost/simd/detail/overload.hpp>
+#include <boost/simd/detail/traits.hpp>
 #include <boost/simd/constant/valmax.hpp>
 #include <boost/simd/constant/zero.hpp>
 #include <boost/simd/function/simd/bitwise_cast.hpp>
@@ -33,52 +31,60 @@ namespace boost { namespace simd { namespace ext
 {
    namespace bd = boost::dispatch;
    namespace bs = boost::simd;
-   BOOST_DISPATCH_OVERLOAD(adds_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_<bd::floating_<A0>, X>
-                          , bs::pack_<bd::floating_<A0>, X>
-                          )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
-      {
-        return a0+a1;
-      }
-   };
 
-   BOOST_DISPATCH_OVERLOAD(adds_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_<bd::uint_<A0>, X>
-                          , bs::pack_<bd::uint_<A0>, X>
-                          )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
-      {
-        A0 a0pa1 = a0+a1;
-        return bitwise_or(a0pa1, genmask(is_less(a0pa1, a0)));
-      }
-   };
+  BOOST_DISPATCH_OVERLOAD_IF( adds_
+                            , (typename A0, typename X)
+                            , (detail::is_native<X>)
+                            , bd::cpu_
+                            , bs::pack_<bd::floating_<A0>, X>
+                            , bs::pack_<bd::floating_<A0>, X>
+                            )
+  {
+    BOOST_FORCEINLINE A0 operator()(const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+    {
+      return a0+a1;
+    }
+  };
 
-   BOOST_DISPATCH_OVERLOAD(adds_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_<bd::int_<A0>, X>
-                          , bs::pack_<bd::int_<A0>, X>
-                          )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
-      {
-        using utype = bd::as_unsigned_t<A0>;
-        using stype = bd::scalar_of_t<A0>;
-        utype ux = bitwise_cast<utype>(a0);
-        utype uy = bitwise_cast<utype>(a1);
-        utype res = ux + uy;
-        ux = shr(ux, sizeof(stype)*CHAR_BIT-1) + splat<utype>(Valmax<stype>()); //+ static_cast<bd::scalar_of_t<utype>>(Valmax<stype>());
-        auto t = is_gez(bitwise_cast<A0>(bitwise_ornot(bitwise_xor(ux, uy), bitwise_xor(uy, res))));
-        return bitwise_cast<A0>(if_else(t, ux, res));
-      }
-   };
+  BOOST_DISPATCH_OVERLOAD_IF( adds_
+                            , (typename A0, typename X)
+                            , (detail::is_native<X>)
+                            , bd::cpu_
+                            , bs::pack_<bd::uint_<A0>, X>
+                            , bs::pack_<bd::uint_<A0>, X>
+                            )
+  {
+    BOOST_FORCEINLINE A0 operator()(const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+    {
+      A0 const s = a0+a1;
+      return s | genmask(s < a0);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD_IF( adds_
+                            , (typename A0, typename X)
+                            , (detail::is_native<X>)
+                            , bd::cpu_
+                            , bs::pack_<bd::int_<A0>, X>
+                            , bs::pack_<bd::int_<A0>, X>
+                            )
+  {
+    BOOST_FORCEINLINE A0 operator()(const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+    {
+      using utype = bd::as_unsigned_t<A0>;
+      using stype = typename A0::value_type;
+
+      utype ux = bitwise_cast<utype>(a0);
+      utype const uy = bitwise_cast<utype>(a1);
+      utype const res = ux + uy;
+
+      ux = shr(ux, sizeof(stype)*CHAR_BIT-1) + utype(Valmax<stype>());
+
+      auto t = is_gez(bitwise_cast<A0>(bitwise_ornot(ux ^ uy, uy ^ res )));
+
+      return bitwise_cast<A0>(if_else(t, ux, res));
+    }
+  };
 } } }
 
 #endif
