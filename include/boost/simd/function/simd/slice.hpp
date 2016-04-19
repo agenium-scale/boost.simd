087098bfeb8cbfa35d7445fc3bd336a6ae37120d
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-19 07:40:13 +0200

    Implement slice

--- include/boost/simd/function/simd/slice.hpp
+++ include/boost/simd/function/simd/slice.hpp
@@ -1,20 +1,16 @@
 //==================================================================================================
-/*!
-  @file
-
-  Copyright 2015 NumScale SAS
-  Copyright 2015 J.T.Lapreste
+/**
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T.Lapreste
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-*/
+**/
 //==================================================================================================
 #ifndef BOOST_SIMD_FUNCTION_SIMD_SLICE_INCLUDED
 #define BOOST_SIMD_FUNCTION_SIMD_SLICE_INCLUDED
 
-#include <boost/simd/function/scalar/slice.hpp>
-#include <boost/simd/arch/common/generic/function/autodispatcher.hpp>
-//#include <boost/simd/arch/common/simd/function/slice.hpp>
+#include <boost/simd/function/definition/slice.hpp>
+#include <boost/simd/arch/common/simd/function/slice.hpp>
 
 #endif
-
