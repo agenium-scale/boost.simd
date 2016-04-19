f55d26c5203ac5350dc0aee2c6ed988295475c58
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-19 18:35:16 +0200

    Refurbishing of common abs

--- include/boost/simd/arch/common/simd/function/abs.hpp
+++ include/boost/simd/arch/common/simd/function/abs.hpp
@@ -1,9 +1,7 @@
 //==================================================================================================
-/*!
-  @file
-
-  @copyright 2016 NumScale SAS
-  @copyright 2016 J.T. Lapreste
+/**
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T. Lapreste
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
@@ -11,58 +9,66 @@
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_ABS_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_ABS_HPP_INCLUDED
-#include <boost/simd/detail/overload.hpp>
 
+#include <boost/simd/detail/overload.hpp>
+#include <boost/simd/detail/traits.hpp>
 #include <boost/simd/meta/hierarchy/simd.hpp>
-#include <boost/simd/constant/maxleftshift.hpp>
-#include <boost/simd/constant/mzero.hpp>
 #include <boost/simd/function/simd/bitwise_notand.hpp>
 #include <boost/simd/function/simd/bitwise_xor.hpp>
-#include <boost/simd/function/simd/plus.hpp>
 #include <boost/simd/function/simd/shift_right.hpp>
+#include <boost/simd/function/simd/plus.hpp>
+#include <boost/simd/constant/maxleftshift.hpp>
+#include <boost/simd/constant/mzero.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
-   namespace bd = boost::dispatch;
-   namespace bs = boost::simd;
-   BOOST_DISPATCH_OVERLOAD(abs_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_<bd::signed_<A0>, X>
-                          )
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+
+  //------------------------------------------------------------------------------------------------
+  // abs on signed values
+  BOOST_DISPATCH_OVERLOAD_IF( abs_
+                            , (typename A0, typename X)
+                            , (detail::is_native<X>)
+                            , bd::cpu_
+                            , bs::pack_<bd::signed_<A0>, X>
+                            )
    {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0) const BOOST_NOEXCEPT
+      BOOST_FORCEINLINE A0 operator()(const A0& a0) const BOOST_NOEXCEPT
       {
-        using int_type = bd::as_integer_t<A0, signed>;
-        using sint_type = bd::scalar_of_t<int_type>;
-        A0 const s = shift_right(a0, Maxleftshift<sint_type>());
+        A0 const s = shift_right(a0, Maxleftshift<typename A0::value_type>());
         return (a0+s)^s;
       }
    };
 
-   BOOST_DISPATCH_OVERLOAD(abs_
+  //------------------------------------------------------------------------------------------------
+  // abs on unsigned values is identity
+  BOOST_DISPATCH_OVERLOAD ( abs_
                           , (typename A0, typename X)
                           , bd::cpu_
                           , bs::pack_<bd::unsigned_<A0>, X>
                           )
    {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0) const BOOST_NOEXCEPT
+      BOOST_FORCEINLINE A0 operator()(const A0& a0) const BOOST_NOEXCEPT
       {
-         return a0;
+        return a0;
       }
    };
 
-   BOOST_DISPATCH_OVERLOAD(abs_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_<bd::floating_<A0>, X>
-                          )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0) const BOOST_NOEXCEPT
-      {
-        return bitwise_notand(Mzero<A0>(),a0);
-      }
-   };
+  //------------------------------------------------------------------------------------------------
+  // abs on real values cleans the sign bit
+  BOOST_DISPATCH_OVERLOAD_IF( abs_
+                            , (typename A0, typename X)
+                            , (detail::is_native<X>)
+                            , bd::cpu_
+                            , bs::pack_<bd::floating_<A0>, X>
+                            )
+  {
+    BOOST_FORCEINLINE A0 operator()( const A0& a0) const BOOST_NOEXCEPT
+    {
+      return bitwise_notand(Mzero<A0>(),a0);
+    }
+  };
 } } }
 
 #endif
