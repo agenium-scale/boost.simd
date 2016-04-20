b0659a321f38a482862449e90524aa5f9efeab20
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-20 07:54:40 +0200

    Implement slice_high

---
+++ include/boost/simd/function/simd/slice_high.hpp
@@ -0,0 +1,16 @@
+//==================================================================================================
+/**
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+**/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SIMD_SLICE_HIGH_INCLUDED
+#define BOOST_SIMD_FUNCTION_SIMD_SLICE_HIGH_INCLUDED
+
+#include <boost/simd/function/definition/slice_high.hpp>
+#include <boost/simd/arch/common/simd/function/slice_high.hpp>
+
+#endif
