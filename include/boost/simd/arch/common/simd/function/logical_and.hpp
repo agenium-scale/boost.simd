d0e69b703388dbb43248ed1cd327133b884db7ab
Charly Chevalier <charly.chevalier@numscale.com>
2016-04-20 14:50:04 +0200

    Cleanup logical operators

--- include/boost/simd/arch/common/simd/function/logical_and.hpp
+++ include/boost/simd/arch/common/simd/function/logical_and.hpp
@@ -23,21 +23,20 @@ namespace boost { namespace simd { namespace ext
    namespace bd = boost::dispatch;
    namespace bs = boost::simd;
 
-   BOOST_DISPATCH_OVERLOAD_IF( logical_and_
-                             , (typename A0,typename A1,typename X)
-                             , (detail::same_size<A0,A1>)
-                             , bd::cpu_
-                             , bs::pack_<bd::fundamental_<A0>,X>
-                             , bs::pack_<bd::fundamental_<A1>,X>
-                             )
-
-   {
-     using result = bs::as_logical_t<A0>;
-     BOOST_FORCEINLINE result operator()(const A0& a0, const A1& a1) const BOOST_NOEXCEPT
-     {
-       return mask2logical(bitwise_and(genmask(a0), genmask(a1)));
-     }
-   };
+  BOOST_DISPATCH_OVERLOAD_IF( logical_and_
+                            , (typename A0,typename A1,typename X)
+                            , (brigand::and_<detail::same_size<A0,A1>, detail::is_native<X>>)
+                            , bd::cpu_
+                            , bs::pack_<bd::fundamental_<A0>,X>
+                            , bs::pack_<bd::fundamental_<A1>,X>
+                            )
+    {
+      BOOST_FORCEINLINE
+      bs::as_logical_t<A0> operator()(const A0& a0, const A1& a1) const BOOST_NOEXCEPT
+      {
+        return mask2logical(bitwise_and(genmask(a0), genmask(a1)));
+      }
+    };
 } } }
 
 #endif
