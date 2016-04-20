1cd3f15861a9291744a83ef43ab1e180e02b4356
jtlap <jtlapreste@gmail.com>
2016-04-20 23:00:21 +0200

    toint_s touint_s

---
+++ include/boost/simd/function/simd/touint_s.hpp
@@ -0,0 +1,19 @@
+//==================================================================================================
+/*!
+  @file
+
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SIMD_TOUINT_S_INCLUDED
+#define BOOST_SIMD_FUNCTION_SIMD_TOUINT_S_INCLUDED
+
+#include <boost/simd/function/scalar/touint_s.hpp>
+//#include <boost/simd/arch/common/simd/function/touint_s.hpp>
+
+#endif
+
---
+++ include/boost/simd/function/touint_s.hpp
@@ -0,0 +1,76 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2012-2016 NumScale SAS
+  @copyright 2016 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_TOUINT_S_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_TOUINT_S_HPP_INCLUDED
+
+#if defined(DOXYGEN_ONLY)
+namespace boost { namespace simd
+{
+  /*!
+
+    @ingroup group-arithmetic
+
+    Convert to unsigned integer by saturated truncation.
+
+    @par semantic:
+    For any given value @c x of type @c T:
+
+    @code
+    as_integer_t<T, unsigned> r = touint(x);
+    @endcode
+
+    The code is similar to:
+
+    @code
+    using T_t = as_integer_t<T,unsigned>;
+    as_integer_t<T,unsigned> r = static_cast<T_t>(saturate<as_integer_t<T,unsigned> (x)))
+    @endcode
+
+    @par Notes:
+
+    The @ref Inf, @ref Nan and negative values are treated properly and go respectively to
+    @ref Valmax, and @ref Zero of the destination integral type.
+
+    All values superior (resp. less) than @ref Valmax (resp. @ref Valmin) of the return type
+    are saturated accordingly.
+
+    If you do not care about invalid values or overflows toint is speedier.
+
+    @see touint
+
+    @return      a value of the unsigned integer type associated to the input.
+
+
+**/
+  template<typename T> auto touint_s(T const& x) {}
+
+  namespace functional
+  {
+    /*!
+      @ingroup group-callable-arithmetic
+
+      Convert to unsigned integer by saturated truncation.
+
+
+      Function object tied to simd::touint_s
+
+      @see simd::touint_s
+    **/
+    const boost::dispatch::functor<tag::touint_s_> touint_s = {};
+  }
+} }
+#endif
+
+#include <boost/simd/function/scalar/touint_s.hpp>
+#include <boost/simd/function/simd/touint_s.hpp>
+
+#endif
