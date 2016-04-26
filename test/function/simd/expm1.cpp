9b3f7ff51136631c20944b393cfa7c1e4d2882a6
jtlap <jtlapreste@gmail.com>
2016-04-26 19:49:18 +0200

    forgotten expm1 test

---
+++ test/function/simd/expm1.cpp
@@ -0,0 +1,46 @@
+//==================================================================================================
+/*!
+  @file
+
+  Copyright 2016 NumScale SAS
+  Copyright 2016 J.T. Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/simd/pack.hpp>
+#include <boost/simd/function/expm1.hpp>
+#include <boost/simd/function/std.hpp>
+#include <boost/simd/meta/cardinal_of.hpp>
+#include <simd_test.hpp>
+
+template <typename T, std::size_t N, typename Env>
+void test(Env& $)
+{
+  namespace bs = boost::simd;
+  using p_t = bs::pack<T, N>;
+
+  T a1[N], b[N], c[N];
+  for(std::size_t i = 0; i < N; ++i)
+  {
+    a1[i] = (i%2) ? T(i) : T(-i);
+    b[i] = bs::expm1(a1[i]) ;
+    c[i] = bs::std_(bs::expm1)(a1[i]) ;
+  }
+  p_t aa1(&a1[0], &a1[N]);
+  p_t bb (&b[0], &b[N]);
+  p_t cc (&c[0], &c[N]);
+  STF_IEEE_EQUAL(bs::expm1(aa1), bb);
+  STF_IEEE_EQUAL(bs::std_(bs::expm1)(aa1), cc);
+}
+
+STF_CASE_TPL("Check expm1 on pack" , STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  using p_t = bs::pack<T>;
+  static const std::size_t N = bs::cardinal_of<p_t>::value;
+  test<T, N>($);
+  test<T, N/2>($);
+  test<T, N*2>($);
+}
