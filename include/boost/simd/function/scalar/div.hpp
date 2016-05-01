52f42ca2f9ec32261bae6ba203965f62e4e018aa
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-02 00:00:06 +0200

    Fix scalar function header guards

--- include/boost/simd/function/scalar/div.hpp
+++ include/boost/simd/function/scalar/div.hpp
@@ -8,8 +8,8 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#ifndef BOOST_SIMD_FUNCTION_SCALAR_DIV_INCLUDED
-#define BOOST_SIMD_FUNCTION_SCALAR_DIV_INCLUDED
+#ifndef BOOST_SIMD_FUNCTION_SCALAR_DIV_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_SCALAR_DIV_HPP_INCLUDED
 
 #include <boost/simd/function/definition/div.hpp>
 #include <boost/simd/arch/common/generic/function/div.hpp>
--- include/boost/simd/function/scalar/idiv.hpp
+++ include/boost/simd/function/scalar/idiv.hpp
@@ -8,8 +8,8 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#ifndef BOOST_SIMD_FUNCTION_SCALAR_IDIV_INCLUDED
-#define BOOST_SIMD_FUNCTION_SCALAR_IDIV_INCLUDED
+#ifndef BOOST_SIMD_FUNCTION_SCALAR_IDIV_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_SCALAR_IDIV_HPP_INCLUDED
 
 #include <boost/simd/function/definition/idiv.hpp>
 
