d6b96903b0d2f17d01d7ae1d8a68cb61581954cf
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-02 00:01:02 +0200

    Fix main function header guards

--- include/boost/simd/function/div.hpp
+++ include/boost/simd/function/div.hpp
@@ -8,8 +8,8 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#ifndef BOOST_SIMD_FUNCTION_DIV_INCLUDED
-#define BOOST_SIMD_FUNCTION_DIV_INCLUDED
+#ifndef BOOST_SIMD_FUNCTION_DIV_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_DIV_HPP_INCLUDED
 
 #if defined(DOXYGEN_ONLY)
 namespace boost { namespace simd
--- include/boost/simd/function/select.hpp
+++ include/boost/simd/function/select.hpp
@@ -9,8 +9,8 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#ifndef BOOST_SIMD_FUNCTION_SELECT_INCLUDED
-#define BOOST_SIMD_FUNCTION_SELECT_INCLUDED
+#ifndef BOOST_SIMD_FUNCTION_SELECT_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_SELECT_HPP_INCLUDED
 
 #if defined(DOXYGEN_ONLY)
 namespace boost { namespace simd
