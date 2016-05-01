1006ac3e04e5a6108aa64ac26506fe9b25096adc
jtlap <jtlapreste@gmail.com>
2016-05-01 09:26:22 +0200

    divfix simd test but commenting simd divfix

--- include/boost/simd/arch/common/simd/function/divfix.hpp
+++ include/boost/simd/arch/common/simd/function/divfix.hpp
@@ -23,66 +23,67 @@
 #include <boost/simd/function/simd/touint.hpp>
 #include <boost/simd/function/simd/trunc.hpp>
 
+//TODO wait for split groups
 namespace boost { namespace simd { namespace ext
 {
    namespace bd = boost::dispatch;
    namespace bs = boost::simd;
-   BOOST_DISPATCH_OVERLOAD(divfix_
-                             , (typename A0, typename X)
-                             , bd::cpu_
-                             , bs::pack_<bd::int_<A0>, X>
-                             , bs::pack_<bd::int_<A0>, X>
-                             )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
-      {
-        using ivtype = bd::upgrade_t<A0>;
-        ivtype a0l, a0h, a1l, a1h;
-        bs::split(a0, a0l, a0h );
-        bs::split(a1, a1l, a1h );
-        ivtype d0 = saturated_(toint)(divfix(tofloat(a0l), tofloat(a1l)));
-        ivtype d1 = saturated_(toint)(divfix(tofloat(a0h), tofloat(a1h)));
-        return groups(d0, d1);
-      }
-   };
+ //   BOOST_DISPATCH_OVERLOAD(divfix_
+//                              , (typename A0, typename X)
+//                              , bd::cpu_
+//                              , bs::pack_<bd::int_<A0>, X>
+//                              , bs::pack_<bd::int_<A0>, X>
+//                              )
+//    {
+//       BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+//       {
+//         using ivtype = bd::upgrade_t<A0>;
+//         ivtype a0l, a0h, a1l, a1h;
+//         bs::split(a0, a0l, a0h );
+//         bs::split(a1, a1l, a1h );
+//         ivtype d0 = saturated_(toint)(divfix(tofloat(a0l), tofloat(a1l)));
+//         ivtype d1 = saturated_(toint)(divfix(tofloat(a0h), tofloat(a1h)));
+//         return groups(d0, d1);
+//       }
+//    };
 
-   BOOST_DISPATCH_OVERLOAD(divfix_
-                             , (typename A0, typename X)
-                             , bd::cpu_
-                             , bs::pack_<bd::uint_<A0>, X>
-                             , bs::pack_<bd::uint_<A0>, X>
-                             )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
-      {
-        using ivtype = bd::upgrade_t<A0>;
-        ivtype a0l, a0h, a1l, a1h;
-        bs::split(a0, a0l, a0h );
-        bs::split(a1, a1l, a1h );
-        ivtype d0 = saturated_(touint)(divfix(tofloat(a0l), tofloat(a1l)));
-        ivtype d1 = saturated_(touint)(divfix(tofloat(a0h), tofloat(a1h)));
-        return groups(d0, d1);
-      }
-   };
+ //   BOOST_DISPATCH_OVERLOAD(divfix_
+//                              , (typename A0, typename X)
+//                              , bd::cpu_
+//                              , bs::pack_<bd::uint_<A0>, X>
+//                              , bs::pack_<bd::uint_<A0>, X>
+//                              )
+//    {
+//       BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+//       {
+//         using ivtype = bd::upgrade_t<A0>;
+//         ivtype a0l, a0h, a1l, a1h;
+//         bs::split(a0, a0l, a0h );
+//         bs::split(a1, a1l, a1h );
+//         ivtype d0 = saturated_(touint)(divfix(tofloat(a0l), tofloat(a1l)));
+//         ivtype d1 = saturated_(touint)(divfix(tofloat(a0h), tofloat(a1h)));
+//         return groups(d0, d1);
+//       }
+//    };
 
-   BOOST_DISPATCH_OVERLOAD(divfix_
-                          , (typename A0, typename X)
-                          , bd::cpu_
-                          , bs::pack_<bd::ints8_<A0>, X>
-                          , bs::pack_<bd::ints8_<A0>, X>
-                          )
-   {
-      BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
-      {
-        using ivtype = bd::upgrade_t<A0>;
-        ivtype a0l, a0h, a1l, a1h;
-        bs::split(a0, a0l, a0h );
-        bs::split(a1, a1l, a1h );
-        ivtype d0 = divfix(a0l, a1l);
-        ivtype d1 = divfix(a0h, a1h);
-        return groups(d0, d1);
-      }
-   };
+ //   BOOST_DISPATCH_OVERLOAD(divfix_
+//                           , (typename A0, typename X)
+//                           , bd::cpu_
+//                           , bs::pack_<bd::ints8_<A0>, X>
+//                           , bs::pack_<bd::ints8_<A0>, X>
+//                           )
+//    {
+//       BOOST_FORCEINLINE A0 operator()( const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+//       {
+//         using ivtype = bd::upgrade_t<A0>;
+//         ivtype a0l, a0h, a1l, a1h;
+//         bs::split(a0, a0l, a0h );
+//         bs::split(a1, a1l, a1h );
+//         ivtype d0 = divfix(a0l, a1l);
+//         ivtype d1 = divfix(a0h, a1h);
+//         return groups(d0, d1);
+//       }
+//    };
 
    BOOST_DISPATCH_OVERLOAD(divfix_
                           , (typename A0, typename X)
