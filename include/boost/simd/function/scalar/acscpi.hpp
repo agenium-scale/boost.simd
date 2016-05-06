1b1a2413f56eb952b8d2ebe7b3b5d7457d3fbe98
jtlap <jtlapreste@gmail.com>
2016-05-06 18:32:47 +0200

    asec, acsc families

---
+++ include/boost/simd/function/scalar/acscpi.hpp
@@ -0,0 +1,18 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2015 NumScale SAS
+  @copyright 2015 J.T.Lapreste
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SCALAR_ACSCPI_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_SCALAR_ACSCPI_HPP_INCLUDED
+
+#include <boost/simd/function/definition/acscpi.hpp>
+#include <boost/simd/arch/common/generic/function/acscpi.hpp>
+
+#endif
