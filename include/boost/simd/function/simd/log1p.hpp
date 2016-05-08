4ff5c32c5db40c517d215a5b1bdc131685b7bc9a
jtlap <jtlapreste@gmail.com>
2016-05-08 16:29:53 +0200

    including simd log1p

--- include/boost/simd/function/simd/log1p.hpp
+++ include/boost/simd/function/simd/log1p.hpp
@@ -14,5 +14,6 @@
 
 #include <boost/simd/function/scalar/log1p.hpp>
 #include <boost/simd/arch/common/generic/function/autodispatcher.hpp>
+#include <boost/simd/arch/common/simd/function/log1p.hpp>
 
 #endif
