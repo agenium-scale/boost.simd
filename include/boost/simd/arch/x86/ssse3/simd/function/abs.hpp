aac73067672a9c709dae808389c86bf58f1f5e1a
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-17 14:24:59 +0200

    Fix typo in ssse3 abs

--- include/boost/simd/arch/x86/ssse3/simd/function/abs.hpp
+++ include/boost/simd/arch/x86/ssse3/simd/function/abs.hpp
@@ -11,9 +11,9 @@
 */
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_X86_SSSE3_SIMD_FUNCTION_ABS_HPP_INCLUDED
-#include <boost/simd/detail/overload.hpp>
 #define BOOST_SIMD_ARCH_X86_SSSE3_SIMD_FUNCTION_ABS_HPP_INCLUDED
 
+#include <boost/simd/detail/overload.hpp>
 #include <boost/simd/meta/as_logical.hpp>
 #include <boost/simd/function/simd/genmask.hpp>
 
