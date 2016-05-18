299c79615244f979f2eaa217414313ccce296bc6
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-18 16:16:25 +0200

    long long is not a supported type

--- test/regression/issue_21.cpp
+++ test/regression/issue_21.cpp
@@ -21,13 +21,14 @@ class op_boost_utc_add
 
 int main()
 {
-  typedef boost::simd::pack<long long, 2> p_l_t;
+  // long long is not supported, prefer std::int64_t
+  typedef boost::simd::pack<std::int64_t, 2> p_l_t;
 
   p_l_t a(10, 100);
   p_l_t b(10, 100);
   p_l_t c(1, 1);
 
-  p_l_t r = op_boost_utc_add<long long>()(a, b, c);
+  p_l_t r = op_boost_utc_add<std::int64_t>()(a, b, c);
 
   cout << r << endl;
 
