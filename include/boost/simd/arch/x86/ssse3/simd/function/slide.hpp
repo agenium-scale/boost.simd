5523ed1c834c872fa667f0c13fef0292984160ec
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-18 11:51:23 +0200

    Fix slide behavior on MSVC

--- include/boost/simd/arch/x86/ssse3/simd/function/slide.hpp
+++ include/boost/simd/arch/x86/ssse3/simd/function/slide.hpp
@@ -13,6 +13,7 @@
 #include <boost/simd/meta/as_arithmetic.hpp>
 #include <boost/simd/detail/overload.hpp>
 #include <boost/simd/detail/brigand.hpp>
+#include <boost/simd/function/bitwise_cast.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -27,10 +28,11 @@ namespace boost { namespace simd { namespace ext
                           , bd::constant_<bd::integer_<Offset>>
                           )
   {
-    using storage_t = typename T::storage_type;
-
     BOOST_FORCEINLINE T operator()(T const& a0, T const& a1, Offset const&) const BOOST_NOEXCEPT
     {
+      // Work on byte level
+      using byte_t = typename T::template retype<std::uint8_t,16>;
+
       // Compute relative offsets
       using bitcount = std::integral_constant < std::size_t
                                               , sizeof(as_arithmetic_t<typename T::value_type>)
@@ -38,10 +40,10 @@ namespace boost { namespace simd { namespace ext
                                               >;
 
       // Shift everything in place
-      return (storage_t)( _mm_alignr_epi8 ( (__m128i)(a1.storage()), (__m128i)(a0.storage())
-                                          , bitcount::value
-                                          )
-                        );
+      return bitwise_cast<T>( _mm_alignr_epi8 ( bitwise_cast<byte_t>(a1), bitwise_cast<byte_t>(a0)
+                                              , bitcount::value
+                                              )
+                            );
     }
   };
 } } }
