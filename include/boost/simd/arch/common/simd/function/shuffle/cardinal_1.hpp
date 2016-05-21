2aa86821cd63c5fbb67b9081e912e0e310b6e778
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-21 16:50:08 +0200

    shuffle for pack<T,1>

---
+++ include/boost/simd/arch/common/simd/function/shuffle/cardinal_1.hpp
@@ -0,0 +1,140 @@
+//==================================================================================================
+/**
+  Copyright 2016 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+**/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_SHUFFLE_CARDINAL_1_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_SIMD_FUNCTION_SHUFFLE_CARDINAL_1_HPP_INCLUDED
+
+#include <boost/simd/detail/overload.hpp>
+#include <boost/simd/constant/zero.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd = boost::dispatch;
+  namespace bs = boost::simd;
+
+  // -----------------------------------------------------------------------------------------------
+  // Yes, yes, we're shuffling for pack of cardinal 1 ... Jeez, TMP eats my soul
+  // -----------------------------------------------------------------------------------------------
+
+  // -----------------------------------------------------------------------------------------------
+  // Handle invalid Indexes by a flat static_assert
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (int Idx, typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<Idx>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<Idx> const&, A0 const& a0) const BOOST_NOEXCEPT
+    {
+      static_assert ( Idx >= -1 && Idx < 1
+                    , "boost::simd::shuffle: Invalid permutation index"
+                    );
+      return a0;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (int Idx, typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<Idx>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<Idx> const&, A0 const& a0, A0 const&) const BOOST_NOEXCEPT
+    {
+      static_assert ( Idx >= -1 && Idx < 2
+                    , "boost::simd::shuffle: Invalid permutation index"
+                    );
+      return a0;
+    }
+  };
+
+  // -----------------------------------------------------------------------------------------------
+  // Two cases of unary shuffle for scalar : 0, -1
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<0>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<0> const&, A0 const& a0) const BOOST_NOEXCEPT
+    {
+      return a0;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<-1>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<-1> const&, A0 const&) const BOOST_NOEXCEPT
+    {
+      return Zero<A0>();
+    }
+  };
+
+  // -----------------------------------------------------------------------------------------------
+  // Three cases of unary shuffle for scalar : 0, 1, -1
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<0>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<0> const&, A0 const& a0, A0 const&) const BOOST_NOEXCEPT
+    {
+      return a0;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<1>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<1> const&, A0 const&, A0 const& a1) const BOOST_NOEXCEPT
+    {
+      return a1;
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( shuffle_
+                          , (typename A0, typename X)
+                          , bd::cpu_
+                          , bs::detail::pattern_<-1>
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          , bs::pack_< bd::unspecified_<A0>, X >
+                          )
+  {
+    BOOST_FORCEINLINE
+    A0 operator()(bs::detail::pattern_<-1> const&, A0 const&, A0 const&) const BOOST_NOEXCEPT
+    {
+      return Zero<A0>();
+    }
+  };
+} } }
+
+#endif
+
---
+++ test/function/simd/shuffle/cardinal_1.cpp
@@ -0,0 +1,61 @@
+//==================================================================================================
+/**
+  Copyright 2016 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+**/
+//==================================================================================================
+#include <boost/simd/function/shuffle.hpp>
+#include <boost/simd/pack.hpp>
+#include <simd_test.hpp>
+
+using namespace boost::simd;
+
+STF_CASE_TPL("Check unary shuffle behavior with direct permutation", STF_NUMERIC_TYPES)
+{
+  using p_t = pack<T,1>;
+  STF_EQUAL( shuffle<-1>(p_t(4)), Zero<p_t>() );
+  STF_EQUAL( shuffle<0>(p_t(4)) , p_t(4) );
+}
+
+STF_CASE_TPL("Check binary shuffle behavior with direct permutation", STF_NUMERIC_TYPES)
+{
+  using p_t = pack<T,1>;
+  STF_EQUAL( shuffle<-1>(p_t(4),p_t(7)) , Zero<p_t>() );
+  STF_EQUAL( shuffle< 0>(p_t(4),p_t(7)) , p_t(4) );
+  STF_EQUAL( shuffle< 1>(p_t(4),p_t(7)) , p_t(7) );
+}
+
+template<int N> struct grab_
+{
+  template<typename I, typename C> struct apply : std::integral_constant<int, N> {};
+};
+
+STF_CASE_TPL("Check unary shuffle behavior with direct meta-permutation", STF_NUMERIC_TYPES)
+{
+  using p_t = pack<T,1>;
+  STF_EQUAL( shuffle<grab_<0>>(p_t(4)) , p_t(4) );
+}
+
+STF_CASE_TPL("Check binary shuffle behavior with direct meta-permutation", STF_NUMERIC_TYPES)
+{
+  using p_t = pack<T,1>;
+  STF_EQUAL( shuffle<grab_<0>>(p_t(4),p_t(7)) , p_t(4) );
+  STF_EQUAL( shuffle<grab_<1>>(p_t(4),p_t(7)) , p_t(7) );
+}
+
+#if !defined(BOOST_NO_CONSTEXPR)
+// -------------------------------------------------------------------------------------------------
+// Not much variation when cardinal is equal to 1, so we just make two "permutations"
+
+constexpr int grab0(int /*i*/, int /*c*/) { return 0; }
+constexpr int grab1(int /*i*/, int /*c*/) { return 1; }
+
+STF_CASE_TPL("Check unary shuffle behavior with constexpr-permutation", STF_NUMERIC_TYPES)
+{
+  using p_t = pack<T,1>;
+  STF_EQUAL( shuffle<pattern<grab0>>(p_t(4),p_t(7)) , p_t(4) );
+  STF_EQUAL( shuffle<pattern<grab1>>(p_t(4),p_t(7)) , p_t(7) );
+}
+#endif
