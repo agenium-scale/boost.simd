9ff55dc94f72c24e458726ea77dcfc2e675114b3
Alan Kelly <alankelly@live.com>
2016-05-24 14:50:28 +0200

    doc updated

--- include/boost/simd.hpp
+++ include/boost/simd.hpp
@@ -4,7 +4,7 @@
 
   Convenience header for Boost.SIMD
 
-  @copyright 2012 - 2015 NumScale SAS
+  @copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
@@ -31,19 +31,19 @@ namespace boost
     /*!
       @defgroup group-api User API
 
-      The Boost.SIMD User's API includes all the types, meta-functions, algorithms, functions,
-      callable object allowing the design and implementation of SIMD aware code.
+      The Boost.SIMD User API includes all the types, meta-functions, algorithms, functions
+      and callable objects allowing the design and implementation of SIMD code.
     **/
 
     /*!
       @ingroup group-api
       @defgroup group-types Types and helpers
 
-      Boost.SIMD provides a set of new types to handle SIMD operations in a portable way.
+      Boost.SIMD provides a set of types to handle SIMD operations in a portable way.
       Those types include:
 
-      - boost::simd::pack which embeds SIMD wide-registers
-      - boost::simd::logical which embeds typed boolean values
+      - boost::simd::pack which encapsulates SIMD registers
+      - boost::simd::logical which encapsulates typed boolean values
     **/
 
     /*!
@@ -51,17 +51,17 @@ namespace boost
       @defgroup group-functions Functions and Constants
 
       Boost.SIMD functions and constants are designed to work seamlessly on both scalar
-      and SIMD parameters. Their polymorphic behavior ensure that the best available
-      implementation will be selected for any given type or types combination.
+      and SIMD parameters. Their compile-time polymorphic behavior ensures that the best available
+      implementation will be selected for any given combination of types.
 
-      Boost.SIMD functions are accessible via its eponymous include file located in the
+      Boost.SIMD functions are accessible via the header files located in the
       @c boost/simd/function folder. E.g, boost::simd::abs requires the inclusion
       of boost/simd/function/abs.hpp.
 
-      Similarly, Boost.SIMD constants are accessible via its eponymous include file located in the
+      Similarly, Boost.SIMD constants are accessible via the header files located in the
       @c boost/simd/constant folder. E.g, boost::simd::True requires the inclusion
-      of boost/simd/constant/true.hpp. Note that contrary to functions, all Boost.SIMD constants
-      name starts with a capital letter.
+      of boost/simd/constant/true.hpp. All Boost.SIMD functions begin with a lower-case 
+      letter and all Boost.SIMD constants begin with an upper-case letter.
 
       Function semantics are usually similar to the pre-existing ones, if any, with the
       following caveats:
@@ -72,7 +72,7 @@ namespace boost
       - Type promotion on result type may be disabled, i.e boost::simd::plus @c (char,char)
         returns @c char
 
-      Those changes are motivated by the fact SIMD operations require similar limitations.
+      These limitations are due to the hardware implementation of these functions.
     **/
 
      /*!
--- include/boost/simd/config.hpp
+++ include/boost/simd/config.hpp
@@ -4,7 +4,7 @@
 
   Defines various macro for controlling Boost.SIMD behavior
 
-  @copyright 2012 - 2015 NumScale SAS
+  @copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
@@ -34,14 +34,14 @@
     @ingroup group-config
     @brief Fast math behavior option
 
-    By defining this symbol or passing -ffast-math option to the compiler, Boost.SIMD mathematical
-    functions will change behaviour to align with standard definition of fast IEEE math which
-    includes:
+    By defining this symbol or passing -ffast-math option to the compiler, the behaviour of 
+    Boost.SIMD mathematical functions will be in accorandance with standard definition of 
+    fast IEEE math which includes:
 
-    - No handling for NaN values
-    - No handling for infinite values
-    - No handling for denormals
-    - No handling for -0.
+    - No handling of NaN values
+    - No handling of infinite values
+    - No handling of denormals
+    - No handling of -0.
 
     @see BOOST_SIMD_NO_NANS
     @see BOOST_SIMD_NO_MINUSZERO
@@ -59,8 +59,8 @@
     @brief NaN handling behavior option
 
     By defining this symbol, Boost.SIMD mathematical functions will ignore NaN values passed as input
-    and won't try to behave correctly if NaN are generated in intermediate computations. This option
-    usually provides increased performances at the cost of correct handling of NaN related corner
+    and will not try to behave correctly if NaN are generated in intermediate computations. This option
+    usually provides increased performance at the cost of correct handling of NaN related corner
     cases.
 
     @see BOOST_SIMD_NO_INVALIDS
@@ -77,7 +77,7 @@
     @brief Negative zero handling behavior option
 
     By defining this symbol, Boost.SIMD mathematical functions will not take care of handling zero
-    and minus zero properly. This option usually provides increased performances at the cost of
+    and minus zero correctly. This option usually provides increased performance at the cost of
     correct handling of negative zeros related corner cases.
 
     @see BOOST_SIMD_FAST_MATH
@@ -93,7 +93,7 @@
     @brief Infinites handling behavior option
 
     By defining this symbol, Boost.SIMD mathematical functions will not take care of handling
-    infinites properly. This option usually provides increased performances at the cost of
+    infinites correctly. This option usually provides increased performance at the cost of
     correct handling of infinites related corner cases.
 
     @see BOOST_SIMD_NO_INVALIDS
@@ -111,7 +111,7 @@
 
     By defining this symbol, Boost.SIMD mathematical functions will ignore denormals passed as input
     and won't try to behave correctly if denormals are generated in intermediate computations.
-    This option usually provides increased performances at the cost of correct handling of denormals
+    This option usually provides increased performance at the cost of correct handling of denormals
     related corner cases.
 
     @see BOOST_SIMD_FAST_MATH
@@ -129,7 +129,7 @@
 
     By defining this symbol, Boost.SIMD mathematical functions will ignore all invalid values
     - namely NaNs and infinites - passed as input and won't try to behave correctly if those are
-    generated in intermediate computations. This option usually provides increased performances
+    generated in intermediate computations. This option usually provides increased performance
     at the cost of correct handling of invalid related corner cases.
 
     @see BOOST_SIMD_NO_NANS
