a94914316afd4e66d82ce56776b776c450812d05
Charly Chevalier <charly.chevalier@numscale.com>
2016-05-30 19:30:47 +0200

    Check store/aligned_store don't store random integral types

--- test/function/simd/aligned_store.cpp
+++ test/function/simd/aligned_store.cpp
@@ -1,15 +1,15 @@
 //==================================================================================================
-/*!
-
-  Copyright 2015 NumScale SAS
+/**
+  Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-*/
+**/
 //==================================================================================================
-#include <boost/simd/pack.hpp>
 #include <boost/simd/function/aligned_store.hpp>
-#include <boost/align/aligned_alloc.hpp>
+#include <boost/simd/pack.hpp>
+#include <boost/align/aligned_allocator.hpp>
+#include <vector>
 #include <simd_test.hpp>
 
 namespace ba = boost::alignment;
@@ -20,10 +20,8 @@ void test(Env& $)
 {
   using p_t = bs::pack<T, N>;
 
-  std::size_t alg = p_t::alignment;
-
-  T* a1 = static_cast<T*>(ba::aligned_alloc(alg, (sizeof(T)) * N));
-  T* a2 = static_cast<T*>(ba::aligned_alloc(alg, (sizeof(T)) * N));
+  std::vector<T,ba::aligned_allocator<T,p_t::alignment>> a1(N);
+  std::vector<T,ba::aligned_allocator<T,p_t::alignment>> a2(N);
 
   for(std::size_t i = 0; i < N; ++i)
   {
@@ -34,10 +32,7 @@ void test(Env& $)
   p_t aa1(&a1[0], &a1[0]+N);
   bs::aligned_store(aa1, &a2[0]);
 
-  for(std::size_t i=0; i <N ; ++i) STF_EQUAL(a1[i], a2[i]);
-
-  ba::aligned_free(a1);
-  ba::aligned_free(a2);
+  STF_EQUAL(a1, a2);
 }
 
 STF_CASE_TPL( "Check aligned_store behavior with all types", STF_NUMERIC_TYPES )
