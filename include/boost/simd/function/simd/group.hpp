6d73a6fce601acd55fe34b0b476eaca7c64c6180
Charly Chevalier <charly.chevalier@numscale.com>
2016-06-06 18:56:02 +0200

    groups.hpp -> group.hpp
    As there is a few saturated implementations we're just putting
    them inside group.hpp directly

--- include/boost/simd/function/simd/group.hpp
+++ include/boost/simd/function/simd/group.hpp
@@ -18,13 +18,13 @@
 #    include <boost/simd/arch/x86/sse2/simd/function/group.hpp>
 #  endif
 #  if BOOST_HW_SIMD_X86 >= BOOST_HW_SIMD_X86_SSE4_1_VERSION
-#    include <boost/simd/arch/x86/sse4_1/simd/function/groups.hpp>
+#    include <boost/simd/arch/x86/sse4_1/simd/function/group.hpp>
 #  endif
 #  if BOOST_HW_SIMD_X86 >= BOOST_HW_SIMD_X86_AVX_VERSION
 #    include <boost/simd/arch/x86/avx/simd/function/group.hpp>
 #  endif
 #  if BOOST_HW_SIMD_X86 >= BOOST_HW_SIMD_X86_AVX2_VERSION
-#    include <boost/simd/arch/x86/avx2/simd/function/groups.hpp>
+#    include <boost/simd/arch/x86/avx2/simd/function/group.hpp>
 #  endif
 #endif
 
