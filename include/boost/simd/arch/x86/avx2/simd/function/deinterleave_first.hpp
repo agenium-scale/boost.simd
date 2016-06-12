43fde321e808aff9f293206c472491d93dd233e0
Charly Chevalier <charly.chevalier@numscale.com>
2016-06-12 18:39:32 +0200

    Use bitwise_cast deinterleave_first

--- include/boost/simd/arch/x86/avx2/simd/function/deinterleave_first.hpp
+++ include/boost/simd/arch/x86/avx2/simd/function/deinterleave_first.hpp
@@ -1,16 +1,17 @@
 //==================================================================================================
 /**
   Copyright 2016 Numscale SAS
-  Copyright 2016 J.T.Lapreste
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-*/
+**/
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_X86_AVX2_SIMD_FUNCTION_DEINTERLEAVE_FIRST_HPP_INCLUDED
 #define BOOST_SIMD_ARCH_X86_AVX2_SIMD_FUNCTION_DEINTERLEAVE_FIRST_HPP_INCLUDED
 
 #include <boost/simd/detail/overload.hpp>
+#include <boost/simd/function/slice.hpp>
+#include <boost/simd/function/bitwise_cast.hpp>
 
 namespace boost { namespace simd { namespace ext
 {
