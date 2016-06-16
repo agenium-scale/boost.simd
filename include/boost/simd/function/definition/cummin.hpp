30947edd8e0dcee5312fd8cd11c4a11cf61f9fec
Charly Chevalier <charly.chevalier@numscale.com>
2016-06-16 22:42:08 +0200

    Implementation for cummin

--- include/boost/simd/function/definition/cummin.hpp
+++ include/boost/simd/function/definition/cummin.hpp
@@ -1,18 +1,17 @@
 //==================================================================================================
-/*!
-  @file
-
-  @copyright 2016 NumScale SAS
-  @copyright 2016 J.T.Lapreste
+/**
+  Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-*/
+**/
 //==================================================================================================
 #ifndef BOOST_SIMD_FUNCTION_DEFINITION_CUMMIN_HPP_INCLUDED
 #define BOOST_SIMD_FUNCTION_DEFINITION_CUMMIN_HPP_INCLUDED
 
 #include <boost/simd/config.hpp>
+#include <boost/simd/function/min.hpp>
+#include <boost/simd/constant/valmax.hpp>
 #include <boost/simd/detail/dispatch/function/make_callable.hpp>
 #include <boost/simd/detail/dispatch/hierarchy/functions.hpp>
 #include <boost/simd/detail/dispatch.hpp>
@@ -21,7 +20,9 @@ namespace boost { namespace simd
 {
   namespace tag
   {
-    BOOST_DISPATCH_MAKE_TAG(ext, cummin_, boost::dispatch::elementwise_<cummin_>);
+    BOOST_DISPATCH_MAKE_TAG ( ext, cummin_
+                            , (boost::dispatch::cumulative_<cummin_,min_,valmax_>)
+                            );
   }
 
   namespace ext
