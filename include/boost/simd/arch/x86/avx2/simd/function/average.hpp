bc9c8021516774b5f6b70d34d6fb9ab8b819bfff
jtlap <jtlapreste@gmail.com>
2016-06-23 16:28:48 +0200

    average sse2/2 avx2

---
+++ include/boost/simd/arch/x86/avx2/simd/function/average.hpp
@@ -0,0 +1,46 @@
+//==================================================================================================
+/*!
+    @file
+
+    @Copyright 2016 Numscale SAS
+    @copyright 2016 J.T.Lapreste
+
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_ARCH_X86_AVX2_SIMD_FUNCTION_BITWISE_AND_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_X86_AVX2_SIMD_FUNCTION_BITWISE_AND_HPP_INCLUDED
+#include <boost/simd/detail/overload.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  namespace bd =  boost::dispatch;
+  BOOST_DISPATCH_OVERLOAD ( bitwise_and_
+                          , (typename A0, typename A1)
+                          , bs::avx2_
+                          , bs::pack_<bd::uint8_<A0>, bs::avx_>
+                          , bs::pack_<bd::uint8_<A1>, bs::avx_>
+                         )
+  {
+    BOOST_FORCEINLINE A0 operator()( const A0 & a0, const A1 & a1 ) const BOOST_NOEXCEPT
+    {
+      return _mm256_avg_epu8(a0, a1);
+    }
+  };
+
+  BOOST_DISPATCH_OVERLOAD ( bitwise_and_
+                          , (typename A0)
+                          , bs::avx2_
+                          , bs::pack_<bd::uint16_<A0>, bs::avx_>
+                          , bs::pack_<bd::uint16_<A0>, bs::avx_>
+                         )
+  {
+    BOOST_FORCEINLINE A0 operator()( const A0 & a0, const A0 & a1 ) const BOOST_NOEXCEPT
+    {
+      return _mm256_avg_epu16(a0, a1);
+    }
+  };
+} } }
+
+#endif
