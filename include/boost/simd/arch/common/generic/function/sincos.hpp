b1460b7f07406c289cd6d6c358af2e2e22d69282
jtlap <jtlapreste@gmail.com>
2016-06-28 10:38:31 +0200

    sincos/d/pi lacked restricted version

--- include/boost/simd/arch/common/generic/function/sincos.hpp
+++ include/boost/simd/arch/common/generic/function/sincos.hpp
@@ -14,6 +14,7 @@
 
 
 #include <boost/simd/arch/common/detail/generic/trigo.hpp>
+#include <boost/simd/function/restricted.hpp>
 #include <boost/simd/meta/is_not_scalar.hpp>
 #include <boost/simd/detail/dispatch/function/overload.hpp>
 #include <boost/config.hpp>
@@ -30,13 +31,24 @@ namespace boost { namespace simd { namespace ext
                           , bd::generic_< bd::floating_<A0> >
                           )
   {
-    using result_t = std::pair<A0, A0>                     ;
-    BOOST_FORCEINLINE result_t operator() ( A0 const& a0) const
+    BOOST_FORCEINLINE std::pair<A0, A0> operator() ( A0 const& a0) const
     {
       return detail::trig_base <A0,tag::radian_tag,is_not_scalar_t<A0>,tag::big_tag>::sincosa(a0);
     }
   };
 
+  BOOST_DISPATCH_OVERLOAD ( sincos_
+                          , (typename A0)
+                          , bd::cpu_
+                          , bs::restricted_tag
+                          , bd::generic_< bd::floating_<A0> >
+                          )
+  {
+    BOOST_FORCEINLINE std::pair<A0, A0> operator() (const restricted_tag &,  A0 const& a0) const BOOST_NOEXCEPT
+    {
+      return detail::trig_base<A0, tag::radian_tag,is_not_scalar_t<A0>,tag::clipped_pio4_tag>::sincosa(a0);
+    }
+  };
 } } }
 
 
