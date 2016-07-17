9fd96bf78936ed5a98b8f38f575bdefa77a4cb47
jtlap <jtlapreste@gmail.com>
2016-07-17 14:36:59 +0200

    more hyperblolic benches

---
+++ bench/function/scalar/tanh.cpp
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2016 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+#include <simd_bench.hpp>
+#include <boost/simd/function/simd/tanh.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SCALAR_BENCH(scalar_tanh, bs::tanh);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_tanh, NS_BENCH_IEEE_TYPES>(-10, 10);
+}
---
+++ bench/function/simd/sinh.cpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2016 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+#include <simd_bench.hpp>
+#include <boost/simd/function/simd/sinh.hpp>
+#include <boost/simd/pack.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SIMD_BENCH(simd_sinh, bs::sinh);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<simd_sinh, NS_BENCH_IEEE_TYPES>(-10, 10);
+}
