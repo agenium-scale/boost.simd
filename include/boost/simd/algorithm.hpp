5132c2d7272bbe0e2751fffbc7c25a270978bdd8
Joel Falcou <joel.falcou@lri.fr>
2016-08-17 13:42:36 +0200

    Implement simd::reduce

---
+++ include/boost/simd/algorithm.hpp
@@ -0,0 +1,24 @@
+//==================================================================================================
+/**
+  Copyright 2016 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+**/
+//==================================================================================================
+#ifndef BOOST_SIMD_ALGORITHM_HPP_INCLUDED
+#define BOOST_SIMD_ALGORITHM_HPP_INCLUDED
+
+
+namespace boost { namespace simd
+{
+  /*!
+    @ingroup group-api
+    @defgroup group-std Algorithms and Ranges
+
+    SIMD-aware Range adaptors and implementation of standard algorithms;
+  **/
+} }
+#include <boost/simd/algorithm/reduce.hpp>
+
+#endif
