f6db4f501389502726ec934544f8759212e0ab10
Alan Kelly <alankelly@live.com>
2016-08-19 13:37:48 +0200

    update copyright to 2016

--- include/boost/simd/detail/dispatch/function/overload.hpp
+++ include/boost/simd/detail/dispatch/function/overload.hpp
@@ -4,7 +4,7 @@
 
   Provides macros for function overload definition and declaration
 
-  @copyright 2015 NumScale SAS
+  @copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
--- test/stf.hpp
+++ test/stf.hpp
@@ -2,7 +2,7 @@
 /*!
   @file
   Main header for the unit test system
-  @copyright 2015 Joel Falcou
+  @copyright 2016 Joel Falcou
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 **/
