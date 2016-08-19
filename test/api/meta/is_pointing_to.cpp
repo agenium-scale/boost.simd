359dd9c029b699ddae2b38cbed9a0fa843926422
Alan Kelly <alankelly@live.com>
2016-08-19 13:39:46 +0200

    remove references to LASMEA

--- test/api/meta/is_pointing_to.cpp
+++ test/api/meta/is_pointing_to.cpp
@@ -1,11 +1,12 @@
-//==============================================================================
-//         Copyright 2003 - 2012   LASMEA UMR 6602 CNRS/Univ. Clermont II
-//         Copyright 2009 - 2012   LRI    UMR 8623 CNRS/Univ Paris Sud XI
-//
-//          Distributed under the Boost Software License, Version 1.0.
-//                 See accompanying file LICENSE.txt or copy at
-//                     http://www.boost.org/LICENSE_1_0.txt
-//==============================================================================
+//==================================================================================================
+/*!
+
+  Copyright 2016 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
 #include <boost/simd/meta/is_pointing_to.hpp>
 #include <simd_test.hpp>
 #include <list>
