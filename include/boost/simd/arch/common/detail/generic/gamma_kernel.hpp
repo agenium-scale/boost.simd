b58e851dda2f63436e7996056b868036ecce7ac1
Alan Kelly <alankelly@live.com>
2016-08-19 16:34:45 +0200

    correct more copyrights

--- include/boost/simd/arch/common/detail/generic/gamma_kernel.hpp
+++ include/boost/simd/arch/common/detail/generic/gamma_kernel.hpp
@@ -1,4 +1,5 @@
 //==============================================================================
+//          Copyright 2016   NumScale SAS
 //
 //          Distributed under the Boost Software License, Version 1.0.
 //                 See accompanying file LICENSE.txt or copy at
