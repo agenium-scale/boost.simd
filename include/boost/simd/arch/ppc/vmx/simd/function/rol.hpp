9302263ca9922cad7e6e7fb761a6189099c85ace
Charly Chevalier <charly.chevalier@numscale.com>
2016-08-25 16:20:06 +0200

    Fix rol include + Fix ambigous call to rol for vmx

--- include/boost/simd/arch/ppc/vmx/simd/function/rol.hpp
+++ include/boost/simd/arch/ppc/vmx/simd/function/rol.hpp
@@ -12,7 +12,7 @@
 #define BOOST_SIMD_ARCH_PPC_VMX_SIMD_FUNCTION_ROL_HPP_INCLUDED
 
 #include <boost/simd/detail/overload.hpp>
-#include <boost/simd/operator/function/details/assert_utils.hpp>
+#include <boost/simd/detail/assert_utils.hpp>
 #include <boost/simd/function/simd/bitwise_cast.hpp>
 #include <boost/simd/detail/dispatch/meta/as_unsigned.hpp>
 
@@ -23,15 +23,14 @@ namespace boost { namespace simd { namespace ext
    BOOST_DISPATCH_OVERLOAD( rol_
                           , (typename A0)
                           , bs::vmx_
-                          , bs::pack_< bd::integer_<A0>, bs::vmx_>
-                          , bs::pack_< bd::integer_<A0>, bs::vmx_>
+                          , bs::pack_< bd::unsigned_<A0>, bs::vmx_>
+                          , bs::pack_< bd::unsigned_<A0>, bs::vmx_>
                           )
    {
      BOOST_FORCEINLINE A0 operator()(A0 const& a0, A0 const& a1) const
       {
         BOOST_ASSERT_MSG(assert_good_shift<A0>(a1), "rol : rotation is out of range");
-        using c_t =  bd::as_unsigned_t<A0>;
-        return vec_rl(a0.storage(), bitwise_cast<c_t>(a1)());
+        return vec_rl(a0.storage(), a1.storage());
       }
    };
 
