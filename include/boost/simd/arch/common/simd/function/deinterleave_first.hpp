b8458ae8a23ebc307b7e3ab5d2ac15d85968d16b
Joel Falcou <joel.falcou@lri.fr>
2016-08-30 14:18:47 +0200

    Add deinterleave function
    deinterleave(x,y) now returns an array containing
    both deinterleave_first and _second call over x
    and y. Implementation is made to be optimized in
    number of operations as far as possible.

--- include/boost/simd/arch/common/simd/function/deinterleave_first.hpp
+++ include/boost/simd/arch/common/simd/function/deinterleave_first.hpp
@@ -38,9 +38,9 @@ namespace boost { namespace simd { namespace ext
       return make<T>( bs::extract<N::value*2>(x)..., bs::extract<N::value*2>(y)... );
     }
 
-    template<typename... N> static BOOST_FORCEINLINE
+    template<typename N0, typename N1, typename... Ns> static BOOST_FORCEINLINE
     typename T::storage_type
-    do_( T const& x, T const& y, aggregate_storage const&, br::list<N...> const&) BOOST_NOEXCEPT
+    do_( T const& x, T const& y, aggregate_storage const&, br::list<N0,N1,Ns...> const&) BOOST_NOEXCEPT
     {
       return  { { deinterleave_first(x.storage()[0],x.storage()[1])
                 , deinterleave_first(y.storage()[0],y.storage()[1])
--- include/boost/simd/arch/common/simd/function/deinterleave_second.hpp
+++ include/boost/simd/arch/common/simd/function/deinterleave_second.hpp
@@ -36,9 +36,9 @@ namespace boost { namespace simd { namespace ext
       return make<T>( bs::extract<N::value*2+1>(x)..., bs::extract<N::value*2+1>(y)... );
     }
 
-    template<typename... N> static BOOST_FORCEINLINE
+    template<typename N0, typename N1, typename... Ns> static BOOST_FORCEINLINE
     typename T::storage_type
-    do_( T const& x, T const& y, aggregate_storage const&, br::list<N...> const&) BOOST_NOEXCEPT
+    do_( T const& x, T const& y, aggregate_storage const&, br::list<N0,N1,Ns...> const&) BOOST_NOEXCEPT
     {
       return  { { deinterleave_second(x.storage()[0],x.storage()[1])
                 , deinterleave_second(y.storage()[0],y.storage()[1])
