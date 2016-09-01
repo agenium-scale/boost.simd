1f1ab40ac8d394d2db232c5c611d292495bbf483
Charly Chevalier <charly.chevalier@numscale.com>
2016-09-01 10:51:41 +0200

    Let the compiler generates vmx multiplies for ints32_

--- include/boost/simd/arch/ppc/vmx/simd/function/multiplies.hpp
+++ include/boost/simd/arch/ppc/vmx/simd/function/multiplies.hpp
@@ -14,6 +14,7 @@
 #include <boost/simd/constant/mzero.hpp>
 #include <boost/simd/constant/zero.hpp>
 #include <boost/simd/detail/dispatch/meta/as_unsigned.hpp>
+#include <boost/predef/compiler.h>
 
 namespace boost { namespace simd { namespace ext
 {
@@ -61,37 +62,20 @@ namespace boost { namespace simd { namespace ext
     }
   };
 
+  #if BOOST_COMP_CLANG || BOOST_COMP_GNUC
   BOOST_DISPATCH_OVERLOAD ( multiplies_
                           , (typename A0)
                           , bs::vmx_
-                          , bs::pack_<bd::uint32_<A0>, bs::vmx_>
-                          , bs::pack_<bd::uint32_<A0>, bs::vmx_>
+                          , bs::pack_<bd::ints32_<A0>, bs::vmx_>
+                          , bs::pack_<bd::ints32_<A0>, bs::vmx_>
                           )
   {
     BOOST_FORCEINLINE A0 operator()(const A0& a0, const A0& a1) const BOOST_NOEXCEPT
     {
-      using s_t =  bd::downgrade_t<A0,unsigned>;
-
-      // reinterpret as u16
-      s_t short0 = bitwise_cast<s_t>(a0);
-      s_t short1 = bitwise_cast<s_t>(a1);
-
-      // shifting constant
-      auto shift_ = vec_splat_u32(-16);
-      A0 sf = vec_rl(a1.storage(), shift_);
-
-      // Compute high part of the product
-      A0 high    = vec_msum ( short0.storage()
-                            , bitwise_cast<s_t>(sf).storage()
-                            , vec_splat_u32(0)
-                            );
-
-      // Complete by adding low part of the 16 bits product
-      return vec_add( vec_sl(high.storage(), shift_)
-                    , vec_mulo(short0.storage(), short1.storage())
-                    );
+      return a0.storage() * a1.storage();
     }
   };
+  #endif
 } } }
 
 #endif
