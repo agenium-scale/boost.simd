4a1ba2bb7a8b2725ca47161b9eabc604d95f9bc5
Joel Falcou <joel.falcou@lri.fr>
2016-09-08 11:16:16 +0200

    More group/saturated group implementations

--- include/boost/simd/arch/x86/avx/simd/function/group.hpp
+++ include/boost/simd/arch/x86/avx/simd/function/group.hpp
@@ -12,6 +12,7 @@
 #include <boost/simd/detail/overload.hpp>
 #include <boost/simd/function/combine.hpp>
 #include <boost/simd/function/slice.hpp>
+#include <boost/simd/function/saturated.hpp>
 #include <boost/simd/detail/dispatch/meta/downgrade.hpp>
 
 namespace boost { namespace simd { namespace ext
@@ -49,6 +50,27 @@ namespace boost { namespace simd { namespace ext
         auto a0x = slice(a0), a1x = slice(a1);
         auto v0  = group(a0x[0], a0x[1]);
         auto v1  = group(a1x[0], a1x[1]);
+
+        return _mm256_insertf128_si256(_mm256_castsi128_si256(v0), v1, 1);
+      }
+   };
+
+   BOOST_DISPATCH_OVERLOAD( group_
+                          , (typename A0)
+                          , bs::avx_
+                          , bs::saturated_tag
+                          , bs::pack_<bd::integer_<A0>, avx_>
+                          , bs::pack_<bd::integer_<A0>, avx_>
+                          )
+   {
+      using result = bd::downgrade_t<A0>;
+
+      BOOST_FORCEINLINE
+      result operator()(bs::saturated_tag const&, const A0& a0, const A0& a1) const BOOST_NOEXCEPT
+      {
+        auto a0x = slice(a0), a1x = slice(a1);
+        auto v0  = saturated_(group)(a0x[0], a0x[1]);
+        auto v1  = saturated_(group)(a1x[0], a1x[1]);
 
         return _mm256_insertf128_si256(_mm256_castsi128_si256(v0), v1, 1);
       }
