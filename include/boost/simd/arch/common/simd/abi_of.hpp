1e7a4a6b154b64aee7fd5296323bdf82c2fe8151
Joel Falcou <joel.falcou@lri.fr>
2016-09-12 16:49:20 +0200

    Remove more unneeded doc

--- include/boost/simd/arch/common/simd/abi_of.hpp
+++ include/boost/simd/arch/common/simd/abi_of.hpp
@@ -1,12 +1,9 @@
 //==================================================================================================
-/*!
-  @file
-
-  @copyright 2016 NumScale SAS
+/**
+  Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-
 **/
 //==================================================================================================
 #ifndef BOOST_SIMD_ARCH_COMMON_SIMD_ABI_OF_HPP_INCLUDED
@@ -17,17 +14,7 @@
 
 namespace boost { namespace simd { namespace ext
 {
-  /*!
-    @ingroup  group-ext
-    @brief    Type/Cardinal to ABI mapping extension point
-
-    Provide a customizable extension point for SIMD ABI to type mapping.
-
-    @tparam Type      Type of the elements
-    @tparam Cardinal  Number of elements in the register
-  **/
-  template<typename Type, std::size_t Cardinal, typename Enable = void>
-  struct abi_of
+  template<typename Type, std::size_t Cardinal, typename Enable = void> struct abi_of
   {
     using type = boost::simd::simd_emulation_;
   };
