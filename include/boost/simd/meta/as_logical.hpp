74aed5a0564500ad718986698084ba43126eb0fa
Joel Falcou <joel.falcou@lri.fr>
2016-09-23 00:57:50 +0200

    Cosmetic on as_logical

--- include/boost/simd/meta/as_logical.hpp
+++ include/boost/simd/meta/as_logical.hpp
@@ -1,14 +1,10 @@
 //==================================================================================================
 /*!
   @file
-
-  Defines the as_logical meta-function
-
   @copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-
 **/
 //==================================================================================================
 #ifndef BOOST_SIMD_META_AS_LOGICAL_HPP_INCLUDED
@@ -23,7 +19,7 @@
 
 namespace boost { namespace simd
 {
-  namespace details
+  namespace detail
   {
     template<typename T, typename F>
     struct  as_logical
@@ -46,24 +42,26 @@ namespace boost { namespace simd
 
   /*!
     @ingroup  group-api
-    @brief    COnvert types to a logical type
+    @brief    Convert type to its logical type counterpart
 
-    For a given type @c T , provides a type of same structure but able to fit a logical value.
-    Provisions are taken so that conversion of logical and bool types is idempotent.
+    For a given type @c T , provides a type of same structure but able to store a logical value.
+    Provisions are taken so that conversion of logical and @c bool types is idempotent.
 
-    @tparam T      Type to convert
+    @tparam T Type to convert
   **/
   template<typename T>
   struct  as_logical
-        : details::as_logical < real_of_t<T>
-                              , dispatch::factory_of<T,dispatch::scalar_of_t<T>>
-                              >
+        : detail::as_logical< real_of_t<T>
+                            , dispatch::factory_of<T,dispatch::scalar_of_t<T>>
+                            >
   {
   };
 
   /*!
     @ingroup  group-api
-    @brief Eager short-cut to as_logical meta-function
+    @brief Eager short-cut to the as_logical meta-function
+
+    @tparam T Type to convert
   **/
   template<typename T> using as_logical_t = typename as_logical<T>::type;
 } }
