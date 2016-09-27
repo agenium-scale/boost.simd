6ef34e7494e090fc121bbf8f3c9a5c1c0d78b2f0
jtlap <jtlapreste@gmail.com>
2016-09-27 20:30:33 +0200

    correction to rem_2pi

--- include/boost/simd/arch/common/simd/function/rem_2pi.hpp
+++ include/boost/simd/arch/common/simd/function/rem_2pi.hpp
@@ -64,7 +64,7 @@ namespace boost { namespace simd { namespace ext
                           , (detail::is_native<X>)
                           , bd::cpu_
                           , bs::pack_ <bd::floating_<A0>, X>
-                          , bd::target_ <bd::unspecified_<A1>, X>
+                          , bd::target_ <bd::unspecified_<A1>>, X>
                           )
   {
     BOOST_FORCEINLINE A0 operator() ( A0 const& a0, A1 const&) const BOOST_NOEXCEPT
