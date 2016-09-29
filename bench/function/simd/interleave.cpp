1f17602fbdb34d7fe2e3e5371e6aada733a6dfdd
Alan Kelly <alankelly@live.com>
2016-09-29 17:01:19 +0200

    Adds benchmark for ddeinterleave and interleave

---
+++ bench/function/simd/interleave.cpp
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2016 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+#include <simd_bench.hpp>
+#include <boost/simd/function/simd/interleave.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SCALAR_BENCH(scalar_interleave, bs::interleave);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_interleave, NS_BENCH_IEEE_TYPES>(-10, 10,-10, 10);
+}
