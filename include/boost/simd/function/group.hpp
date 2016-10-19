90d54131d683e1200770cba6141753e7d70afd8c
Joel Falcou <joel.falcou@lri.fr>
2016-10-19 16:17:25 +0200

    Cosmetic

--- include/boost/simd/function/group.hpp
+++ include/boost/simd/function/group.hpp
@@ -1,10 +1,11 @@
 //==================================================================================================
-/**
-  Copyright 2016 NumScale SAS
+/*!
+  @file
+  @copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-**/
+*/
 //==================================================================================================
 #ifndef BOOST_SIMD_FUNCTION_GROUP_HPP_INCLUDED
 #define BOOST_SIMD_FUNCTION_GROUP_HPP_INCLUDED
@@ -15,10 +16,10 @@ namespace boost { namespace simd
   /*!
     @ingroup group-swar
     This function object aggregates two boost::simd::pack of basic type @c T and cardinal @c N into
-    a single boost::simd::pack of cardinal @'c 2*N and using a type twice smaller than @c T.
+    a single boost::simd::pack of cardinal @c 2*N and using a type twice smaller than @c T.
 
-    Conversion of values during this operations are made using the C conversion rules and may
-    induce roll over.
+    Conversion of values during this operations are made using the C language conversion rules and
+    may induce roll over.
 
     @par Semantic:
     For any type @c T and integral constant @c N, consider two value @c x and @c y of type
