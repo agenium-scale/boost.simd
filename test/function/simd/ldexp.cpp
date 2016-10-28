8e02fce16674cfa656e8e58e234dfb3daaaca17e
Joel Falcou <joel.falcou@lri.fr>
2016-10-28 19:21:56 +0200

    Reinforce some IEEE related tests (#303)

--- test/function/simd/ldexp.cpp
+++ test/function/simd/ldexp.cpp
@@ -8,19 +8,19 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#include <boost/simd/pack.hpp>
 #include <boost/simd/function/ldexp.hpp>
 #include <boost/simd/function/std.hpp>
-#include <boost/simd/meta/cardinal_of.hpp>
-#include <boost/simd/logical.hpp>
+#include <boost/simd/function/fast.hpp>
+#include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
 #include <boost/simd/detail/dispatch/meta/as_integer.hpp>
+
+namespace bs = boost::simd;
+namespace bd = boost::dispatch;
 
 template <typename T, std::size_t N, typename Env>
 void test(Env& $)
 {
-  namespace bs = boost::simd;
-  namespace bd = boost::dispatch;
   using iT = bd::as_integer_t<T>;
   using p_t = bs::pack<T, N>;
   using pi_t = bs::pack<iT, N>;
@@ -29,59 +29,56 @@ void test(Env& $)
   iT a2[N];
   for(std::size_t i = 0; i < N; ++i)
   {
-     a1[i] = (i%2) ? T(i) : T(-i);
-     a2[i] = i%(sizeof(T)*8-1);
-     b[i] = bs::ldexp(a1[i], a2[i]);
-     c[i] = bs::std_(bs::ldexp)(a1[i], a2[i]);
-     d[i] = bs::fast_(bs::ldexp)(a1[i], a2[i]);
-   }
+    a1[i] = (i%2) ? T(i) : T(-1.*i);
+    a2[i] = i%(sizeof(T)*8-1);
+    b[i] = bs::ldexp(a1[i], a2[i]);
+    c[i] = bs::std_(bs::ldexp)(a1[i], a2[i]);
+    d[i] = bs::fast_(bs::ldexp)(a1[i], a2[i]);
+  }
+
   p_t aa1(&a1[0], &a1[0]+N);
   pi_t aa2(&a2[0], &a2[0]+N);
   p_t bb(&b[0], &b[0]+N);
   p_t cc(&c[0], &c[0]+N);
   p_t dd(&d[0], &d[0]+N);
-  STF_IEEE_EQUAL(bs::ldexp(aa1, aa2), bb);
-  STF_IEEE_EQUAL(bs::std_(bs::ldexp)(aa1, aa2), cc);
-  STF_IEEE_EQUAL(bs::fast_(bs::ldexp)(aa1, aa2), dd);
+
+  STF_IEEE_EQUAL(bs::ldexp(aa1, aa2)            , bb);
+  STF_IEEE_EQUAL(bs::std_(bs::ldexp)(aa1, aa2)  , cc);
+  STF_IEEE_EQUAL(bs::fast_(bs::ldexp)(aa1, aa2) , dd);
 }
 
 STF_CASE_TPL("Check ldexp on pack" , STF_NUMERIC_TYPES)
 {
-  namespace bs = boost::simd;
-  using p_t = bs::pack<T>;
-  static const std::size_t N = bs::cardinal_of<p_t>::value;
+  static const std::size_t N = bs::pack<T>::static_size;
   test<T, N>($);
   test<T, N/2>($);
   test<T, N*2>($);
 }
+
 template <typename T, std::size_t N, typename Env>
 void tests(Env& $)
 {
-  namespace bs = boost::simd;
-  namespace bd = boost::dispatch;
   using p_t = bs::pack<T, N>;
 
-  T a1[N],  b[N], c[N], d[N];
+  T a1[N],  b[N], c[N];
   for(std::size_t i = 0; i < N; ++i)
   {
-     a1[i] = (i%2) ? T(i) : T(-i);
-     b[i] = bs::ldexp(a1[i], 2);
-     c[i] = bs::std_(bs::ldexp)(a1[i], 2);
-     d[i] = bs::fast_(bs::ldexp)(a1[i], 2);
-   }
+    a1[i] = (i%2) ? T(i) : T(-1.*i);
+    b[i] = bs::ldexp(a1[i], 2);
+    c[i] = bs::fast_(bs::ldexp)(a1[i], 2);
+  }
+
   p_t aa1(&a1[0], &a1[0]+N);
   p_t bb(&b[0], &b[0]+N);
   p_t cc(&c[0], &c[0]+N);
-  p_t dd(&d[0], &d[0]+N);
-  STF_IEEE_EQUAL(bs::ldexp(aa1, 2), bb);
-  STF_IEEE_EQUAL(bs::fast_(bs::ldexp)(aa1, 2), dd);
+
+  STF_IEEE_EQUAL(bs::ldexp(aa1, 2)            , bb);
+  STF_IEEE_EQUAL(bs::fast_(bs::ldexp)(aa1, 2) , cc);
 }
 
-STF_CASE_TPL("Check ldexp on pack and scal" , STF_NUMERIC_TYPES)
+STF_CASE_TPL("Check ldexp on pack/scalar" , STF_NUMERIC_TYPES)
 {
-  namespace bs = boost::simd;
-  using p_t = bs::pack<T>;
-  static const std::size_t N = bs::cardinal_of<p_t>::value;
+  static const std::size_t N = bs::pack<T>::static_size;
   tests<T, N>($);
   tests<T, N/2>($);
   tests<T, N*2>($);
