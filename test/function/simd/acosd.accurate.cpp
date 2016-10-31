2ff12c4b42044392e530901aa2c5fe72bd490d0e
jtlap <jtlapreste@gmail.com>
2016-10-31 21:29:13 +0100

    acos bug confusing accurate and regular in some cases

--- test/function/simd/acosd.accurate.cpp
+++ test/function/simd/acosd.accurate.cpp
@@ -9,7 +9,14 @@
 #include <simd_test.hpp>
 #include <boost/simd/function/acosd.hpp>
 #include <boost/simd/pack.hpp>
-#include <boost/simd/function/std.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/pi.hpp>
+#include <boost/simd/constant/four.hpp>
 
 
 namespace bs = boost::simd;
@@ -40,3 +47,27 @@ STF_CASE_TPL("Check acosd on pack" , STF_IEEE_TYPES)
   test<T, N*2>($);
 }
 
+STF_CASE_TPL ("accurate  acosd", STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using p_t = bs::pack<T>;
+  using bs::acosd;
+
+  using r_t = decltype(bs::accurate_(bs::acosd)(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Inf<p_t>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Minf<p_t>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Nan<p_t>()), bs::Nan<r_t>(), 0);
+#endif
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Half<p_t>()), r_t(60), 0.5);
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Mhalf<p_t>()), r_t(120), 0.5);
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Mone<p_t>()), r_t(180), 0.5);
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::One<p_t>()), bs::Zero<r_t>(), 0.5);
+  STF_ULP_EQUAL(bs::accurate_(bs::acosd)(bs::Zero<p_t>()), r_t(90), 0.5);
+}
