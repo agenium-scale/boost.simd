eafa7c5fdc3914f151c5cf2e40c0209b45ad3905
jtlap <jtlapreste@gmail.com>
2016-11-01 17:34:20 +0100

    adding special values tests for simd 6 (bxxx.cpp)

--- test/function/simd/bitfloating.cpp
+++ test/function/simd/bitfloating.cpp
@@ -12,6 +12,10 @@
 #include <boost/simd/function/bitfloating.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/bitincrement.hpp>
 
 template <typename T, int N, typename Env>
 void test(Env& $)
@@ -44,4 +48,37 @@ STF_CASE_TPL("Check bitfloating on pack" , (uint32_t)(uint64_t)(int32_t)(int64_t
   test<T, N>($);
   test<T, N/2>($);
   test<T, N*2>($);
+}
+
+STF_CASE_TPL (" bitfloating int_convert",  (int32_t)(int64_t))
+{
+  namespace bs = boost::simd;
+  using bs::bitfloating;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(bitfloating(p_t()));
+  typedef  boost::dispatch::as_floating_t<p_t>  wished_r_t;
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, wished_r_t);
+
+  // specific values tests
+  STF_EQUAL(bitfloating(bs::One<p_t>()), bs::Bitincrement<r_t>());
+  STF_EQUAL(bitfloating(bs::Mone<p_t>()), -bs::Bitincrement<r_t>());
+  STF_EQUAL(bitfloating(bs::Zero<p_t>()), bs::Zero<r_t>());
+}
+
+STF_CASE_TPL (" bitfloating uint_convert",  (uint32_t)(uint64_t))
+{
+  namespace bs = boost::simd;
+  using bs::bitfloating;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(bitfloating(p_t()));
+  typedef boost::dispatch::as_floating_t<p_t>  wished_r_t;
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, wished_r_t);
+
+  // specific values tests
+  STF_EQUAL(bitfloating(bs::One<p_t>()), bs::Bitincrement<r_t>());
+  STF_EQUAL(bitfloating(bs::Zero<p_t>()), bs::Zero<r_t>());
 }
--- test/function/simd/bitofsign.cpp
+++ test/function/simd/bitofsign.cpp
@@ -13,6 +13,13 @@
 #include <boost/simd/function/bits.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
 
 template <typename T, std::size_t N, typename Env>
 void test(Env& $)
@@ -40,3 +47,56 @@ STF_CASE_TPL("Check bitofsign on pack" , STF_IEEE_TYPES)
   test<T, N/2>($);
   test<T, N*2>($);
 }
+
+STF_CASE_TPL (" bitofsign real",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  using bs::bitofsign;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(bitofsign(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_EQUAL(bitofsign(bs::Inf<p_t>()), bs::Zero<r_t>());
+  STF_EQUAL(bitofsign(bs::Minf<p_t>()), bs::Mzero<r_t>());
+  STF_IEEE_EQUAL(bitofsign(bs::Nan<p_t>()), bs::Mzero<r_t>());
+#endif
+  STF_EQUAL(bitofsign(bs::Mzero<p_t>()), bs::Mzero<r_t>());
+  STF_EQUAL(bitofsign(bs::One<p_t>()), bs::Zero<r_t>());
+  STF_EQUAL(bitofsign(bs::Zero<p_t>()), bs::Zero<r_t>());
+}
+
+STF_CASE_TPL (" bitofsign unsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  using bs::bitofsign;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(bitofsign(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+  STF_EQUAL(bitofsign(bs::One<p_t>()), bs::Zero<r_t>());
+}
+
+STF_CASE_TPL (" bitofsign signed_int",  STF_SIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  using bs::bitofsign;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(bitofsign(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, T);
+
+  // specific values tests
+  STF_EQUAL(bitofsign(bs::Mone<p_t>()), r_t(1ull << (sizeof(T)*8-1)));
+  STF_EQUAL(bitofsign(bs::One<p_t>()), bs::Zero<r_t>());
+  STF_EQUAL(bitofsign(bs::Zero<p_t>()), bs::Zero<r_t>());
+}
+
+
