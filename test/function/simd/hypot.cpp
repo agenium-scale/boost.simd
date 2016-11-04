ecf8de2b64c3496c7aeb6e156601885682278792
jtlap <jtlapreste@gmail.com>
2016-11-04 18:06:23 +0100

    adding special values tests for simd 16 hypot ldexp)

--- test/function/simd/hypot.cpp
+++ test/function/simd/hypot.cpp
@@ -12,6 +12,13 @@
 #include <boost/simd/function/hypot.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/sqrt_2.hpp>
 
 template <typename T, std::size_t N, typename Env>
 void test(Env& $)
@@ -42,3 +49,32 @@ STF_CASE_TPL("Check hypot on pack" , STF_IEEE_TYPES)
   test<T, N/2>($);
   test<T, N*2>($);
 }
+
+
+
+STF_CASE_TPL (" hypot real",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::hypot;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(hypot(p_t(), p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_ULP_EQUAL(hypot(bs::Inf<p_t>(), bs::Inf<p_t>()), bs::Inf<r_t>(), 0);
+  STF_ULP_EQUAL(hypot(bs::Minf<p_t>(), bs::Minf<p_t>()), bs::Inf<r_t>(), 0);
+  STF_ULP_EQUAL(hypot(bs::Nan<p_t>(), bs::Nan<p_t>()), bs::Nan<r_t>(), 0);
+  STF_ULP_EQUAL(hypot(bs::Nan<p_t>(), bs::Inf<p_t>()), bs::Inf<r_t>(), 0);
+  STF_ULP_EQUAL(hypot(bs::Inf<p_t>(), bs::Nan<p_t>()), bs::Inf<r_t>(), 0);
+#endif
+  STF_ULP_EQUAL(hypot(bs::Mone<p_t>(), bs::Mone<p_t>()), bs::Sqrt_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(hypot(bs::One<p_t>(), bs::One<p_t>()), bs::Sqrt_2<r_t>(), 0.5);
+  STF_ULP_EQUAL(hypot(bs::Zero<p_t>(), bs::Zero<p_t>()), bs::Zero<p_t>(), 0);
+  STF_ULP_EQUAL(hypot(bs::Valmax<p_t>(), bs::Zero<p_t>()), bs::Valmax<p_t>(), 0);
+  STF_ULP_EQUAL(hypot(bs::Zero<p_t>(), bs::Valmax<p_t>()), bs::Valmax<p_t>(), 0);
+} // end of test for floating_
+
--- test/function/simd/ldexp.cpp
+++ test/function/simd/ldexp.cpp
@@ -14,6 +14,20 @@
 #include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
 #include <boost/simd/detail/dispatch/meta/as_integer.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/two.hpp>
+#include <boost/simd/constant/four.hpp>
+#include <boost/simd/constant/halfeps.hpp>
+#include <boost/simd/constant/three.hpp>
+#include <boost/simd/detail/constant/maxexponent.hpp>
+#include <boost/simd/detail/constant/limitexponent.hpp>
+#include <boost/simd/detail/constant/minexponent.hpp>
+#include <boost/simd/constant/smallestposval.hpp>
+#include <boost/simd/function/scalar/dec.hpp>
 
 namespace bs = boost::simd;
 namespace bd = boost::dispatch;
@@ -79,3 +93,57 @@ STF_CASE_TPL("Check ldexp on pack/scalar" , STF_NUMERIC_TYPES)
   tests<T, N/2>($);
   tests<T, N*2>($);
 }
+
+
+STF_CASE_TPL("ldexp", STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::ldexp;
+  using p_t = bs::pack<T>;
+
+  using pi_t = bd::as_integer_t<p_t>;
+  using r_t = decltype(ldexp(p_t(), pi_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_EQUAL(ldexp(bs::Inf<p_t>(),  2), bs::Inf<r_t>());
+  STF_EQUAL(ldexp(bs::Minf<p_t>(), 2), bs::Minf<r_t>());
+  STF_IEEE_EQUAL(ldexp(bs::Nan<p_t>(),  2), bs::Nan<r_t>());
+#endif
+
+  STF_EQUAL(ldexp(bs::Mone<p_t>(), 2), -bs::Four<r_t>());
+  STF_EQUAL(ldexp(bs::One<p_t>(),  2), bs::Four<r_t>());
+  STF_EQUAL(ldexp(bs::Zero<p_t>(), 2), bs::Zero<r_t>());
+  STF_EQUAL(ldexp(bs::One <p_t>(), bs::Minexponent<p_t>()), bs::Smallestposval<r_t>());
+  STF_EQUAL(ldexp(bs::One<p_t>()-bs::Halfeps<p_t>(),  bs::Maxexponent<p_t>()), bs::Valmax<p_t>()/2);
+  STF_EQUAL(ldexp(bs::One<p_t>()-bs::Halfeps<p_t>(),  bs::Limitexponent<p_t>()), bs::Valmax<p_t>());
+
+#ifndef BOOST_SIMD_NO_DENORMALS
+  using bs::dec;
+  STF_EQUAL(ldexp(bs::One <p_t>(), dec(bs::Minexponent<p_t>())), bs::Smallestposval<p_t>()/2);
+  STF_EQUAL(ldexp(bs::Two <p_t>(), dec(bs::Minexponent<p_t>())), bs::Smallestposval<p_t>());
+  STF_EQUAL(ldexp(bs::Two <p_t>(), dec(bs::Minexponent<p_t>()-1)), bs::Smallestposval<p_t>()/2);
+  STF_EQUAL(ldexp(bs::One <p_t>(), bs::Minexponent<p_t>()-5), bs::Smallestposval<p_t>()/32);
+#endif
+}
+
+STF_CASE_TPL("ldexp", STF_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::ldexp;
+ using p_t = bs::pack<T>;
+  using pi_t = bd::as_integer_t<p_t>;
+  using r_t = decltype(ldexp(p_t(), pi_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+  STF_EQUAL(ldexp(bs::One <p_t>(), 2), r_t(4));
+  STF_EQUAL(ldexp(bs::Three<p_t>(), 2), r_t(12));
+  STF_EQUAL(ldexp(bs::Mone<p_t>(), 2), r_t(-4));
+}
+
