8997adba8eecbb51e8413232180907d75bed6236
Joel Falcou <joel.falcou@lri.fr>
2016-11-06 16:20:33 +0100

    Cleaning up inc tests

--- test/function/simd/inc.regular.cpp
+++ test/function/simd/inc.regular.cpp
@@ -7,10 +7,8 @@
 **/
 //==================================================================================================
 #include <boost/simd/function/inc.hpp>
-#include <boost/simd/function/saturated.hpp>
 #include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
-
 
 namespace bs = boost::simd;
 
@@ -70,31 +68,4 @@ STF_CASE_TPL("Check post-increment on pack" , STF_NUMERIC_TYPES)
   post_test<T, N>($);
   post_test<T, N/2>($);
   post_test<T, N*2>($);
-}
-
-template <typename T, std::size_t N, typename Env>
-void tests(Env& $)
-{
-  using p_t = bs::pack<T, N>;
-
-  T a1[N], b[N];
-  for(std::size_t i = 0; i < N; ++i)
-  {
-    a1[i] = (i%2) ? T(i) : T(-i);
-    b[i] = bs::inc(a1[i]) ;
-  }
-
-  p_t aa1(&a1[0], &a1[0]+N);
-  p_t bb (&b[0], &b[0]+N);
-
-  STF_EQUAL(bs::saturated_(bs::inc)(aa1), bb);
-}
-
-STF_CASE_TPL("Check inc on pack" , STF_NUMERIC_TYPES)
-{
-  static const std::size_t N = bs::pack<T>::static_size;
-
-  tests<T, N>($);
-  tests<T, N/2>($);
-  tests<T, N*2>($);
 }
