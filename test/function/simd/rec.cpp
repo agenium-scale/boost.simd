ce93046e417985fc1f9cf8dc7efb5bee2f23235e
jtlap <jtlapreste@gmail.com>
2016-11-06 17:22:13 +0100

    adding test values to simd rec.cpp

--- test/function/simd/rec.cpp
+++ test/function/simd/rec.cpp
@@ -11,6 +11,13 @@
 #include <boost/simd/function/raw.hpp>
 #include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/constant/mzero.hpp>
 
 namespace bs = boost::simd;
 
@@ -96,3 +103,24 @@ STF_CASE_TPL("Check raw(rec) on pack", STF_IEEE_TYPES)
   test_raw<T, N/2>($);
   test_raw<T, N*2>($);
 }
+
+STF_CASE_TPL("regular rec",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::rec;
+  using p_t = bs::pack<T>;
+
+  // return type conformity test
+  STF_EXPR_IS( rec(p_t()) , p_t );
+
+  // specific values tests
+  STF_EQUAL(rec(bs::Inf<p_t>()), bs::Zero<p_t>());
+  STF_EQUAL(rec(bs::Minf<p_t>()), bs::Zero<p_t>());
+  STF_EQUAL(rec(bs::Mone<p_t>()), bs::Mone<p_t>());
+  STF_EQUAL(rec(bs::Mzero<p_t>()), bs::Minf<p_t>());
+  STF_IEEE_EQUAL(rec(bs::Nan<p_t>()), bs::Nan<p_t>());
+  STF_EQUAL(rec(bs::One<p_t>()), bs::One<p_t>());
+  STF_EQUAL(rec(bs::Zero<p_t>()), bs::Inf<p_t>());
+} // end of test for floating_
+
