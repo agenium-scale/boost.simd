100b6cec1bef4598edf3491b98e61e3cd58b1594
jtlap <jtlapreste@gmail.com>
2016-11-07 14:07:49 +0100

    adding values test for sign and signnz simd unit

--- test/function/simd/sign.cpp
+++ test/function/simd/sign.cpp
@@ -13,6 +13,12 @@
 #include <boost/simd/function/bits.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
 
 template <typename T, std::size_t N, typename Env>
 void test(Env& $)
@@ -39,4 +45,59 @@ STF_CASE_TPL("Check sign on pack" , STF_NUMERIC_TYPES)
   test<T, N>($);
   test<T, N/2>($);
   test<T, N*2>($);
+}
+
+STF_CASE_TPL (" sign real",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::sign;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(sign(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+  // specific values tests
+#ifndef BOOST_SIMD_NO_INVALIDS
+  STF_EQUAL(sign(bs::Inf<p_t>()), bs::One<r_t>());
+  STF_EQUAL(sign(bs::Minf<p_t>()), bs::Mone<r_t>());
+  STF_IEEE_EQUAL(sign(bs::Nan<p_t>()), bs::Nan<r_t>());
+#endif
+  STF_EQUAL(sign(bs::Mone<p_t>()), bs::Mone<r_t>());
+  STF_EQUAL(sign(bs::One<p_t>()), bs::One<r_t>());
+  STF_EQUAL(sign(bs::Zero<p_t>()), bs::Zero<r_t>());
+}
+
+STF_CASE_TPL (" sign unsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::sign;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(sign(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+  // specific values tests
+  STF_EQUAL(sign(bs::One<p_t>()), bs::One<r_t>());
+  STF_EQUAL(sign(bs::Zero<p_t>()), bs::Zero<r_t>());
+}
+
+STF_CASE_TPL (" signsigned_int",  STF_SIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using p_t = bs::pack<T>;
+  using bs::sign;
+  using r_t = decltype(sign(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, p_t);
+
+  // specific values tests
+  STF_EQUAL(sign(bs::Mone<p_t>()), bs::Mone<r_t>());
+  STF_EQUAL(sign(bs::One<p_t>()), bs::One<r_t>());
+  STF_EQUAL(sign(bs::Zero<p_t>()), bs::Zero<r_t>());
 }
