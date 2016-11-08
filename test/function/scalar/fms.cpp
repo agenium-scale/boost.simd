f76a2dd5d0f7badebbe60a5328e0a036627ab712
jtlap <jtlapreste@gmail.com>
2016-11-08 15:47:09 +0100

    spurious character in fms comment

--- test/function/scalar/fms.cpp
+++ test/function/scalar/fms.cpp
@@ -4,7 +4,7 @@
   Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
-  (See accompanying file LICENSE.md or copy fat http://boost.org/LICENSE_1_0.txt)
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
 #include <boost/simd/function/scalar/fms.hpp>
