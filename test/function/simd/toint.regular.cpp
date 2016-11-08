46e4e8110b09c0b2342dc9a59c954044e82e38d1
jtlap <jtlapreste@gmail.com>
2016-11-08 17:08:27 +0100

    adding values test for toint family

--- test/function/simd/toint.regular.cpp
+++ test/function/simd/toint.regular.cpp
@@ -10,6 +10,13 @@
 #include <boost/simd/function/toint.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+
 
 template <typename T, int N, typename Env>
 void test(Env& $)
@@ -42,3 +49,58 @@ STF_CASE_TPL("Check toint on pack" , STF_IEEE_TYPES)
   test<T, N/2>($);
   test<T, N*2>($);
 }
+
+
+
+STF_CASE_TPL (" toint real",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::toint;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(toint(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, (bd::as_integer_t<p_t, signed>));
+
+  // specific values tests
+  STF_EQUAL(toint(bs::Mone<p_t>()), bs::Mone<r_t>());
+  STF_EQUAL(toint(bs::One<p_t>()),  bs::One<r_t>());
+  STF_EQUAL(toint(bs::Zero<p_t>()), bs::Zero<r_t>());
+
+} // end of test for floating_
+
+STF_CASE_TPL (" toint unsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::toint;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(toint(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, (bd::as_integer_t<p_t, signed>));
+
+  // specific values tests
+  STF_EQUAL(toint(bs::One<p_t>()),  bs::One<r_t>());
+  STF_EQUAL(toint(bs::Zero<p_t>()), bs::Zero<r_t>());
+} // end of test for unsigned_int_
+
+STF_CASE_TPL (" toint signed",  STF_SIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using bs::toint;
+  using p_t = bs::pack<T>;
+  using r_t = decltype(toint(p_t()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, (bd::as_integer_t<p_t, signed>));
+
+  // specific values tests
+  STF_EQUAL(toint(bs::Mone<p_t>()), bs::Mone<r_t>());
+  STF_EQUAL(toint(bs::One<p_t>()),  bs::One<r_t>());
+  STF_EQUAL(toint(bs::Zero<p_t>()), bs::Zero<r_t>());
+} // end of test for signed_int_
+
+
--- test/function/simd/toint.saturated.cpp
+++ test/function/simd/toint.saturated.cpp
@@ -10,6 +10,13 @@
 #include <boost/simd/function/toint.hpp>
 #include <boost/simd/meta/cardinal_of.hpp>
 #include <simd_test.hpp>
+#include <boost/simd/constant/inf.hpp>
+#include <boost/simd/constant/minf.hpp>
+#include <boost/simd/constant/mone.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <boost/simd/constant/one.hpp>
+#include <boost/simd/constant/zero.hpp>
+#include <boost/simd/function/ldexp.hpp>
 
 template <typename T, int N, typename Env>
 void test(Env& $)
@@ -42,3 +49,75 @@ STF_CASE_TPL("Check toint_s on pack" , STF_IEEE_TYPES)
   test<T, N/2>($);
   test<T, N*2>($);
 }
+
+
+
+STF_CASE_TPL (" bs::saturated_(bs::toint) real",  STF_IEEE_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using r_t = decltype(bs::saturated_(bs::toint)(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, (bd::as_integer_t<T, signed>));
+
+  // specific values tests
+  STF_EQUAL(bs::saturated_(bs::toint)(T(2)*bs::Valmax<r_t>()),  bs::Valmax<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(T(2)*bs::Valmin<r_t>()),  bs::Valmin<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(T(1.5)*bs::Valmax<r_t>()),  bs::Valmax<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(T(1.5)*bs::Valmin<r_t>()),  bs::Valmin<r_t>());
+
+
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Inf<T>()),  bs::Inf<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Minf<T>()), bs::Minf<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Mone<T>()), bs::Mone<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Nan<T>()),  bs::Zero<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::One<T>()),  bs::One<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Zero<T>()), bs::Zero<r_t>());
+
+  T v = T(1);
+  r_t iv = 1;
+  int N = sizeof(T)*8-1;
+  for(int i=0; i < N ; i++, v*= 2, iv <<= 1)
+  {
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+    STF_EQUAL(bs::saturated_(bs::toint)(v), iv);
+    STF_EQUAL(bs::saturated_(bs::toint)(-v), -iv);
+  }
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::ldexp(bs::One<T>(), N)), bs::Valmax<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::ldexp(bs::One<T>(), N+1)), bs::Valmax<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(-bs::ldexp(bs::One<T>(), N+1)), bs::Valmin<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(-bs::ldexp(bs::One<T>(), N+1)), bs::Valmin<r_t>());
+
+} // end of test for floating_
+
+STF_CASE_TPL (" bs::saturated_(bs::toint) unsigned_int",  STF_UNSIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+  using r_t = decltype(bs::saturated_(bs::toint)(T()));
+
+  // return type conformity test
+  STF_TYPE_IS(r_t, (bd::as_integer_t<T, signed>));
+
+  // specific values tests
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::One<T>()),  bs::One<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Zero<T>()), bs::Zero<r_t>());
+} // end of test for unsigned_int_
+
+STF_CASE_TPL (" bs::saturated_(bs::toint) signed",  STF_SIGNED_INTEGRAL_TYPES)
+{
+  namespace bs = boost::simd;
+  namespace bd = boost::dispatch;
+
+  using r_t = decltype(bs::saturated_(bs::toint)(T()));
+  // return type conformity test
+  STF_TYPE_IS(r_t, (bd::as_integer_t<T, signed>));
+
+
+  // specific values tests
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Mone<T>()), bs::Mone<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::One<T>()),  bs::One<r_t>());
+  STF_EQUAL(bs::saturated_(bs::toint)(bs::Zero<T>()), bs::Zero<r_t>());
+} // end of test for signed_int_
