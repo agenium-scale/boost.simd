eea2a3fcec3d81bed5f283290452f00009f9dce2
jtlap <jtlapreste@gmail.com>
2016-11-10 11:04:32 +0100

    scalar float inearbyint uses _mm_cvtss_si32 (2 versions one fast_ decorated just call the intrinsic, the other take care of inf -> valmax and nan -> 0

--- include/boost/simd/function/scalar/inearbyint.hpp
+++ include/boost/simd/function/scalar/inearbyint.hpp
@@ -12,5 +12,10 @@
 
 #include <boost/simd/function/definition/inearbyint.hpp>
 #include <boost/simd/arch/common/scalar/function/inearbyint.hpp>
+#if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
+#  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE_VERSION
+#    include <boost/simd/arch/x86/sse1/scalar/function/inearbyint.hpp>
+#  endif
+#endif
 
 #endif
