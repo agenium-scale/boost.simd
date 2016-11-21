6ed087e62037b3f5fbedc64923ce07398cd8325e
Joel Falcou <joel.falcou@lri.fr>
2016-11-21 20:55:21 +0100

    Fix #327 - Remnant of log1p optimization was laying around

--- include/boost/simd/function/simd/log1p.hpp
+++ include/boost/simd/function/simd/log1p.hpp
@@ -15,16 +15,9 @@
 #include <boost/simd/arch/common/simd/function/log1p.hpp>
 
 #if defined(BOOST_HW_SIMD_X86_OR_AMD_AVAILABLE)
-
-#  if BOOST_HW_SIMD_X86_OR_AMD < BOOST_HW_SIMD_X86_SSE4_1_VERSION
-#    if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_SSE2_VERSION
-#      include <boost/simd/arch/x86/sse2/simd/function/log1p.hpp>
-#    endif
-#  endif
 #  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_AVX_VERSION
 #    include <boost/simd/arch/x86/avx/simd/function/log1p.hpp>
 #  endif
-
 #endif
 
 #endif
