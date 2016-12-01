add7673369f64420cd97680f51ea456ad6411a50
Joel Falcou <joel.falcou@lri.fr>
2016-12-01 18:09:26 +0100

    Improve if_dec/if_inc codegen quality

--- test/function/simd/if_dec.cpp
+++ test/function/simd/if_dec.cpp
@@ -1,46 +1,73 @@
 //==================================================================================================
-/*!
-  @file
-
+/**
   Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-*/
+**/
 //==================================================================================================
-#include <boost/simd/pack.hpp>
 #include <boost/simd/function/if_dec.hpp>
-#include <boost/simd/meta/cardinal_of.hpp>
+#include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
 
-template <typename T, int N, typename Env>
-void test(Env& $)
+namespace bs = boost::simd;
+
+template <typename T, int N, typename Env> void test(Env& $)
 {
-  namespace bs = boost::simd;
   using p_t = bs::pack<T, N>;
+  using pl_t = bs::pack<bs::logical<T>, N>;
 
-  namespace bs = boost::simd;
-  namespace bd = boost::dispatch;
+  bs::logical<T> a1[N];
+  T a2[N], b[N];
 
-  T a1[N], a2[N], b[N];
   for(int i = 0; i < N; ++i)
   {
-     a1[i] = (i%2) ? T(1) : T(0);
-     a2[i] = (i%2) ? T(i+N) : T(-(i+N));
-     b[i] = bs::if_dec(a1[i], a2[i]);
-   }
-  p_t aa1(&a1[0], &a1[0]+N);
+    a1[i] = (i%2 != 1);
+    a2[i] = T(1+i/2);
+    b[i] = bs::if_dec(a1[i], a2[i]);
+  }
+
+  pl_t aa1(&a1[0], &a1[0]+N);
   p_t aa2(&a2[0], &a2[0]+N);
   p_t bb(&b[0], &b[0]+N);
-  STF_IEEE_EQUAL(bs::if_dec(aa1, aa2), bb);
+
+  STF_EQUAL(bs::if_dec(aa1, aa2), bb);
 }
 
-STF_CASE_TPL("Check if_dec on pack" , STF_NUMERIC_TYPES)
+STF_CASE_TPL("Check if_dec on pack with logical condition" , STF_NUMERIC_TYPES)
 {
-  namespace bs = boost::simd;
-  using p_t = bs::pack<T>;
-  static const std::size_t N = bs::cardinal_of<p_t>::value;
+  static const std::size_t N = bs::pack<T>::static_size;
+
   test<T, N>($);
   test<T, N/2>($);
   test<T, N*2>($);
+}
+
+template <typename T, int N, typename Env> void testa(Env& $)
+{
+  using p_t = bs::pack<T, N>;
+
+  T a1[N],a2[N], b[N];
+
+  for(int i = 0; i < N; ++i)
+  {
+    a1[i] = (i%2 != 1) ? T(1) : T(0);
+    a2[i] = T(1+i/2);
+    b[i] = bs::if_dec(a1[i], a2[i]);
+  }
+
+  p_t aa1(&a1[0], &a1[0]+N);
+  p_t aa2(&a2[0], &a2[0]+N);
+  p_t bb(&b[0], &b[0]+N);
+
+  STF_EQUAL(bs::if_dec(aa1, aa2), bb);
+}
+
+STF_CASE_TPL("Check if_dec on pack with arithmetic condition" , STF_NUMERIC_TYPES)
+{
+  static const std::size_t N = bs::pack<T>::static_size;
+
+  testa<T, N>($);
+  testa<T, N/2>($);
+  testa<T, N*2>($);
 }
