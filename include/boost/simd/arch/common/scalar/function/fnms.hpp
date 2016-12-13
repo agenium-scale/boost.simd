60fabab3eb1884698db5869c451effabce5a4601
jtlap <jtlapreste@gmail.com>
2016-12-13 14:39:48 +0100

    fms,fnma,fnms pedantic_ have an erroneous dispatch

--- include/boost/simd/arch/common/scalar/function/fnms.hpp
+++ include/boost/simd/arch/common/scalar/function/fnms.hpp
@@ -47,7 +47,7 @@ namespace boost { namespace simd { namespace ext
                            )
   {
     BOOST_FORCEINLINE
-    A0 operator() ( A0 a0, A0 a1, A0 a2) const BOOST_NOEXCEPT
+    A0 operator() (pedantic_tag const &, A0 a0, A0 a1, A0 a2) const BOOST_NOEXCEPT
     {
       return -pedantic_(fms)(a0, a1, a2);
     }
