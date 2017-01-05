13c5c2f3e468534d190209395868f9ad2a193776
Joel Falcou <joel.falcou@lri.fr>
2017-01-05 16:40:12 +0100

    Make static indexing more flexible (#351)
    Some functions requires parameters that may be a std::integral_constant. To simplify this API,
    we now support user-defined litteral like 1_c to represent  std::integral_constant<int,1>.
    
    Affected functions include:
    
    - insert           :  insert(p, 1_c, 42);
    - extract          :  auto x = extract(p, 1_c);
    - pack::operator[] :  auto x = p[3_c];
    
      Note that in this case, the access to the pack's element is  retuning a value instead of a reference.

--- test/function/simd/extract/static.arithmetic.cpp
+++ test/function/simd/extract/static.arithmetic.cpp
@@ -1,21 +1,22 @@
 //==================================================================================================
-/*!
-  @file
-
+/**
   Copyright 2016 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-*/
+**/
 //==================================================================================================
 #include <boost/simd/function/extract.hpp>
 #include <boost/simd/detail/unroll.hpp>
+#include <boost/simd/litteral.hpp>
 #include <boost/simd/pack.hpp>
 #include <simd_test.hpp>
 #include <array>
 
 namespace bs = boost::simd;
 namespace bd = boost::dispatch;
+
+using namespace bs::literal;
 
 template <typename T, std::size_t N, typename Env, typename Idx>
 void unroll_step ( bs::pack<T,N> const& p, std::array<T,N> const& ref, Idx const&, Env& $)
@@ -51,4 +52,26 @@ STF_CASE_TPL("Check static extract on pack" , STF_NUMERIC_TYPES)
   test_st<T, N/2>($);
   test_st<T, N>($);
   test_st<T, N*2>($);
+}
+
+template <typename T, std::size_t N, typename Env>
+void test_lt(Env& $)
+{
+  std::array<T,N> ref;
+  for(std::size_t i = 0; i < N; ++i) ref[i] = T(i*2);
+  bs::pack<T,N> p(&ref[0], &ref[0]+N);
+
+          STF_EQUAL(bs::extract(p, 0_c), ref[0]);
+  if(N>1) STF_EQUAL(bs::extract(p, 1_c), ref[1]);
+  if(N>3) STF_EQUAL(bs::extract(p, 3_c), ref[3]);
+  if(N>7) STF_EQUAL(bs::extract(p, 7_c), ref[7]);
+}
+
+STF_CASE_TPL("Check extract on pack using literals" , STF_NUMERIC_TYPES)
+{
+  static const std::size_t N = boost::simd::pack<T>::static_size;
+
+  test_lt<T, N/2>($);
+  test_lt<T, N>($);
+  test_lt<T, N*2>($);
 }
