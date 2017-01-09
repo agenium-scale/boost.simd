835eea66a19825531882097afff3110241c99bb0
Alain Miniussi <alain.miniussi@oca.eu>
2017-01-09 18:19:56 +0100

    iterator test issue with centos65/icpc15 (#373)
    At least with intel15/gcc4.4.7, the iterator type traits seemss to be instantiated too soon, so the compilation fails before the code can be dismisssed by SFINAE (which is sad).

--- include/boost/simd/meta/is_iterator.hpp
+++ include/boost/simd/meta/is_iterator.hpp
@@ -12,8 +12,8 @@
 #ifndef BOOST_SIMD_META_IS_ITERATOR_HPP_INCLUDED
 #define BOOST_SIMD_META_IS_ITERATOR_HPP_INCLUDED
 
-#include <boost/simd/detail/dispatch/detail/declval.hpp>
 #include <boost/simd/config.hpp>
+#include <boost/simd/detail/dispatch/detail/declval.hpp>
 #include <type_traits>
 
 namespace boost { namespace simd
@@ -21,6 +21,21 @@ namespace boost { namespace simd
   namespace bd = boost::dispatch;
   namespace detail
   {
+#if defined(BOOST_DISPATCH_USE_INCOMPLETE_STD)
+    // This is far from perfect, but we are in a far from perfect situation.
+    template <typename T> struct is_iterator_impl
+    {
+      struct big{ void *p[2];};
+      static_assert(sizeof(big) != sizeof(void*), 
+                    "This machine is too weird for us.");
+      
+      static big test(...); 
+      template<class It> static typename It::iterator_category* test(It); // Iterator
+      template<class It> static void * test(It *); // Pointer
+
+      static const bool value = sizeof(test(bd::detail::declval<T>())) != sizeof(big);
+    };
+#else
     template <typename T> struct is_iterator_impl
     {
       static std::false_type test(...);
@@ -37,6 +52,7 @@ namespace boost { namespace simd
                                             , std::true_type
                                             >::value;
     };
+#endif
   }
 
   /*!
