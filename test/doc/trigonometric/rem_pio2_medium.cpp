3a758660edcbf149c440367d2ca89803d17e8461
jtlap <jtlapreste@gmail.com>
2017-01-12 18:58:39 +0100

    trigo doc examples

---
+++ test/doc/trigonometric/rem_pio2_medium.cpp
@@ -0,0 +1,41 @@
+//==================================================================================================
+/*
+  Copyright 2017 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+//! [rem_pio2_medium]
+#include <boost/simd/trigonometric.hpp>
+#include <boost/simd/pack.hpp>
+#include <iostream>
+
+
+namespace bs =  boost::simd;
+using pack_ft = bs::pack <float, 4>;
+
+int main()
+{
+  pack_ft pf = {1.0f, 2.0f, -1.0f, 0.5f};
+  pack_ft pe, pm;
+  std::tie(pe, pm) = bs::rem_pio2_medium(pf);
+  std::cout
+    << "---- simd" << '\n'
+    << "<- pf =           " << pf << '\n'
+    << "-> std::tie(pe, pm) = bs::rem_pio2_medium(pf) = " << '\n'
+    << "-> pe = " << pe << '\n'
+    << "-> pm = " << pm << '\n';
+
+  float xf = 2.0f;
+  float e, m;
+  std::tie(e, m) = bs::rem_pio2_medium(xf);
+  std::cout
+    << "---- scalar"  << '\n'
+    << "<- xf =           " << xf << '\n'
+    << "-> std::tie(pe, pm) =  bs::rem_pio2_medium(xf) = " << '\n'
+    << "-> e =  " << e <<  '\n'
+    << "-> m =  " << m <<  '\n';
+  return 0;
+}
+//! [rem_pio2_medium]
