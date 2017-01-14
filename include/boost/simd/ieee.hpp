6499596132c9a1c12e88858f472bb59452087457
jtlap <jtlapreste@gmail.com>
2017-01-14 11:20:44 +0100

    examples for ieee

--- include/boost/simd/ieee.hpp
+++ include/boost/simd/ieee.hpp
@@ -19,10 +19,24 @@ namespace boost { namespace simd
     @defgroup group-ieee Ieee functions
 
       These functions provide scalar and SIMD algorithms for inspecting, generating or
-      decomposing IEEE 754 floating point numbers. Operations such as exponent and mantissa
-      extraction, floating point modulo, IEEE bit patterns manipulation and magnitude comparison
-      are provided.
+      decomposing IEEE 754 floating point numbers.
 
+      Operations such as exponent and mantissa
+      extraction, floating point modulo, IEEE bit patterns manipulation and
+      magnitude comparison are provided.
+
+<center>
+ |                      |                  |                  |                 |                 |
+ |:--------------------:|:----------------:|:----------------:|:---------------:|:---------------:|
+ | @ref bitfloating     | @ref bitinteger  | @ref bitofsign   | @ref copysign   | @ref eps        |
+ | @ref exponentbits    | @ref exponent    | @ref fpclassify  | @ref frac       | @ref frexp      |
+ | @ref ilogb           | @ref ldexp       | @ref mantissa    | @ref maxmag     | @ref maxnum     |
+ | @ref maxnummag       | @ref minmag      | @ref minnum      | @ref minnummag  | @ref modf       |
+ | @ref negate          | @ref negatenz    | @ref nextafter   | @ref next       | @ref nextpow2   |
+ | @ref predecessor     | @ref prev        | @ref safe_max    | @ref safe_min   | @ref saturate   |
+ | @ref sbits           | @ref sign        | @ref signnz      | @ref successor  | @ref ulpdist    |
+ | @ref ulp             |                  |                  |                 |                 |
+</center>
   **/
 
 } }
