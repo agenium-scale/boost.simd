e89debaa1ef2a109d777408ddcc899e4a1c211bc
jtlap <jtlapreste@gmail.com>
2017-01-17 12:13:36 +0100

    examples for bitwise

---
+++ test/doc/bitwise/bitwise_and.cpp
@@ -0,0 +1,37 @@
+//==================================================================================================
+/*
+  Copyright 2017 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+//! [bitwise_and]
+#include <boost/simd/bitwise.hpp>
+#include <boost/simd/pack.hpp>
+#include <boost/simd/constant/nan.hpp>
+#include <iostream>
+
+namespace bs =  boost::simd;
+using pack_it =  bs::pack <std::uint32_t, 4>;
+
+int main()
+{
+  pack_it pf = { 3, -2, -0, 0  };
+  pack_it qf = { 4, -1,  0, ~0 };
+  std::cout
+    <<  "---- simd" << '\n'
+    << " <- pf =                      " << pf << '\n'
+    << " <- qf =                      " << qf << '\n'
+    << " -> bs::bitwise_and(pf, qf) = " << bs::bitwise_and(pf, qf) << '\n';
+
+  std::uint32_t xf = 3, yf = 4;
+
+  std::cout
+    << "---- scalar"  << '\n'
+    << " xf =                         " << xf << '\n'
+    << " yf =                         " << yf << '\n'
+    << " -> bs::bitwise_and(xf, yf) = " << bs::bitwise_and(xf, yf) << '\n';
+  return 0;
+}
+//! [bitwise_and]
---
+++ test/doc/bitwise/hi.cpp
@@ -0,0 +1,35 @@
+//==================================================================================================
+/*
+  Copyright 2017 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+//! [hi]
+#include <boost/simd/bitwise.hpp>
+#include <boost/simd/pack.hpp>
+#include <iostream>
+
+
+namespace bs =  boost::simd;
+using pack_ft = bs::pack <float, 4>;
+
+int main()
+{
+  pack_ft pf = {1.0f, 2.0f, -1.0f, 0.5f};
+
+  std::cout
+    << "---- simd" << '\n'
+    << "<- pf =           " << pf << '\n'
+    << "-> bs::hi(pf) =   " << bs::hi(pf) << '\n';
+
+  float xf = 2.0f;
+
+  std::cout
+    << "---- scalar"  << '\n'
+    << "<- xf =           " << xf << '\n'
+    << "-> bs::hi(xf) =   " << bs::hi(xf) << '\n';
+  return 0;
+}
+//! [hi]
