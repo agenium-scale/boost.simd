565b0f7f2e60ffd9ef44aa5ec6c723f51adb8975
Alain Miniussi <alain.miniussi@oca.eu>
2017-01-24 07:25:11 +0100

    Make ICPC15 behaves correctly with make implementation  (#398)
    With Intel15 (yes, I know...) boost::simd::ext::impl_make_ get easily instantiated with empty values. Somehow, it's like the variadic part of the templates gets bound to '<>'.
    Helping the compiler by spikking the intsantiation step between make<Target>(ts...) -> often used at call site and make(as_<Target>{}, ts...)

--- include/boost/simd/pack.hpp
+++ include/boost/simd/pack.hpp
@@ -185,7 +185,7 @@ namespace boost { namespace simd
                    , "pack<T,N>(T v...) must take exactly N arguments"
                    );
 
-      data_ = boost::simd::make<pack>(v0,v1,vn...).storage();
+      data_ = boost::simd::detail::make(as_<pack>{},v0,v1,vn...).storage();
     }
 
     /*!
