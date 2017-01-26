310f9593ce553aeaaddc1cfca65f3e3537482a64
Joel Falcou <joel.falcou@lri.fr>
2017-01-26 16:26:03 +0100

    Remove spurrious include

--- include/boost/simd/function/simd/pack_cast.hpp
+++ include/boost/simd/function/simd/pack_cast.hpp
@@ -24,9 +24,6 @@
 #  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_AVX2_VERSION
 #    include <boost/simd/arch/x86/avx2/simd/function/pack_cast.hpp>
 #  endif
-#  if BOOST_HW_SIMD_X86_OR_AMD >= BOOST_HW_SIMD_X86_AVX512_VERSION
-#    include <boost/simd/arch/x86/avx512/simd/function/pack_cast.hpp>
-#  endif
 #endif
 
 #endif
