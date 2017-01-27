fa68f3ea324724a3f9a17f8b8795731d33367944
Joel Falcou <joel.falcou@lri.fr>
2017-01-27 11:17:08 +0100

    Don't test undefined behavior in pack_cast

--- test/function/simd/cast/cast_test_base.hpp
+++ test/function/simd/cast/cast_test_base.hpp
@@ -10,6 +10,7 @@
 #define CAST_TEST_BASE_HPP_INCLUDED
 
 #include <boost/simd/function/enumerate.hpp>
+#include <boost/simd/function/abs.hpp>
 
 namespace bs = boost::simd;
 
@@ -33,20 +34,35 @@ template <typename T, typename Env> void test_val(Env& $)
   sT base = (sT(-1) < 0) ? sT(-1) : sT(0);
   T x =  bs::enumerate<T>(base, sT(4)/3);
 
-  std::cout << "Source type: pack<"   << stf::type_id<sT>()
-                              << ","  << T::static_size
-                              << ">"  << std::endl;
-
+  STF_EQUAL(bs::pack_cast<float        >(x), (res<T,float        >::comp(x)));
+  STF_EQUAL(bs::pack_cast<double       >(x), (res<T,double       >::comp(x)));
   STF_EQUAL(bs::pack_cast<std::int8_t  >(x), (res<T,std::int8_t  >::comp(x)));
   STF_EQUAL(bs::pack_cast<std::int16_t >(x), (res<T,std::int16_t >::comp(x)));
   STF_EQUAL(bs::pack_cast<std::int32_t >(x), (res<T,std::int32_t>::comp(x)));
   STF_EQUAL(bs::pack_cast<std::int64_t >(x), (res<T,std::int64_t >::comp(x)));
-  STF_EQUAL(bs::pack_cast<std::uint8_t >(x), (res<T,std::uint8_t >::comp(x)));
-  STF_EQUAL(bs::pack_cast<std::uint16_t>(x), (res<T,std::uint16_t>::comp(x)));
-  STF_EQUAL(bs::pack_cast<std::uint32_t>(x), (res<T, std::uint32_t>::comp(x)));
-  STF_EQUAL(bs::pack_cast<std::uint64_t>(x), (res<T,std::uint64_t>::comp(x)));
-  STF_EQUAL(bs::pack_cast<float        >(x), (res<T,float        >::comp(x)));
-  STF_EQUAL(bs::pack_cast<double       >(x), (res<T,double       >::comp(x)));
+
+  /*
+    As per the standard static_cast<std::uint*_t>(-1.) is undefined so we don't test
+    for undefined cases.
+
+    See: http://www.embeddeduse.com/2013/08/25/casting-a-negative-float-to-an-unsigned-int/
+    See: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3337.pdf
+
+    Section 4.9:
+    " A prvalue of a floating point type can be converted to a prvalue of an
+      integer type. The conversion truncates; that is, the fractional part
+      is discarded. The behavior is undefined if the truncated value cannot
+      be represented in the destination type."
+  */
+  if(std::is_floating<T>::value)
+  {
+    x = bs::abs(x);
+    STF_EQUAL(bs::pack_cast<std::uint8_t >(x), (res<T,std::uint8_t >::comp(x)));
+    STF_EQUAL(bs::pack_cast<std::uint16_t>(x), (res<T,std::uint16_t>::comp(x)));
+    STF_EQUAL(bs::pack_cast<std::uint32_t>(x), (res<T,std::uint32_t>::comp(x)));
+    STF_EQUAL(bs::pack_cast<std::uint64_t>(x), (res<T,std::uint64_t>::comp(x)));
+  }
+
   std::cout << "=====================================================================\n" << std::endl;
 }
 
