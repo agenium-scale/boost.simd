a74a070ffaf8a7d28412490631bb9d98d1f5a67f
gquintin <guillaume.quintin@numscale.com>
2017-02-02 02:36:00 +0100

    Fix doc for compare_greater

--- include/boost/simd/function/compare_greater.hpp
+++ include/boost/simd/function/compare_greater.hpp
@@ -12,31 +12,29 @@
 #define BOOST_SIMD_FUNCTION_COMPARE_GREATER_HPP_INCLUDED
 
 #if defined(DOXYGEN_ONLY)
-namespace boost {
-namespace simd {
-/*!
-  @ingroup group-reduction
-  This function object returns the return a bool that is the result of the
-lexicographic
-  test for > on all elements of the entries
+namespace boost { namespace simd
+{
+  /*!
+    @ingroup group-reduction
+    This function object returns the return a bool that is the result of the
+    lexicographic test for > on all elements of the entries
 
-  It is probably not what you wish. Have a look to @ref is_greater
+    It is probably not what you wish. Have a look to @ref is_greater
 
 
     @par Header <boost/simd/function/compare_greater.hpp>
 
-  @par Example:
+    @par Example:
 
-   @snippet compare_greater.cpp compare_greater
+      @snippet compare_greater.cpp compare_greater
 
- @par Possible output:
+    @par Possible output:
 
-   @snippet compare_greater.txt compare_greater
+      @snippet compare_greater.txt compare_greater
 
-**/
-bool compare_greater(Value const &x, Value const &y);
-}
-}
+  **/
+  bool compare_greater(Value const& x, Value const& y);
+} }
 #endif
 
 #include <boost/simd/function/scalar/compare_greater.hpp>
