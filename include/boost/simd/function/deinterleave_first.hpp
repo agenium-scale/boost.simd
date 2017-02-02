e0470aa48a56dc03d40e3690bdd3346dfb0d0389
gquintin <guillaume.quintin@numscale.com>
2017-02-02 02:57:11 +0100

    Fix doc for deinterleave_first

--- include/boost/simd/function/deinterleave_first.hpp
+++ include/boost/simd/function/deinterleave_first.hpp
@@ -36,7 +36,7 @@ namespace boost { namespace simd
     @param  v1  Second part of the data to deinterleave
     @return A boost::simd::pack containing the deinterleaved value of the first data channel.
   **/
-  Value deinterleave_first(Value const& v0, Value const& v1);
+  Value deinterleave_first(Value const& x, Value const& y);
 } }
 #endif
 
