dd0ed6e49b12f09127bd28080641ca35097d9b0a
gquintin <guillaume.quintin@numscale.com>
2017-02-02 03:12:34 +0100

    Fix doc for dot

--- include/boost/simd/function/dot.hpp
+++ include/boost/simd/function/dot.hpp
@@ -12,28 +12,27 @@
 #define BOOST_SIMD_FUNCTION_DOT_HPP_INCLUDED
 
 #if defined(DOXYGEN_ONLY)
-namespace boost {
-namespace simd {
+namespace boost { namespace simd
+{
 
-/*!
-   @ingroup group-reduction
-   This function object returns the returns the dot product of the two arguments
+  /*!
+    @ingroup group-reduction
+    This function object returns the returns the dot product of the two arguments
 
 
     @par Header <boost/simd/function/dot.hpp>
 
-  @par Example:
+    @par Example:
 
-    @snippet edot.cpp dot
+      @snippet edot.cpp dot
 
-  @par Possible output:
+    @par Possible output:
 
-    @snippet edot.txt dot
+      @snippet edot.txt dot
 
- **/
-scalar_of_t<Value> dot(Value const &x, Value const &y);
-}
-}
+  **/
+  scalar_of_t<Value> dot(Value const& x, Value const& y);
+} }
 #endif
 
 #include <boost/simd/function/scalar/dot.hpp>
