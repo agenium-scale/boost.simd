fe979214b7649e230fd10aa3fa6589d0316106a7
Alain Miniussi <alain.miniussi@oca.eu>
2017-02-03 11:50:36 +0100

    Nested varaidic templates are an issue for intel15

--- include/boost/simd/arch/common/simd/function/split_high.hpp
+++ include/boost/simd/arch/common/simd/function/split_high.hpp
@@ -33,7 +33,8 @@ namespace boost { namespace simd { namespace ext
     static BOOST_FORCEINLINE result_t do_(A0 const& a, nsm::list<N...> const&) BOOST_NOEXCEPT
     {
       using type = typename result_t::value_type;
-      return result_t( static_cast<type>(bs::extract<sizeof...(N)+N::value>(a))... );
+      static int const list_size = sizeof...(N);
+      return result_t( static_cast<type>(bs::extract<list_size+N::value>(a))... );
     }
 
     BOOST_FORCEINLINE result_t operator()(A0 const& a) const BOOST_NOEXCEPT
