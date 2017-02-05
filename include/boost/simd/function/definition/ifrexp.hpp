daac84f4fd43b47200fc95a8cc573db78073a155
Fytch <josua.rieder1996@gmail.com>
2017-02-05 11:59:22 +0100

    removed superfluous semicolons, would not compile with -pedantic-errors

--- include/boost/simd/function/definition/ifrexp.hpp
+++ include/boost/simd/function/definition/ifrexp.hpp
@@ -25,7 +25,7 @@ namespace boost { namespace simd
 
   namespace ext
   {
-    BOOST_DISPATCH_FUNCTION_DECLARATION(tag, ifrexp_);
+    BOOST_DISPATCH_FUNCTION_DECLARATION(tag, ifrexp_)
   }
 
   BOOST_DISPATCH_CALLABLE_DEFINITION(tag::ifrexp_,ifrexp);
