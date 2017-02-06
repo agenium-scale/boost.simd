c85542af5477908fb48750621befcd7614bd2450
gquintin <guillaume.quintin@numscale.com>
2017-02-06 02:50:48 +0100

    Fix doc for interleave_second

--- include/boost/simd/function/interleave_second.hpp
+++ include/boost/simd/function/interleave_second.hpp
@@ -41,7 +41,7 @@ namespace boost { namespace simd
     with <tt> n = cardinal_of<T>::value </tt>
 
   **/
-  Value interleave_second(Value const& v0, Value const& v1);
+  Value interleave_second(Value const& x, Value const& y);
 #endif
 } }
 
