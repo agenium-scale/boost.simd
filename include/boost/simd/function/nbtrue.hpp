7b9a7c0666851061f126d43b20576bbc7a21ec1c
gquintin <guillaume.quintin@numscale.com>
2017-02-06 15:48:06 +0100

    Fix doc for nbtrue

--- include/boost/simd/function/nbtrue.hpp
+++ include/boost/simd/function/nbtrue.hpp
@@ -12,33 +12,32 @@
 #define BOOST_SIMD_FUNCTION_NBTRUE_HPP_INCLUDED
 
 #if defined(DOXYGEN_ONLY)
-namespace boost {
-namespace simd {
+namespace boost { namespace simd
+{
 
-/*!
-   @ingroup group-reduction
-   This function object returns the number of non @ref Zero elements of the
- argument.
+  /*!
+     @ingroup group-reduction
+     This function object returns the number of non @ref Zero elements of the
+   argument.
 
 
-    @par Header <boost/simd/function/nbtrue.hpp>
+      @par Header <boost/simd/function/nbtrue.hpp>
 
-   @par Note
+     @par Note
 
-   This function is peculiarly useful in horizontal branching idioms.
+     This function is peculiarly useful in horizontal branching idioms.
 
-  @par Example:
+    @par Example:
 
-    @snippet nbtrue.cpp nbtrue
+      @snippet nbtrue.cpp nbtrue
 
-  @par Possible output:
+    @par Possible output:
 
-    @snippet nbtrue.txt nbtrue
+      @snippet nbtrue.txt nbtrue
 
- **/
-std::size_t nbtrue(Value const &x);
-}
-}
+  **/
+  std::size_t nbtrue(Value const& x);
+} }
 #endif
 
 #include <boost/simd/function/scalar/nbtrue.hpp>
