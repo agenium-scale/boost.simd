33e1b413f4c1df1aa6e1af30342d0afc067a8a62
Charly Chevalier <charly.chevalier@numscale.com>
2017-02-06 15:07:22 +0100

    Re-generate all aggregates

--- include/boost/simd/function/simd/quadrant.hpp
+++ include/boost/simd/function/simd/quadrant.hpp
@@ -1,6 +1,6 @@
 //==================================================================================================
 /**
-  Copyright 2016 NumScale SAS
+  Copyright 2017 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
@@ -13,6 +13,5 @@
 #include <boost/simd/function/scalar/quadrant.hpp>
 #include <boost/simd/arch/common/generic/function/autodispatcher.hpp>
 #include <boost/simd/arch/common/simd/function/quadrant.hpp>
-
 
 #endif
