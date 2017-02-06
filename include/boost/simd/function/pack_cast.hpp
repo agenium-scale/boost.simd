097d98652556d1d329eab36fc870da4caa8e30de
gquintin <guillaume.quintin@numscale.com>
2017-02-06 16:00:55 +0100

    Fix doc for pack_cast

--- include/boost/simd/function/pack_cast.hpp
+++ include/boost/simd/function/pack_cast.hpp
@@ -14,7 +14,7 @@
 #if defined(DOXYGEN_ONLY)
 namespace boost { namespace simd
 {
-/*!
+  /*!
       @ingroup group-bitwise
       This function objects converts the elements of its argument an arbitrary type
       @c Target while preserving its argument's cardinal.
@@ -26,9 +26,8 @@ namespace boost { namespace simd
       @param  @c x Value to cast
 
       @return A value of type @c Target which is cast to @c x.
-    **/
-    Target pack_cast(Value const& x);
-  }
+  **/
+  Target pack_cast(Value const& x);
 } }
 #endif
 
