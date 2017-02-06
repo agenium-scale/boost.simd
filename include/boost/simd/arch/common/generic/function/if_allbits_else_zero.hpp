a7eb1dc6991f6191bec0373e18c601b25c0e646b
Charly Chevalier <charlyy.chevalier@gmail.com>
2017-02-06 16:54:25 +0100

    Aggregated fixes (#437)
    * Clean up (remove if_neg.cpp)
    
    * Clean up (sse2/scalar/stream.hpp -> sse2/scalar/function/stream.hpp)
    
    * Add simple .hpp wrapper for aliases
    
    * Re-generate all aggregates

--- include/boost/simd/arch/common/generic/function/if_allbits_else_zero.hpp
+++ include/boost/simd/arch/common/generic/function/if_allbits_else_zero.hpp
@@ -2,18 +2,16 @@
 /*!
   @file
 
-  Copyright 2016 NumScale SAS
+  @copyright 2017 NumScale SAS
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 */
 //==================================================================================================
-#ifndef BOOST_SIMD_FUNCTION_SIMD_IF_NEG_CPP_INCLUDED
-#define BOOST_SIMD_FUNCTION_SIMD_IF_NEG_CPP_INCLUDED
+#ifndef BOOST_SIMD_ARCH_COMMON_GENERIC_FUNCTION_IF_ALLBITS_ELSE_ZERO_HPP_INCLUDED
+#define BOOST_SIMD_ARCH_COMMON_GENERIC_FUNCTION_IF_ALLBITS_ELSE_ZERO_HPP_INCLUDED
 
-#include <boost/simd/function/scalar/if_neg.hpp>
-#include <boost/simd/arch/common/generic/function/autodispatcher.hpp>
-#include <boost/simd/arch/common/simd/function/if_neg.hpp>
+// Alias
+#include <boost/simd/function/genmask.hpp>
 
 #endif
-
