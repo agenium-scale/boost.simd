522bac8279ce3f820e1da60357aaeb3fcec71b6c
gquintin <guillaume.quintin@numscale.com>
2017-02-06 03:11:44 +0100

    Fix doc for is_not_greater_equal

--- include/boost/simd/function/is_not_greater_equal.hpp
+++ include/boost/simd/function/is_not_greater_equal.hpp
@@ -15,7 +15,7 @@
 namespace boost { namespace simd
 {
 
- /*!
+  /*!
     @ingroup group-predicates
 
     This function object returns @ref True if x is not greater or equal to y else returns @ref False.
