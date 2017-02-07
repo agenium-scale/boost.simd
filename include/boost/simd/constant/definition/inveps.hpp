e32d7148d76c9de4c13e026c72a2afc58cf6b2f0
Fytch <josua.rieder1996@gmail.com>
2017-02-07 18:09:14 +0100

    removed superfluous semicolons; failed with -pedantic-errors (#438)

--- include/boost/simd/constant/definition/inveps.hpp
+++ include/boost/simd/constant/definition/inveps.hpp
@@ -32,7 +32,7 @@ namespace boost { namespace simd
 
   namespace ext
   {
-    BOOST_DISPATCH_FUNCTION_DECLARATION(tag,inveps_);
+    BOOST_DISPATCH_FUNCTION_DECLARATION(tag,inveps_)
   }
 
   namespace detail
