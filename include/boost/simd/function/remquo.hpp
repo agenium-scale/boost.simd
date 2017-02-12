0323cfb417972d5ea8106270df40d82afd18145c
gquintin <guillaume.quintin@numscale.com>
2017-02-12 23:07:53 +0100

    Fix doc for remquo

--- include/boost/simd/function/remquo.hpp
+++ include/boost/simd/function/remquo.hpp
@@ -18,9 +18,9 @@ namespace boost { namespace simd
   /*!
     @ingroup group-arithmetic
 
-    This function object  computes the remainder (rem) and a part of the quotient (quo) upon division of
-    @c x by @c y. By design, the value of the remainder is the same as that
-    computed by the @ref remainder standard function. The value of the computed quotient has
+    This function object  computes the remainder (rem) and a part of the quotient (quo) upon
+    division of @c x by @c y. By design, the value of the remainder is the same as that
+    computed by the @ref rem standard function. The value of the computed quotient has
     the sign of @c x/y and agrees with the actual quotient in at least the low
     order 3 bits.
 
@@ -29,7 +29,8 @@ namespace boost { namespace simd
 
     @par semantic:
 
-     If T is the common type to @c x and @c y
+    If T is the common type to @c x and @c y
+
     @code
       std::pair< T, as_integer_t<T> > p = remquo(x, y);
     @endcode
@@ -39,15 +40,15 @@ namespace boost { namespace simd
     @par Note
 
       - This function mimics a standard C library one that was mainly written in its time to
-      help computation of periodic trigonometric functions: three bits of @c quo allowing to know the
-      'octant'
+        help computation of periodic trigonometric functions: three bits of @c quo allowing
+        to know the 'octant'
 
-      - This implementation differs from std::remquo as the quotient is not returned as a pointer, and
-      his type is not int but the signed integer type associated to the floating one, to allow
-      proper SIMD implementation.
+      - This implementation differs from std::remquo as the quotient is not returned as a
+        pointer, and his type is not int but the signed integer type associated to the floating
+        one, to allow proper SIMD implementation.
 
       - also note that the double implementation of std::remquo is flawed in GNU C
-      Library until version 2.21 (2.22 been corrected).
+        Library until version 2.21 (2.22 been corrected).
 
     @par Example:
 
