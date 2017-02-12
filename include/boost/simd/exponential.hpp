dae3e00ee2fbd8cb299352473de6d8ca04090c66
gquintin <guillaume.quintin@numscale.com>
2017-02-12 23:50:47 +0100

    Fix doc for exponential

--- include/boost/simd/exponential.hpp
+++ include/boost/simd/exponential.hpp
@@ -20,15 +20,15 @@ namespace boost { namespace simd
 
     Those functions provide algorithms for computing exponentials and logarithms.
 
-center>
- |                      |                  |                  |                 |
- |:--------------------:|:----------------:|:----------------:|:---------------:|
- | @ref cbrt            | @ref exp10       | @ref exp2        | @ref exp        |
- | @ref expm1           | @ref exprecneg   | @ref exprecnegc  | @ref expx2      |
- | @ref log10           | @ref log2        | @ref log         | @ref log1p      |
- | @ref nthroot         | @ref pow2        | @ref pow_abs     | @ref pow        |
- | @ref significants    |                  |                  |                 |
-</center>
+    <center>
+     |                      |                  |                  |                 |
+     |:--------------------:|:----------------:|:----------------:|:---------------:|
+     | @ref cbrt            | @ref exp10       | @ref exp2        | @ref exp        |
+     | @ref expm1           | @ref exprecneg   | @ref exprecnegc  | @ref expx2      |
+     | @ref log10           | @ref log2        | @ref log         | @ref log1p      |
+     | @ref nthroot         | @ref pow2        | @ref pow_abs     | @ref pow        |
+     | @ref significants    |                  |                  |                 |
+    </center>
   **/
 
 } }
