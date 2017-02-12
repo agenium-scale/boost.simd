96548d52aeb384641a24c3895a003675df622552
gquintin <guillaume.quintin@numscale.com>
2017-02-12 23:10:16 +0100

    Fix doc for rem

--- include/boost/simd/function/rem.hpp
+++ include/boost/simd/function/rem.hpp
@@ -14,11 +14,10 @@
 #if defined(DOXYGEN_ONLY)
 namespace boost { namespace simd
 {
-
   /*!
-
     @ingroup group-oerator
-    This function object computes the quotient of the two parameters of the
+
+    This function object computes the remainder of the two parameters of the
     same type with or without option.
 
     @par Header <boost/simd/function/rem.hpp>
@@ -30,21 +29,23 @@ namespace boost { namespace simd
 
     - Options
 
-       With no option this function is equivalent to `x-div(fix, x, y)*y`
+      With no option this function is equivalent to `x-div(fix, x, y)*y`
 
-       `option` may be c@ref ceil, @ref floor, @ref fix, @ref round, @ref nearbyint (in the namespace booost::simd)
-       and provides the same result as `x-div(option, x, y)*y`.
+      `option` may be c@ref ceil, @ref floor, @ref fix, @ref round, @ref nearbyint (in the
+      namespace booost::simd) and provides the same result as `x-div(option, x, y)*y`.
 
     - Supported types
 
-       unsigned types are not supported but for the option @c fix, as in other cases result can be negative
+      unsigned types are not supported but for the option @c fix, as in other cases result
+      can be negative
 
     - Limiting values for floating entries:
 
-       -  if x is +/-inf , Nan is returned
-       -  if x is +/-0 and y is not 0, 0 is returned (if the sign of x matters the pedantic_ decorated version returns x)
-       -  If y is +/-0, Nan is returned
-       -  If either argument is NaN, Nan is returned
+       - if x is +/-inf , Nan is returned
+       - if x is +/-0 and y is not 0, 0 is returned (if the sign of x matters the pedantic_
+         decorated version returns x)
+       - if y is +/-0, Nan is returned
+       - if either argument is NaN, Nan is returned
 
     @par Decorators
 
