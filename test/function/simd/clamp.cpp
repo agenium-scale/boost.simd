514564dc2e4699f4eeac656dbe9ff7a380e4ad75
Joel Falcou <joel.falcou@lri.fr>
2017-02-15 15:05:45 +0100

    Make test of clamp workign for small integer in large vector

--- test/function/simd/clamp.cpp
+++ test/function/simd/clamp.cpp
@@ -20,18 +20,17 @@ void test(Env& runtime)
   namespace bs = boost::simd;
   using p_t = bs::pack<T, N>;
 
-  T a1[N], a2[N], a3[N], b[N];
+  T a1[N],  b[N];
   for(int i = 0; i < N; ++i)
   {
      a1[i] = i;
-     a2[i] = T(i-N/2+N);
-     a3[i] = T((i+N/2+N));
-     b[i] = bs::clamp(a1[i], a2[i], a3[i]);
+     b[i]  = bs::clamp(a1[i], T(0.2*N), T(0.7*N));
    }
   p_t aa1(&a1[0], &a1[0]+N);
-  p_t aa2(&a2[0], &a2[0]+N);
-  p_t aa3(&a3[0], &a3[0]+N);
+  p_t aa2(0.2*N);
+  p_t aa3(0.7*N);
   p_t bb (&b [0], &b [0]+N);
+
   STF_IEEE_EQUAL(bs::clamp(aa1, aa2, aa3), bb);
 }
 
