e2c237a61b61c9456b071f0028a6f02b1265efe1
jtlap <jtlapreste@gmail.com>
2017-02-15 15:31:11 +0100

    integral benches for average

---
+++ bench/function/scalar/average/regular.int16_t.m10_10_m10_10.cpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor average in scalar mode for std::int16_t type with no decorator (regular call).
+#include <scalar_bench.hpp>
+#include <boost/scalar/function/average.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::scalar;
+
+DEFINE_SCALAR_BENCH(scalar_average, bs::average);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_average, std::int16_t>(-10, 10,-10, 10);
+}
