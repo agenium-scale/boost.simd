c6203fc7b1ce6d67ae60bbed36e2253aa45a86b2
jtlap <jtlapreste@gmail.com>
2017-02-15 15:25:39 +0100

    integral benches for any

---
+++ bench/function/scalar/any/regular.uint8_t.0_10.cpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor any in scalar mode for std::uint8_t type with no decorator (regular call).
+#include <scalar_bench.hpp>
+#include <boost/scalar/function/any.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::scalar;
+
+DEFINE_SCALAR_BENCH(scalar_any, bs::any);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_any, std::uint8_t>(0, 10);
+}
