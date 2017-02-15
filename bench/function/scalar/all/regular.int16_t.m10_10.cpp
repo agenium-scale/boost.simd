60f68737f2f3fd3e1bb318612e6654d4eeaea254
jtlap <jtlapreste@gmail.com>
2017-02-15 15:24:02 +0100

    integral benches for all

---
+++ bench/function/scalar/all/regular.int16_t.m10_10.cpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor all in scalar mode for std::int16_t type with no decorator (regular call).
+#include <scalar_bench.hpp>
+#include <boost/scalar/function/all.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::scalar;
+
+DEFINE_SCALAR_BENCH(scalar_all, bs::all);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_all, std::int16_t>(-10, 10);
+}
