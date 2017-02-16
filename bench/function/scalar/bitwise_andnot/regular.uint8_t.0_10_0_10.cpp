6a8c03bfdd335b27dc7ed3eaed76d7e6b7ffe514
jtlap <jtlapreste@gmail.com>
2017-02-16 17:26:32 +0100

    bitwise_andnot benches

---
+++ bench/function/scalar/bitwise_andnot/regular.uint8_t.0_10_0_10.cpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor bitwise_andnot in scalar mode for std::uint8_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/bitwise_andnot.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SCALAR_BENCH(scalar_bitwise_andnot, bs::bitwise_andnot);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_bitwise_andnot, std::uint8_t>(-10, 10,-10, 10);
+}
