ead886a619284dc2c40a95ffed41ff3a0eaa2e92
jtlap <jtlapreste@gmail.com>
2017-02-18 09:39:13 +0100

    firstbitset benches

---
+++ bench/function/scalar/firstbitset/regular.int8_t.m10_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor firstbitset in scalar mode for std::int8_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/firstbitset.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SCALAR_BENCH(scalar_firstbitset, bs::firstbitset);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_firstbitset, std::int8_t>(-10, 10);
+}
+
