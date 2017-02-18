0a8ce99e08c5173b5acda846b130d754ca7b2df5
jtlap <jtlapreste@gmail.com>
2017-02-18 09:15:42 +0100

    deinterleave benches

---
+++ bench/function/simd/deinterleave/regular.int32_t.m10_10_m10_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor deinterleave in simd mode for std::int32_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/deinterleave.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SIMD_BENCH(simd_deinterleave, bs::deinterleave);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<simd_deinterleave, std::int32_t>(-10, 10,-10, 10);
+}
+
