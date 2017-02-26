cb1ff5d0c65af8083c863aac72aea5ed8a81a1f1
jtlap <jtlapreste@gmail.com>
2017-02-26 21:46:26 +0100

    ifnot_inc benches

---
+++ bench/function/simd/ifnot_inc/regular.int32_t.m10_10_m10_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor ifnot_inc in simd mode for std::int32_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/ifnot_inc.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SIMD_BENCH(simd_ifnot_inc, bs::ifnot_inc);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<simd_ifnot_inc, std::int32_t>(-10, 10,-10, 10);
+}
+
