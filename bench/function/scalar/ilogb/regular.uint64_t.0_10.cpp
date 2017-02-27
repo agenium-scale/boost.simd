727f4f7c071fd17090faf26c516e78654a01a363
jtlap <jtlapreste@gmail.com>
2017-02-27 17:32:58 +0100

    ilogb benches

---
+++ bench/function/scalar/ilogb/regular.uint64_t.0_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor ilogb in scalar mode for std::uint64_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/ilogb.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SCALAR_BENCH(scalar_ilogb, bs::ilogb);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<scalar_ilogb, std::uint64_t>(0, 10);
+}
+
