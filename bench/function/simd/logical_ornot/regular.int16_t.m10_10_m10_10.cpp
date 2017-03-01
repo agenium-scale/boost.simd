6b7de434d1ba86a2dfec231a5854e30748bcf339
jtlap <jtlapreste@gmail.com>
2017-03-01 08:14:44 +0100

    logical_xxx benches

---
+++ bench/function/simd/logical_ornot/regular.int16_t.m10_10_m10_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor logical_ornot in simd mode for std::int16_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/logical_ornot.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SIMD_BENCH(simd_logical_ornot, bs::logical_ornot);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<simd_logical_ornot, std::int16_t>(-10, 10,-10, 10);
+}
+
