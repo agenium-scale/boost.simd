3dbd76ed1209ed6085888aa975434cb003ac0dc3
jtlap <jtlapreste@gmail.com>
2017-03-01 13:06:44 +0100

    reverse round benches

---
+++ bench/function/simd/round/regular.int64_t.m10_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor round in simd mode for std::int64_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/round.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SIMD_BENCH(simd_round, bs::round);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<simd_round, std::int64_t>(-10, 10);
+}
+
