63bb12ea9aebe8ad7767a95d5ce954232a026eab
jtlap <jtlapreste@gmail.com>
2017-03-01 11:12:34 +0100

    oneminus and pxxx benches

--- bench/function/scalar/oneminus/regular.uint16_t.0_10.cpp
+++ bench/function/scalar/oneminus/regular.uint16_t.0_10.cpp
@@ -1,21 +1,22 @@
 // -------------------------------------------------------------------------------------------------
-//                              Copyright 2016 - NumScale SAS
+//                              Copyright 2017 - NumScale SAS
 //
 //                   Distributed under the Boost Software License, Version 1.0.
 //                        See accompanying file LICENSE.txt or copy at
 //                            http://www.boost.org/LICENSE_1_0.txt
 // -------------------------------------------------------------------------------------------------
 
-/// bench for functor pow in scalar mode for double type with no decorator (regular call).
+/// bench for functor oneminus in scalar mode for std::uint16_t type with no decorator (regular call).
 #include <simd_bench.hpp>
-#include <boost/simd/function/pow.hpp>
+#include <boost/simd/function/oneminus.hpp>
 
 namespace nsb = ns::bench;
 namespace bs =  boost::simd;
 
-DEFINE_SCALAR_BENCH(scalar_pow, bs::pow);
+DEFINE_SCALAR_BENCH(scalar_oneminus, bs::oneminus);
 
 DEFINE_BENCH_MAIN()
 {
-  nsb::for_each<scalar_pow, double>(-10, 10);
+  nsb::for_each<scalar_oneminus, std::uint16_t>(0, 10);
 }
+
