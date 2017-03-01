77bcdf0ad9743e0ed777b88be78208bf70ec7ef4
jtlap <jtlapreste@gmail.com>
2017-03-01 14:07:09 +0100

    sxx, txxx uxxx integer benches

---
+++ bench/function/simd/successor/regular.uint32_t.0_10.cpp
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+//                              Copyright 2017 - NumScale SAS
+//
+//                   Distributed under the Boost Software License, Version 1.0.
+//                        See accompanying file LICENSE.txt or copy at
+//                            http://www.boost.org/LICENSE_1_0.txt
+// -------------------------------------------------------------------------------------------------
+
+/// bench for functor successor in simd mode for std::uint32_t type with no decorator (regular call).
+#include <simd_bench.hpp>
+#include <boost/simd/function/successor.hpp>
+
+namespace nsb = ns::bench;
+namespace bs =  boost::simd;
+
+DEFINE_SIMD_BENCH(simd_successor, bs::successor);
+
+DEFINE_BENCH_MAIN()
+{
+  nsb::for_each<simd_successor, std::uint32_t>(0, 10);
+}
+
