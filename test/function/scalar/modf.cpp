daee401f0e864572b5bf5647ee890b8fb52ad38d
Joel Falcou <joel.falcou@lri.fr>
2017-04-10 10:53:19 +0200

    Remove spurrious dependency to fusion

--- test/function/scalar/modf.cpp
+++ test/function/scalar/modf.cpp
@@ -8,46 +8,20 @@
 */
 //==================================================================================================
 #include <boost/simd/function/scalar/modf.hpp>
-#include <scalar_test.hpp>
 #include <boost/simd/function/trunc.hpp>
-#include <boost/simd/function/frac.hpp>
+#include <scalar_test.hpp>
 #include <utility>
-
 
 
 STF_CASE_TPL(" modf", STF_NUMERIC_TYPES)
 {
   namespace bs = boost::simd;
-  namespace bd = boost::dispatch;
   using bs::modf;
-
-
-  STF_EXPR_IS( (modf(T()))
-             , (std::pair<T,T>)
-             );
-
-  {
-    T rest;
-    T rounded;
-
-    modf(T(1.5), rest, rounded);
-    STF_EQUAL(rounded, bs::trunc(T(1.5)));
-    STF_EQUAL(rest, T(.5));
-  }
-
-  {
-    T rest;
-    T rounded;
-
-    rest = modf(T(1.5), rounded);
-    STF_EQUAL(rounded, bs::trunc(T(1.5)));
-    STF_EQUAL(rest, T(.5));
-  }
 
   {
     std::pair<T,T> p;
 
-    p = modf(T(1.5));
+    p = bs::modf(T(1.5));
     STF_EQUAL(p.first , T(.5));
     STF_EQUAL(p.second  , bs::trunc(T(1.5)));
   }
