9da88db4e4052ebe7a56603d74391fb589ebabf8
Joel Falcou <joel.falcou@lri.fr>
2017-05-18 14:15:30 +0200

    Fix #6 - Implement subadd (#511)

---
+++ include/boost/simd/function/subadd.hpp
@@ -0,0 +1,37 @@
+//==================================================================================================
+/*!
+  @file
+
+  @copyright 2017 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#ifndef BOOST_SIMD_FUNCTION_SUBADD_HPP_INCLUDED
+#define BOOST_SIMD_FUNCTION_SUBADD_HPP_INCLUDED
+
+namespace boost { namespace simd
+{
+  /*!
+    @ingroup group-swar
+    @ingroup swar-subadd
+
+    Computes the interleaved sum/difference of two vlaues
+
+    @par Header <boost/simd/function/subadd.hpp>
+    @par Example:
+
+    @snippet abs.cpp abs
+
+    @par Possible output:
+
+    @snippet abs.txt abs
+
+  **/
+} }
+
+#include <boost/simd/function/scalar/subadd.hpp>
+#include <boost/simd/function/simd/subadd.hpp>
+
+#endif
