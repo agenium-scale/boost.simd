d2f3735c6b0722aeaaf2a29b229ed62dae5f6772
Joel Falcou <joel.falcou@lri.fr>
2017-06-13 09:42:10 +0200

    ULP fixes on erf* scalar functions

--- test/function/scalar/erf.cpp
+++ test/function/scalar/erf.cpp
@@ -30,27 +30,27 @@ STF_CASE_TPL (" erf", STF_IEEE_TYPES)
 
   // specific values tests
 #ifndef BOOST_SIMD_NO_INVALIDS
-  STF_ULP_EQUAL(erf(bs::Minf<T>()), T(-1), 0);
-  STF_ULP_EQUAL(erf(bs::Inf<T>()), T(1), 0);
-  STF_ULP_EQUAL(erf(bs::Nan<T>()), bs::Nan<T>(), 0);
+  STF_IEEE_EQUAL(erf(bs::Minf<T>()), T(-1));
+  STF_IEEE_EQUAL(erf(bs::Inf<T>()), T(1));
+  STF_IEEE_EQUAL(erf(bs::Nan<T>()), bs::Nan<T>());
 #endif
 
-  STF_ULP_EQUAL(erf(T(0.1)), T(0.112462916018284892203275071744), 0);
+  STF_ULP_EQUAL(erf(T(0.1)), T(0.112462916018284892203275071744), 0.5);
 
-  STF_ULP_EQUAL(erf(bs::Mzero<T>()), T(0), 0);
+  STF_ULP_EQUAL(erf(bs::Mzero<T>()), T(0), 0.5);
   STF_ULP_EQUAL(erf(T(0.5)), T(0.520499877813046537682746653892), 0.5);
   STF_ULP_EQUAL(erf(T(1)), T(0.842700792949714869341220635083), 0.5);
-  STF_ULP_EQUAL(erf(T(2)), T(0.995322265018952734162069256367), 0);
-  STF_ULP_EQUAL(erf(T(0)), T(0), 0);
-  STF_ULP_EQUAL(erf(bs::Halfeps<T>()), bs::Eps<T>()*bs::rsqrt(bs::Pi<T>()), 0);
-  STF_ULP_EQUAL(erf(T(5)), T(0.99999999999846254020557196515), 0);
-  STF_ULP_EQUAL(erf(T(27)), T(1), 0);
+  STF_ULP_EQUAL(erf(T(2)), T(0.995322265018952734162069256367), 0.5);
+  STF_ULP_EQUAL(erf(T(0)), T(0), 0.5);
+  STF_ULP_EQUAL(erf(bs::Halfeps<T>()), bs::Eps<T>()*bs::rsqrt(bs::Pi<T>()), 0.5);
+  STF_ULP_EQUAL(erf(T(5)), T(0.99999999999846254020557196515), 0.5);
+  STF_ULP_EQUAL(erf(T(27)), T(1), 0.5);
 
-  STF_ULP_EQUAL(erf(T(-0.1)), -T(0.112462916018284892203275071744), 0);
+  STF_ULP_EQUAL(erf(T(-0.1)), -T(0.112462916018284892203275071744), 0.5);
   STF_ULP_EQUAL(erf(-T(0.5)), -T(0.520499877813046537682746653892), 0.5);
   STF_ULP_EQUAL(erf(-T(1)), -T(0.842700792949714869341220635083), 0.5);
-  STF_ULP_EQUAL(erf(-T(2)), -T(0.995322265018952734162069256367), 0);
-  STF_ULP_EQUAL(erf(-bs::Halfeps<T>()), -bs::Eps<T>()*bs::rsqrt(bs::Pi<T>()), 0);
-  STF_ULP_EQUAL(erf(-T(5)), -T(0.99999999999846254020557196515), 0);
-  STF_ULP_EQUAL(erf(T(-27)), -T(1), 0);
+  STF_ULP_EQUAL(erf(-T(2)), -T(0.995322265018952734162069256367), 0.5);
+  STF_ULP_EQUAL(erf(-bs::Halfeps<T>()), -bs::Eps<T>()*bs::rsqrt(bs::Pi<T>()), 0.5);
+  STF_ULP_EQUAL(erf(-T(5)), -T(0.99999999999846254020557196515), 0.5);
+  STF_ULP_EQUAL(erf(T(-27)), -T(1), 0.5);
 }
--- test/function/scalar/erfc.regular.cpp
+++ test/function/scalar/erfc.regular.cpp
@@ -35,18 +35,18 @@ STF_CASE_TPL (" erfc",  STF_IEEE_TYPES)
 
   // specific values tests
 #ifndef BOOST_SIMD_NO_INVALIDS
-  STF_ULP_EQUAL(erfc(bs::Inf<T>()), bs::Zero<r_t>(), 0);
-  STF_ULP_EQUAL(erfc(bs::Minf<T>()), bs::Two<r_t>(), 0);
-  STF_ULP_EQUAL(erfc(bs::Nan<T>()), bs::Nan<r_t>(), 0);
+  STF_IEEE_EQUAL(erfc(bs::Inf<T>()), bs::Zero<r_t>());
+  STF_IEEE_EQUAL(erfc(bs::Minf<T>()), bs::Two<r_t>());
+  STF_IEEE_EQUAL(erfc(bs::Nan<T>()), bs::Nan<r_t>());
 #endif
   STF_ULP_EQUAL(erfc(bs::Mzero<T>()), bs::One<r_t>(), 0.5);
-  STF_ULP_EQUAL(erfc(T(0.1)), T(0.887537083981715107796724928256), 0);
+  STF_ULP_EQUAL(erfc(T(0.1)), T(0.887537083981715107796724928256), 0.5);
   STF_ULP_EQUAL(erfc(T(0.4)), T(0.57160764495333154489639615468), 0.5);
   STF_ULP_EQUAL(erfc(bs::Half<T>()), T(0.479500122186953462317253346108), 1.5);
   STF_ULP_EQUAL(erfc(bs::One<T>()), T(0.157299207050285130658779364917), 0.5);
   STF_ULP_EQUAL(erfc(bs::Two<T>()), T(0.00467773498104726583793074363275), 1);
   STF_ULP_EQUAL(erfc(bs::Zero<T>()), bs::One<r_t>(), 0.5);
-  STF_ULP_EQUAL(erfc(T(6)), T(0.0000000000000000215197367124989131165933503992), 0);
+  STF_ULP_EQUAL(erfc(T(6)), T(0.0000000000000000215197367124989131165933503992), 0.5);
   STF_EXPECT(bs::is_less(erfc((bs::Ten<T>()- T(2.088487583762545e-45))), bs::Eps<T>()));
   STF_EXPECT(bs::is_less(erfc(T(15))- T(7.212994172451206e-100), bs::Eps<T>()));
 
@@ -55,7 +55,7 @@ STF_CASE_TPL (" erfc",  STF_IEEE_TYPES)
   STF_ULP_EQUAL(bs::std_(erfc)(-bs::Half<T>()), (erfc)(-bs::Half<T>()), 0.5);
   STF_ULP_EQUAL(erfc(-bs::One<T>()),  bs::std_(erfc)(-bs::One<T>()),  0.5);
   STF_ULP_EQUAL(erfc(-bs::Two<T>()),  bs::std_(erfc)(-bs::Two<T>()),  1);
-  STF_ULP_EQUAL(erfc(T(-6)), T(2), 0);
+  STF_ULP_EQUAL(erfc(T(-6)), T(2), 0.5);
 }
 
 
