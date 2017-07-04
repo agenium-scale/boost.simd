f18a545c206760bd777bed16a972462ef838a006
Charly Chevalier <charly.chevalier@numscale.com>
2017-07-04 17:40:39 +0200

    README + doc

--- doc/html/search/functions_14.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zero',['Zero',['../group__group-constant_gae164aaf5db0db0ba079c2f338d2d794d.html#gae164aaf5db0db0ba079c2f338d2d794d',1,'boost::simd']]]
-];
--- doc/html/search/pages_c.js
+++ doc/html/search/pages_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['tutorial_5falgorithm',['tutorial_algorithm',['../md__d_1_dev_opensource_boost_8simd_doc_custom_sections_tutorials_tutorial_algorithm.html',1,'']]],
+  ['tutorial_5falgorithm',['tutorial_algorithm',['../md__home_charly_workspace_cpp_boost_8simd_8avx2_doc_custom_sections_tutorials_tutorial_algorithm.html',1,'']]],
   ['tutorials',['Tutorials',['../tutorials.html',1,'']]]
 ];
